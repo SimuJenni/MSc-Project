89cbe2b925a0d5d09b95bfec724661d2e0dd5d0d
Simon Jenni <simujenni@students.unibe.ch>
2016-09-29 15:56:53 +0200

    No flipping of images

--- MScProject/sanity_net.py
+++ MScProject/sanity_net.py
@@ -60,7 +60,7 @@ datagen = ImageDataGenerator(
     rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
     width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
     height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-    horizontal_flip=True,  # randomly flip images
+    horizontal_flip=False,  # randomly flip images
     vertical_flip=False)  # randomly flip images
 
 # compute quantities required for featurewise normalization

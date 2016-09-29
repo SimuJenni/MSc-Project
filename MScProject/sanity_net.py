45d330959d7d72c8611beced8e2256547f73cebb
Simon Jenni <simujenni@students.unibe.ch>
2016-09-29 11:09:45 +0200

    Test AE on cifar-10

--- MScProject/sanity_net.py
+++ MScProject/sanity_net.py
@@ -1,8 +1,27 @@
 import matplotlib.pyplot as plt
 from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
+from keras.datasets import cifar10
+from keras.preprocessing.image import ImageDataGenerator
+import numpy as np
 
-input_img = Input(shape=(28, 28, 1))
+
+batch_size = 32
+nb_epoch = 200
+data_augmentation = True
+
+# Image dimensions
+img_rows, img_cols = 32, 32
+img_channels = 3
+
+# the data, shuffled and split between train and test sets
+(X_train, _), (X_test, _) = cifar10.load_data()
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+
+input_img = Input(shape=(img_rows, img_cols, img_channels))
 
 x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(input_img)
 x = MaxPooling2D((2, 2), border_mode='same')(x)
@@ -17,48 +36,44 @@ x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(encoded)
 x = UpSampling2D((2, 2))(x)
 x = Convolution2D(8, 3, 3, activation='relu', border_mode='same')(x)
 x = UpSampling2D((2, 2))(x)
-x = Convolution2D(16, 3, 3, activation='relu')(x)
+x = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(x)
 x = UpSampling2D((2, 2))(x)
-decoded = Convolution2D(1, 3, 3, activation='sigmoid', border_mode='same')(x)
+decoded = Convolution2D(3, 3, 3, activation='sigmoid', border_mode='same')(x)
 
 autoencoder = Model(input_img, decoded)
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
-from keras.datasets import mnist
-import numpy as np
 
-(x_train, _), (x_test, _) = mnist.load_data()
-
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
-x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+X_train = X_train.astype('float32') / 255.
+X_test = X_test.astype('float32') / 255.
+X_train = np.reshape(X_train, (len(X_train), img_rows, img_cols, img_channels))
+X_test = np.reshape(X_test, (len(X_test), img_rows, img_cols, img_channels))
 
 from keras.callbacks import TensorBoard
 
-autoencoder.fit(x_train, x_train,
-                nb_epoch=50,
-                batch_size=128,
+autoencoder.fit(X_train, X_train,
+                nb_epoch=nb_epoch,
+                batch_size=batch_size,
                 shuffle=True,
-                validation_data=(x_test, x_test),
-                callbacks=[TensorBoard(log_dir='/data/cvg/simon/tensorboard/')])
+                validation_data=(X_test, X_test),
+                callbacks=[TensorBoard(log_dir='./logs')])
 
 
-decoded_imgs = autoencoder.predict(x_test)
+decoded_imgs = autoencoder.predict(X_test)
 
 n = 10
 plt.figure(figsize=(20, 4))
 for i in range(n):
     # display original
     ax = plt.subplot(2, n, i)
-    plt.imshow(x_test[i].reshape(28, 28))
+    plt.imshow(X_test[i].reshape(img_rows, img_cols))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
     # display reconstruction
     ax = plt.subplot(2, n, i + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.imshow(decoded_imgs[i].reshape(img_rows, img_cols))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)

377ca66f32f627dc9030492714d82f4ede1de357
Simon Jenni <simujenni@students.unibe.ch>
2016-10-03 12:43:40 +0200

    Trying to fix mem-leak

--- datasets/Dataset.py
+++ datasets/Dataset.py
@@ -31,24 +31,20 @@ class Dataset:
         """
         num_val = len(self.val_files)
         for i in range(0, len(self.train_files)):
-            val = h5py.File(self.val_files[i % num_val], 'r')
-            train = h5py.File(self.train_files[i], 'r')
-            train_X = im2float_array(train['X'][:])
-            train_Y = im2float_array(train['Y'][:])
-            test_X = im2float_array(val['X'][:])
-            test_Y = im2float_array(val['Y'][:])
-            val.close()
-            train.close()
-            yield (train_X,
-                   train_Y,
-                   test_X,
-                   test_Y)
+            with h5py.File(self.val_files[i % num_val], 'r') as val:
+                test_X = im2float_array(val['X'][:])
+                test_Y = im2float_array(val['Y'][:])
+            with h5py.File(self.train_files[i], 'r') as train:
+                train_X = im2float_array(train['X'][:])
+                train_Y = im2float_array(train['Y'][:])
+            yield (train_X, train_Y, test_X, test_Y)
 
 
     def get_sample(self, sample_size):
         """
             Returns a representative sample set of the training data.
         """
-        train = h5py.File(self.train_files[0], 'r')
-        sample_size = min(sample_size, len(train))
-        return im2float_array(train['X'][:sample_size])
+        with h5py.File(self.train_files[0], 'r') as train:
+            sample_size = min(sample_size, len(train))
+            sample = train['X'][:sample_size]
+        return sample

798322e75cbe6a1f11b6c5891131f7133658365b
Simon Jenni <simujenni@students.unibe.ch>
2016-10-04 02:37:03 +0200

    Just trying things out

--- ToonRunner.py
+++ ToonRunner.py
@@ -9,7 +9,7 @@ from datasets.TinyImagenet import TinyImagenet
 from datasets.Imagenet import Imagenet
 from utils import montage
 
-batch_size = 100
+batch_size = 64
 nb_epoch = 1
 
 # Get the data-set object
@@ -17,25 +17,25 @@ data = Imagenet()
 datagen = ImageDataGenerator()
 
 # Load the net
-net, _ = ToonResNet(input_shape=data.get_dims(), batch_size=batch_size, out_activation='sigmoid', num_res_layers=10)
+net, _ = ToonResNet(input_shape=data.get_dims(), batch_size=batch_size, out_activation='sigmoid', num_res_layers=8)
 
 # Training
 for e in range(nb_epoch):
     print("Epoch {} / {}".format(e+1, nb_epoch))
     # Generate batches of around 5000 samples
-    for X_train, Y_train, X_test, Y_test in data.generator(batch_size):
-        progbar = generic_utils.Progbar(X_train.shape[0])
-        for X_batch, Y_batch in datagen.flow(X_train, Y_train, batch_size=batch_size):
-            loss = net.train_on_batch(X_batch, Y_batch)
-            progbar.add(X_batch.shape[0], values=[("train loss", loss)])
+    for X_train, Y_train in data.generator(batch_size):
+        net.fit_generator(datagen.flow(X_train, Y_train, batch_size=batch_size),
+                          samples_per_epoch=X_train.shape[0],
+                          nb_epoch=2,
+                          verbose=2)
         gc.collect()
 
-decoded_imgs = net.predict(X_test, batch_size=batch_size)
-decoded_imgs = np.clip(decoded_imgs, 0.0, 1.0)
-
-
-montage(X_test[:100, :, :], 'ToonResNet-X')
-montage(decoded_imgs[:100, :, :], 'ToonResNet-Out')
-montage(Y_test[:100, :, :], 'ToonResNet-Y')
+# decoded_imgs = net.predict(X_test, batch_size=batch_size)
+# decoded_imgs = np.clip(decoded_imgs, 0.0, 1.0)
+#
+#
+# montage(X_test[:100, :, :], 'ToonResNet-X')
+# montage(decoded_imgs[:100, :, :], 'ToonResNet-Out')
+# montage(Y_test[:100, :, :], 'ToonResNet-Y')
 
 net.save('ToonNet_tiny-imagenet.h5')

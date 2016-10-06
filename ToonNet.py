8efdc8953c90fef965cb02269b7151e38dd747f5
Simon Jenni <simujenni@students.unibe.ch>
2016-10-06 13:01:44 +0200

    Added better initializers and choice of merge for outer-layers

--- ToonNet.py
+++ ToonNet.py
@@ -1,38 +1,40 @@
+import tensorflow as tf
 from keras.layers import Input, Convolution2D, BatchNormalization, Deconvolution2D, Activation, merge
 from keras.models import Model
-import tensorflow as tf
+from keras.layers.advanced_activations import LeakyReLU
 
 NUM_CONV_LAYERS = 5
-# F_DIMS = [32, 64, 128, 256, 512]
 # F_DIMS = [64, 96, 128, 256, 512]
-F_DIMS = [48, 80, 128, 208, 336]
+F_DIMS = [64, 96, 160, 256, 416]
+# F_DIMS = [48, 80, 128, 208, 336]
 
 
-def ToonNet(input_shape, batch_size, out_activation='sigmoid', num_res_layers=10):
+def ToonNet(input_shape, batch_size, out_activation='sigmoid', num_res_layers=10, merge_mode='concat'):
     """Constructs a fully convolutional residual auto-encoder network.
     The network has the follow architecture:
 
     Layer           Filters     Stride  Connected
 
-    L1: Conv-layer  4x4x64      1       L25         =======================================
-    L2: Conv-layer  3x3x96      2       L24               ===========================
-    L3: Conv-layer  3x3x128     2       L23                    =================
-    L4: Conv-layer  3x3x256     2       L22                        =========
-    L5: Conv-layer  3x3x512     2                                     ===
-                                                                      |_|
-    L6:                                                               |_|
-    .               1x1x64      1                                     |_|
-    .   Res-Layers  3x3x64      1                                     |_|
-    .               3x3x512     1                                     |_|
-    L20:                                                              |_|
-                                                                      |_|
-    L21: UpConv     3x3x256     2                                     ===
-    L22: UpConv     3x3x128     2       L4                         =========
-    L23: UpConv     3x3x96      2       L3                     =================
-    L24: UpConv     3x3x64      2       L2                ===========================
-    L25: UpConv     4x4x64      1       L1          =======================================
+    L1: Conv-layer  4x4x64      1       L25            =================================||
+    L2: Conv-layer  3x3x96      2       L24                =========================||  ||
+    L3: Conv-layer  3x3x128     2       L23                    =================||  ||  ||
+    L4: Conv-layer  3x3x256     2       L22                        =========||  ||  ||  ||
+    L5: Conv-layer  3x3x512     2                                     ===   ||  ||  ||  ||
+                                                                      |_|   ||  ||  ||  ||
+    L6:                                                               |_|   ||  ||  ||  ||
+    .               1x1x64      1                                     |_|   ||  ||  ||  ||
+    .   Res-Layers  3x3x64      1                                     |_|   O4  O3  O2  O1
+    .               3x3x512     1                                     |_|   ||  ||  ||  ||
+    L20:                                                              |_|   ||  ||  ||  ||
+                                                                      |_|   ||  ||  ||  ||
+    L21: UpConv     3x3x256     2                                     ===   ||  ||  ||  ||
+    L22: UpConv     3x3x128     2       L4                         =========||  ||  ||  ||
+    L23: UpConv     3x3x96      2       L3                     =================||  ||  ||
+    L24: UpConv     3x3x64      2       L2                 =========================||  ||
+    L25: UpConv     4x4x64      1       L1             =================================||
 
     Args:
+        merge_mode: Mode for merging the outer connections
         input_shape: Shape of the input images (height, width, channels)
         batch_size: Number of images per batch
         out_activation: Type of activation for last layer ('relu', 'sigmoid', 'tanh', ...)
@@ -49,25 +51,25 @@ def ToonNet(input_shape, batch_size, out_activation='sigmoid', num_res_layers=10
     # Layer 1
     with tf.name_scope('conv_layer_1'):
         x = conv_bn_relu(input_im, f_size=4, f_channels=F_DIMS[0], stride=1, border='valid')
-        l1 = Convolution2D(F_DIMS[0], 1, 1, border_mode='valid', subsample=(1, 1))(x)
+        l1 = Convolution2D(F_DIMS[0], 1, 1, border_mode='valid', subsample=(1, 1), init='he_normal')(x)
         l1 = BatchNormalization(axis=3)(l1)
 
     # Layer 2
     with tf.name_scope('conv_layer_2'):
         x = conv_bn_relu(x, f_size=3, f_channels=F_DIMS[1], stride=2, border='same')
-        l2 = Convolution2D(F_DIMS[1], 1, 1, border_mode='valid', subsample=(1, 1))(x)
+        l2 = Convolution2D(F_DIMS[1], 1, 1, border_mode='valid', subsample=(1, 1), init='he_normal')(x)
         l2 = BatchNormalization(axis=3)(l2)
 
     # Layer 3
     with tf.name_scope('conv_layer_3'):
         x = conv_bn_relu(x, f_size=3, f_channels=F_DIMS[2], stride=2, border='valid')
-        l3 = Convolution2D(F_DIMS[2], 1, 1, border_mode='valid', subsample=(1, 1))(x)
+        l3 = Convolution2D(F_DIMS[2], 1, 1, border_mode='valid', subsample=(1, 1), init='he_normal')(x)
         l3 = BatchNormalization(axis=3)(l3)
 
     # Layer 4
     with tf.name_scope('conv_layer_4'):
         x = conv_bn_relu(x, f_size=3, f_channels=F_DIMS[3], stride=2, border='valid')
-        l4 = Convolution2D(F_DIMS[3], 1, 1, border_mode='valid', subsample=(1, 1))(x)
+        l4 = Convolution2D(F_DIMS[3], 1, 1, border_mode='valid', subsample=(1, 1), init='he_normal')(x)
         l4 = BatchNormalization(axis=3)(l4)
 
     # Layer 5
@@ -76,32 +78,35 @@ def ToonNet(input_shape, batch_size, out_activation='sigmoid', num_res_layers=10
 
     # All the res-layers
     for i in range(num_res_layers):
-        with tf.name_scope('res_layer_{}'.format(i+1)):
+        with tf.name_scope('res_layer_{}'.format(i + 1)):
             encoded = res_layer_bottleneck(encoded, F_DIMS[4], 64)
 
     # Layer 6
     with tf.name_scope('deconv_layer_1'):
         x = upconv_bn(encoded, f_size=3, f_channels=F_DIMS[3], out_dim=l_dims[4], batch_size=batch_size, stride=2,
                       border='valid')
-        x = merge([x, l4], mode='concat')
+        x = merge([x, l4], mode=merge_mode)
         x = Activation('relu')(x)
 
     # Layer 7
     with tf.name_scope('deconv_layer_2'):
-        x = upconv_bn(x, f_size=3, f_channels=F_DIMS[2], out_dim=l_dims[3], batch_size=batch_size, stride=2, border='valid')
-        x = merge([x, l3], mode='concat')
+        x = upconv_bn(x, f_size=3, f_channels=F_DIMS[2], out_dim=l_dims[3], batch_size=batch_size, stride=2,
+                      border='valid')
+        x = merge([x, l3], mode=merge_mode)
         x = Activation('relu')(x)
 
     # Layer 8
     with tf.name_scope('deconv_layer_3'):
-        x = upconv_bn(x, f_size=3, f_channels=F_DIMS[1], out_dim=l_dims[2], batch_size=batch_size, stride=2, border='valid')
-        x = merge([x, l2], mode='concat')
+        x = upconv_bn(x, f_size=3, f_channels=F_DIMS[1], out_dim=l_dims[2], batch_size=batch_size, stride=2,
+                      border='valid')
+        x = merge([x, l2], mode=merge_mode)
         x = Activation('relu')(x)
 
     # Layer 9
     with tf.name_scope('deconv_layer_4'):
-        x = upconv_bn(x, f_size=3, f_channels=F_DIMS[0], out_dim=l_dims[1], batch_size=batch_size, stride=2, border='same')
-        x = merge([x, l1], mode='concat')
+        x = upconv_bn(x, f_size=3, f_channels=F_DIMS[0], out_dim=l_dims[1], batch_size=batch_size, stride=2,
+                      border='same')
+        x = merge([x, l1], mode=merge_mode)
         x = Activation('relu')(x)
 
     # Layer 10
@@ -116,15 +121,26 @@ def ToonNet(input_shape, batch_size, out_activation='sigmoid', num_res_layers=10
 
 
 def conv_bn_relu(layer_in, f_size, f_channels, stride, border='valid'):
-    x = Convolution2D(f_channels, f_size, f_size, border_mode=border, subsample=(stride, stride))(layer_in)
+    x = Convolution2D(f_channels, f_size, f_size,
+                      border_mode=border,
+                      subsample=(stride, stride),
+                      init='he_normal')(layer_in)
     x = BatchNormalization(axis=3)(x)
     return Activation('relu')(x)
 
 
+def outter_connections(layer_in, f_channels):
+    l = Convolution2D(f_channels, 1, 1, border_mode='valid', subsample=(1, 1), init='he_normal')(layer_in)
+    l = BatchNormalization(axis=3)(l)
+    return LeakyReLU(alpha=0.3)(l)
+
+
 def upconv_bn(layer_in, f_size, f_channels, out_dim, batch_size, stride, border='valid'):
-    x = Deconvolution2D(f_channels, f_size, f_size, output_shape=(batch_size, out_dim, out_dim, f_channels),
+    x = Deconvolution2D(f_channels, f_size, f_size,
+                        output_shape=(batch_size, out_dim, out_dim, f_channels),
                         border_mode=border,
-                        subsample=(stride, stride))(layer_in)
+                        subsample=(stride, stride),
+                        init='he_normal')(layer_in)
     return BatchNormalization(axis=3)(x)
 
 
@@ -134,7 +150,7 @@ def res_layer_bottleneck(in_layer, out_dim, bn_dim):
     # 3x3 conv
     x = conv_bn_relu(x, f_size=3, f_channels=bn_dim, stride=1, border='same')
     # 1x1 to out_dim
-    x = Convolution2D(out_dim, 1, 1, border_mode='same', subsample=(1, 1))(x)
+    x = Convolution2D(out_dim, 1, 1, border_mode='same', subsample=(1, 1), init='he_normal')(x)
     x = BatchNormalization(axis=3)(x)
     x = merge([x, in_layer], mode='sum')
     x = Activation('relu')(x)
--- ToonRunner.py
+++ ToonRunner.py
@@ -3,23 +3,25 @@ import gc
 from keras.preprocessing.image import ImageDataGenerator
 
 from ToonNet import ToonNet
-from datasets.TinyImagenet import TinyImagenet
 from datasets.Imagenet import Imagenet
 from utils import montage
 
 batch_size = 32
 nb_epoch = 1
-nb_epoch_inner = 5
+nb_epoch_inner = 2
 max_train_chunks = 1000  # Chunks of size ~5000
 plot_while_train = True
+net_name = 'ToonNet-Inits-SumMerge'
 
 # Get the data-set object
 data = Imagenet()
 datagen = ImageDataGenerator()
 
 # Load the net
+# toon_net, encoder, decoder = ToonNet(input_shape=data.get_dims(), batch_size=batch_size, out_activation='sigmoid',
+#                                      num_res_layers=20)
 toon_net, encoder, decoder = ToonNet(input_shape=data.get_dims(), batch_size=batch_size, out_activation='sigmoid',
-                                     num_res_layers=20)
+                                     num_res_layers=10, merge_mode='sum')
 toon_net.summary()  # For debugging
 
 # Define objective and solver
@@ -42,13 +44,12 @@ for e in range(nb_epoch):
         if plot_while_train:
             # Test images
             decoded_imgs = toon_net.predict(X_test[:batch_size], batch_size=batch_size)
-            montage(decoded_imgs[:16, :, :], 'ToonNet-Train-{}'.format(chunk_count))
+            montage(decoded_imgs[:16, :, :], 'Train: {}-{}'.format(net_name, chunk_count))
         del X_train, Y_train, X_test, Y_test
         gc.collect()
 
-
 decoded_imgs = toon_net.predict(X_test, batch_size=batch_size)
-montage(X_test[:100, :, :], 'ToonNet-X')
-montage(decoded_imgs[:100, :, :], 'ToonNet-Out')
-montage(Y_test[:100, :, :], 'ToonNet-Y')
-toon_net.save('ToonNet_1.h5')
+montage(X_test[:100, :, :], '{}-X'.format(net_name))
+montage(decoded_imgs[:100, :, :], '{}-Out'.format(net_name))
+montage(Y_test[:100, :, :], '{}-Y'.format(net_name))
+toon_net.save('{}.h5'.format(net_name))

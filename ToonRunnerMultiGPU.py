819bd598a2dcef09aba798b83443f426c9208bc3
Simon Jenni <simujenni@students.unibe.ch>
2016-10-10 12:43:39 +0200

    trying to fix multi-gpu

--- ToonRunnerMultiGPU.py
+++ ToonRunnerMultiGPU.py
@@ -26,10 +26,11 @@ data = Imagenet()
 datagen = ImageDataGenerator()
 
 with tf.device('/cpu:0'):
+    K.manual_variable_initialization(True)
+
     # Load the net
     model, encoder, decoder = ToonNet(input_shape=data.dims, batch_size=batch_size, out_activation='tanh',
                                       num_res_layers=num_res_layers, merge_mode=merge_mode, f_dims=f_dims)
-    init_op = tf.initialize_all_variables()
 
 # replica 0
 with tf.device('/gpu:0'):
@@ -43,47 +44,48 @@ with tf.device('/gpu:1'):
 with tf.device('/cpu:0'):
     preds = 0.5 * (output_0 + output_1)
 
-# Name used for saving of model and outputs
-net_name = '{}-f_dims:{}-NRes:{}-Merge:{}-Loss:{}-Data:{}'.format(model.name, f_dims, num_res_layers, merge_mode,
-                                                                  loss,
-                                                                  data.name)
-print('Training network: {}'.format(net_name))
+    # Name used for saving of model and outputs
+    net_name = '{}-f_dims:{}-NRes:{}-Merge:{}-Loss:{}-Data:{}'.format(model.name, f_dims, num_res_layers, merge_mode,
+                                                                      loss,
+                                                                      data.name)
+    print('Training network: {}'.format(net_name))
 
-config = tf.ConfigProto(allow_soft_placement=True)
-with tf.Session(config=config) as sess:
-    K.set_session(sess)
+    config = tf.ConfigProto(allow_soft_placement=True)
+    with tf.Session(config=config) as sess:
+        K.set_session(sess)
+        init_op = tf.initialize_all_variables()
 
-    y = tf.placeholder(tf.float32, shape=(None,)+data.dims, name='y')
+        y = tf.placeholder(tf.float32, shape=(None,)+data.dims, name='y')
 
-    # reconstruction loss objective
-    recon_loss = tf.reduce_mean(MAE(y, preds))
+        # reconstruction loss objective
+        recon_loss = tf.reduce_mean(MAE(y, preds))
 
-    # apply regularizers if any
-    if model.regularizers:
-        total_loss = recon_loss * 1.  # copy tensor
-        for regularizer in model.regularizers:
-            total_loss = regularizer(total_loss)
-    else:
-        total_loss = recon_loss
+        # apply regularizers if any
+        if model.regularizers:
+            total_loss = recon_loss * 1.  # copy tensor
+            for regularizer in model.regularizers:
+                total_loss = regularizer(total_loss)
+        else:
+            total_loss = recon_loss
 
-    # set up TF optimizer
-    optimizer = tf.train.AdamOptimizer(0.0005)
-    train_step = optimizer.minimize(total_loss)
-    sess.run(init_op)
+        # set up TF optimizer
+        optimizer = tf.train.AdamOptimizer(0.0005)
+        train_step = optimizer.minimize(total_loss)
+        sess.run(init_op)
 
-    step = 0
-    for epoch in range(nb_epoch):
-        print("Epoch {} / {}".format(epoch + 1, nb_epoch))
-        for X_batch, Y_batch in datagen.flow_from_directory(data.train_dir, batch_size=batch_size):
-            feed_dict = {model.input: X_batch,
-                         y: Y_batch,
-                         K.learning_phase(): 1}
-            _, train_loss = sess.run([train_step, total_loss],
-                                           feed_dict=feed_dict)
-            step += 1
-        print("Step: %d," % step,
-              " Epoch: %2d," % (epoch + 1),
-              " Cost: %.4f," % train_loss)
+        step = 0
+        for epoch in range(nb_epoch):
+            print("Epoch {} / {}".format(epoch + 1, nb_epoch))
+            for X_batch, Y_batch in datagen.flow_from_directory(data.train_dir, batch_size=batch_size):
+                feed_dict = {model.input: X_batch,
+                             y: Y_batch,
+                             K.learning_phase(): 1}
+                _, train_loss = sess.run([train_step, total_loss],
+                                               feed_dict=feed_dict)
+                step += 1
+            print("Step: %d," % step,
+                  " Epoch: %2d," % (epoch + 1),
+                  " Cost: %.4f," % train_loss)
 
 
 # Generate montage of sample-images

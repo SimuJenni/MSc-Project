76b2567a965cd70270471b47d924a1924dea49f6
Simon Jenni <simujenni@students.unibe.ch>
2016-10-10 11:54:48 +0200

    trying multi-gpu

--- ToonRunner.py
+++ ToonRunner.py
@@ -13,7 +13,7 @@ from utils import montage
 
 batch_size = 32
 nb_epoch = 10
-samples_per_epoch = 200000
+samples_per_epoch = 20000
 plot_while_train = True
 f_dims = [64, 96, 160, 256, 512]
 num_res_layers = 8
@@ -35,9 +35,28 @@ net_name = '{}-f_dims:{}-NRes:{}-Merge:{}-Loss:{}-Data:{}'.format(toon_net.name,
 print('Training network: {}'.format(net_name))
 
 # Define objective and solver
-opt = Adam(lr=0.0002, beta_1=0.5)
+opt = Adam(lr=0.0005, beta_1=0.75)
 toon_net.compile(optimizer=opt, loss=loss)
 
+datagen = ImageDataGenerator()
+
+# Training
+toon_net.fit_generator(datagen.flow_from_directory(data.train_dir, batch_size=batch_size),
+                       samples_per_epoch=samples_per_epoch,
+                       nb_epoch=nb_epoch,
+                       validation_data=datagen.flow_from_directory(data.val_dir, batch_size=batch_size),
+                       nb_val_samples=3200,
+                       nb_worker=4,
+                       callbacks=[ModelCheckpoint(os.path.join(MODEL_DIR, '{}.hdf5'.format(net_name))),
+                                  TensorBoard(log_dir=LOG_DIR)])
+
+# Generate montage of sample-images
+sample_size = 64
+X_test, Y_test = data.get_sample_dir(sample_size)
+decoded_imgs = toon_net.predict(X_test, batch_size=batch_size)
+montage(decoded_imgs[:sample_size, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-Out'.format(net_name)))
+montage(X_test[:sample_size, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-X'.format(net_name)))
+montage(Y_test[:sample_size, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-Y'.format(net_name)))
 
 """
 
@@ -70,24 +89,3 @@ montage(X_test[:49, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-X'.format(net_n
 montage(Y_test[:49, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-Y'.format(net_name)))
 
 """
-
-datagen = ImageDataGenerator()
-
-# Training
-toon_net.fit_generator(datagen.flow_from_directory(data.train_dir, batch_size=batch_size),
-                       samples_per_epoch=samples_per_epoch,
-                       nb_epoch=nb_epoch,
-                       validation_data=datagen.flow_from_directory(data.val_dir, batch_size=batch_size),
-                       nb_val_samples=30000,
-                       nb_worker=4,
-                       callbacks=[ModelCheckpoint(os.path.join(MODEL_DIR, '{}.hdf5'.format(net_name))),
-                                  TensorBoard(log_dir=LOG_DIR)])
-
-# Generate montage of sample-images
-sample_size = 49
-X_test, Y_test = data.get_sample_dir(sample_size)
-decoded_imgs = toon_net.predict(X_test, batch_size=batch_size)
-montage(decoded_imgs[:sample_size, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-Out'.format(net_name)))
-montage(X_test[:sample_size, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-X'.format(net_name)))
-montage(Y_test[:sample_size, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-Y'.format(net_name)))
-
---
+++ ToonRunnerMultiGPU.py
@@ -0,0 +1,94 @@
+import os
+from DataGenerator import ImageDataGenerator
+
+from keras.objectives import MAE
+
+from ToonNet import ToonNet
+from constants import MODEL_DIR, IMG_DIR, LOG_DIR
+from datasets.TinyImagenet import TinyImagenet
+from datasets.Imagenet import Imagenet
+from utils import montage
+
+import tensorflow as tf
+sess = tf.Session()
+
+from keras import backend as K
+K.set_session(sess)
+
+batch_size = 32
+nb_epoch = 10
+samples_per_epoch = 20000
+plot_while_train = True
+f_dims = [64, 96, 160, 256, 512]
+num_res_layers = 8
+merge_mode = 'sum'
+loss = 'mae'
+
+# Get the data-set object
+data = Imagenet()
+datagen = ImageDataGenerator()
+
+with tf.device('/cpu:0'):
+    x = tf.placeholder(tf.float32, shape=(None,)+data.dims)
+    # Load the net
+    model, encoder, decoder = ToonNet(input_shape=data.dims, batch_size=batch_size, out_activation='tanh',
+                                      num_res_layers=num_res_layers, merge_mode=merge_mode, f_dims=f_dims)
+    model.summary()
+
+# replica 0
+with tf.device('/gpu:0'):
+    output_0 = model(x)  # all ops in the replica will live on GPU:0
+
+# replica 1
+with tf.device('/gpu:1'):
+    output_1 = model(x)  # all ops in the replica will live on GPU:1
+
+# merge outputs on CPU
+with tf.device('/cpu:0'):
+    preds = 0.5 * (output_0 + output_1)
+
+# Name used for saving of model and outputs
+net_name = '{}-f_dims:{}-NRes:{}-Merge:{}-Loss:{}-Data:{}'.format(model.name, f_dims, num_res_layers, merge_mode,
+                                                                  loss,
+                                                                  data.name)
+print('Training network: {}'.format(net_name))
+
+y = tf.placeholder(tf.float32, shape=(None,)+data.dims, name='y')
+
+# reconstruction loss objective
+recon_loss = tf.reduce_mean(MAE(y, preds))
+
+# apply regularizers if any
+if model.regularizers:
+    total_loss = recon_loss * 1.  # copy tensor
+    for regularizer in model.regularizers:
+        total_loss = regularizer(total_loss)
+else:
+    total_loss = recon_loss
+
+# set up TF optimizer
+optimizer = tf.train.AdamOptimizer(0.0005)
+train_step = optimizer.minimize(total_loss)
+
+step = 0
+for epoch in range(nb_epoch):
+    print("Epoch {} / {}".format(epoch + 1, nb_epoch))
+    for X_batch, Y_batch in datagen.flow_from_directory(data.train_dir, batch_size=batch_size):
+        feed_dict = {model.inputs[0]: X_batch,
+                     y: Y_batch,
+                     K.learning_phase(): 1}
+        _, train_loss = sess.run([train_step, total_loss],
+                                       feed_dict=feed_dict)
+        step += 1
+    print("Step: %d," % step,
+          " Epoch: %2d," % (epoch + 1),
+          " Cost: %.4f," % train_loss)
+
+
+# Generate montage of sample-images
+sample_size = 64
+X_test, Y_test = data.get_sample_dir(sample_size)
+decoded_imgs = model.predict(X_test, batch_size=batch_size)
+montage(decoded_imgs[:sample_size, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-Out'.format(net_name)))
+montage(X_test[:sample_size, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-X'.format(net_name)))
+montage(Y_test[:sample_size, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, '{}-Y'.format(net_name)))

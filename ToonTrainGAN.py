cfa7e0a213a22353a616c1ce227b8746cfa1ff3a
Simon Jenni <simujenni@students.unibe.ch>
2016-10-12 15:51:40 +0200

    Testing output

--- ToonTrainGAN.py
+++ ToonTrainGAN.py
@@ -64,8 +64,8 @@ for epoch in range(nb_epoch):
         if not chunk % 10:
             toonDisc.save_weights(os.path.join(MODEL_DIR, 'ToonDisc-Epoch:{}-Chunk:{}.hdf5'.format(epoch, chunk)))
             toonAE.save_weights(os.path.join(MODEL_DIR, 'ToonAE-Epoch:{}-Chunk:{}.hdf5'.format(epoch, chunk)))
-            decoded_imgs = toonAE.predict(X_train[:49], batch_size=batch_size)
-            montage(decoded_imgs[:49, :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, 'GAN-Epoch:{}-Chunk:{}'.format(epoch, chunk)))
+            decoded_imgs = toonAE.predict(X_train[:(2*batch_size)], batch_size=batch_size)
+            montage(decoded_imgs[:(2*batch_size), :, :] * 0.5 + 0.5, os.path.join(IMG_DIR, 'GAN-Epoch:{}-Chunk:{}'.format(epoch, chunk)))
 
         del X_train, Y_train, X_disc, Y_pred, y
         gc.collect()
---
+++ test_montage.py
@@ -0,0 +1,23 @@
+from DataGenerator import ImageDataGenerator
+from ToonNet import ToonAE
+from datasets.Imagenet import Imagenet
+from utils import montage
+
+batch_size = 32
+
+# Get the data-set object
+data = Imagenet()
+datagen = ImageDataGenerator(horizontal_flip=True)
+
+# Load the auto-encoder
+toonAE = ToonAE(input_shape=data.dims, batch_size=batch_size)
+toonAE.load_weights('/home/sj09l405/MSc-Project/ToonAEGAN.hdf5')
+toonAE.compile(optimizer='adam', loss='mae')
+
+# Generate montage of sample-images
+sample_size = 64
+X_test, Y_test = datagen.flow_from_directory(data.train_dir, batch_size=sample_size).next()
+decoded_imgs = toonAE.predict(X_test, batch_size=batch_size)
+montage(decoded_imgs[:sample_size, :, :] * 0.5 + 0.5, 'Test-Out.jpeg')
+montage(X_test[:sample_size, :, :] * 0.5 + 0.5, 'Test-X.jpeg')
+montage(Y_test[:sample_size, :, :] * 0.5 + 0.5, 'Test-Y.jpeg')

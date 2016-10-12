320e96ef7c3055fe30bc492830fa269680cdb8f3
Simon Jenni <simujenni@students.unibe.ch>
2016-10-12 23:15:51 +0200

    alternative GAN training

--- ToonTrainGAN2.py
+++ ToonTrainGAN2.py
@@ -3,6 +3,7 @@ import os
 
 import numpy as np
 import tensorflow as tf
+from keras.backend import switch
 from keras.layers import Input, merge, Lambda
 from keras.models import Model
 from keras.optimizers import Adam
@@ -10,7 +11,7 @@ from keras.optimizers import Adam
 from DataGenerator import ImageDataGenerator
 from ToonNet import ToonAE, ToonDiscriminator
 from constants import MODEL_DIR, IMG_DIR
-from datasets.Imagenet import Imagenet
+from datasets.TinyImagenet import TinyImagenet
 from utils import montage
 
 batch_size = 32
@@ -18,7 +19,7 @@ chunk_size = 200 * batch_size
 nb_epoch = 2
 
 # Get the data-set object
-data = Imagenet()
+data = TinyImagenet()
 datagen = ImageDataGenerator()
 
 # Define optimizer
@@ -26,24 +27,28 @@ opt = Adam(lr=0.0002, beta_1=0.5)
 
 # Load the auto-encoder
 toonAE = ToonAE(input_shape=data.dims, batch_size=batch_size)
-#toonAE.load_weights('/home/sj09l405/MSc-Project/ToonAE.hdf5')
-toonAE.compile(optimizer=opt, loss='binary_crossentropy')
+# toonAE.load_weights('/home/sj09l405/MSc-Project/ToonAE.hdf5')
+toonAE.compile(optimizer=opt, loss='mae')
 
 # Load the discriminator
-disc_in_dim = (data.dims(0), data.dims(1), 6)
+disc_in_dim = (data.dims[0], data.dims[1], 6)
 toonDisc = ToonDiscriminator(input_shape=disc_in_dim)
-#toonDisc.load_weights('/home/sj09l405/MSc-Project/ToonDisc.hdf5')
+# toonDisc.load_weights('/home/sj09l405/MSc-Project/ToonDisc.hdf5')
 toonDisc.compile(optimizer=opt, loss='binary_crossentropy')
 
 # Stick them together
 X_input = Input(shape=data.dims)
 Y_input = Input(shape=data.dims)
-order_input = Input(shape=(1,))
+order_input = Input(batch_shape=(1,), dtype='int32')
 im_recon = toonAE(X_input)
-disc_in = Lambda(lambda x: tf.cond(order_input == 0,
-                                   tf.concat(0, [x, Y_input]),
-                                   tf.concat(0, [Y_input, x])),
-                 output_shape=disc_in_dim)(im_recon)
+
+
+def concat_order(x):
+    return switch(tf.reshape(order_input, []), merge([x, Y_input], mode='concat'),
+                  merge([Y_input, x], mode='concat'))
+
+
+disc_in = Lambda(concat_order)(im_recon)
 im_class = toonDisc(disc_in)
 toonGAN = Model([X_input, Y_input, order_input], im_class)
 toonGAN.compile(optimizer=opt, loss='binary_crossentropy')
@@ -53,7 +58,6 @@ for epoch in range(nb_epoch):
     print('Epoch: {}/{}'.format(epoch, nb_epoch))
     chunk = 0
     for X_train, Y_train in datagen.flow_from_directory(data.train_dir, batch_size=chunk_size):
-
 
         # Construct data for training
         y_disc = [1] * len(X_train) + [0] * len(Y_train)

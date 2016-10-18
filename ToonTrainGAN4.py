211d56a71fd5ca302cd213557c9d2274274ab784
Simon Jenni <simujenni@students.unibe.ch>
2016-10-18 08:22:19 +0200

    Try mode0 again

--- ToonTrainGAN4.py
+++ ToonTrainGAN4.py
@@ -46,7 +46,7 @@ disc_in_dim = data.dims[:2] + (6,)
 toonDisc = ToonDiscriminator2(input_shape=disc_in_dim)
 
 try:
-    toonDisc.load_weights(os.path.join(MODEL_DIR, 'ToonDisc_m2_converge.hdf5'))
+    toonDisc.load_weights(os.path.join(MODEL_DIR, 'ToonDisc_m0_converge.hdf5'))
     toonDisc.compile(optimizer=opt, loss='binary_crossentropy')
     toonDisc.summary()
 
@@ -94,7 +94,7 @@ except Exception:
                 training_done = True
                 break
 
-    toonDisc.save_weights(os.path.join(MODEL_DIR, 'ToonDisc_m2_converge.hdf5'))
+    toonDisc.save_weights(os.path.join(MODEL_DIR, 'ToonDisc_m0_converge.hdf5'))
 
 # Stick them together
 make_trainable(toonDisc, False)

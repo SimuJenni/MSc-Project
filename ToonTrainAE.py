7458b58bf0b414f665026977d94fa853c153cd85
Simon Jenni <simujenni@students.unibe.ch>
2016-10-19 12:52:52 +0200

    Ready for GAN training

--- ToonTrainAE.py
+++ ToonTrainAE.py
@@ -17,7 +17,7 @@ data = Imagenet()
 datagen = ImageDataGenerator()
 
 # Load the net
-generator = Generator(input_shape=data.dims, batch_size=batch_size, load_weights=True)
+generator = Generator(input_shape=data.dims, batch_size=batch_size, load_weights=False)
 generator.summary()
 
 # Name used for saving of model and outputs

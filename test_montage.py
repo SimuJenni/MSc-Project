2c62a8e94f5d3449efaec472c12af7c7f59d4d35
Simon Jenni <simujenni@students.unibe.ch>
2016-10-25 08:11:07 +0200

    Refactoring

--- test_montage.py
+++ test_montage.py
@@ -17,7 +17,7 @@ generator.load_weights(os.path.join(MODEL_DIR, '{}.hdf5'.format('EncGenTrain')))
 
 # Generate montage of sample-images
 sample_size = 64
-X_test, Y_test = datagen.flow_from_directory(data.train_dir, batch_size=sample_size, xy_fun=Y2X_Y2Y).next()
+X_test, Y_test = datagen.flow_from_directory(data.train_dir, batch_size=sample_size, xy_fun=Y2X_Y2Y, target_size=data.target_size).next()
 decoded_imgs = generator.predict(X_test, batch_size=batch_size)
 montage(decoded_imgs[:sample_size, :, :] * 0.5 + 0.5, 'Test-Out.jpeg')
 montage(X_test[:sample_size, :, :] * 0.5 + 0.5, 'Test-X.jpeg')

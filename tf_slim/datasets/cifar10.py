9b645ad4531cfbc27adc106bad1d580cec8f741d
Simon Jenni <simujenni@students.unibe.ch>
2016-11-03 19:56:43 +0100

    CIFAR10 fine tuning

--- tf_slim/datasets/cifar10.py
+++ tf_slim/datasets/cifar10.py
@@ -91,5 +91,4 @@ def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
         decoder=decoder,
         num_samples=SPLITS_TO_SIZES[split_name],
         items_to_descriptions=_ITEMS_TO_DESCRIPTIONS,
-        num_classes=_NUM_CLASSES,
-        labels_to_names=labels_to_names)
+        num_classes=_NUM_CLASSES)

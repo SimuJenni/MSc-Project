3791baab689ca720d097cf3946b9814250378030
Simon Jenni <simujenni@students.unibe.ch>
2016-11-03 21:05:00 +0100

    CIFAR10 fine tuning

--- fine_tune_cifar10.py
+++ fine_tune_cifar10.py
@@ -14,64 +14,68 @@ NUM_CLASSES = 10
 log_dir = LOG_DIR
 load_weights_tf = False
 
+sess = tf.Session()
 import keras.backend as K
+K.set_session(sess)
 model = Classifier((32, 32, 3), num_classes=NUM_CLASSES, num_layers=3, fine_tune=True)
+
+
 g = K.get_session().graph
-
-
 # g = tf.Graph()
-with g.as_default():
 
-    # Selects the 'validation' dataset.
-    dataset = cifar10.get_split('train', DATA_DIR)
+with sess.as_default():
+    with g.as_default():
 
-    # Creates a TF-Slim DataProvider which reads the dataset in the background
-    # during both training and testing.
-    provider = slim.dataset_data_provider.DatasetDataProvider(
-        dataset,
-        num_readers=4,
-        common_queue_capacity=20 * BATCH_SIZE,
-        common_queue_min=10 * BATCH_SIZE)
+        # Selects the 'validation' dataset.
+        dataset = cifar10.get_split('train', DATA_DIR)
 
-    [image, label] = provider.get(['image', 'label'])
+        # Creates a TF-Slim DataProvider which reads the dataset in the background
+        # during both training and testing.
+        provider = slim.dataset_data_provider.DatasetDataProvider(
+            dataset,
+            num_readers=4,
+            common_queue_capacity=20 * BATCH_SIZE,
+            common_queue_min=10 * BATCH_SIZE)
 
-    # Preprocess images
-    image = (tf.to_float(image) / 255. - 0.5) * 2.0
+        [image, label] = provider.get(['image', 'label'])
 
-    images, labels = tf.train.batch(
-        [image, label],
-        batch_size=BATCH_SIZE,
-        num_threads=4,
-        capacity=5 * BATCH_SIZE)
+        # Preprocess images
+        image = (tf.to_float(image) / 255. - 0.5) * 2.0
 
-    labels = slim.one_hot_encoding(labels, NUM_CLASSES)
+        images, labels = tf.train.batch(
+            [image, label],
+            batch_size=BATCH_SIZE,
+            num_threads=4,
+            capacity=5 * BATCH_SIZE)
 
-    # TODO: Create the model
-    # predictions = myModel(images, is_training=True)
-    predictions = model(images)
+        labels = slim.one_hot_encoding(labels, NUM_CLASSES)
 
-    # Define the loss
-    slim.losses.softmax_cross_entropy(predictions, labels)
-    total_loss = slim.losses.get_total_loss()
-    tf.scalar_summary('losses/total loss', total_loss)
+        # TODO: Create the model
+        # predictions = myModel(images, is_training=True)
+        predictions = model(images)
 
-    # Define optimizer
-    optimizer = tf.train.AdamOptimizer(learning_rate=0.0002)
+        # Define the loss
+        slim.losses.softmax_cross_entropy(predictions, labels)
+        total_loss = slim.losses.get_total_loss()
+        tf.scalar_summary('losses/total loss', total_loss)
 
-    # Create training operation
-    train_op = slim.learning.create_train_op(total_loss, optimizer)
+        # Define optimizer
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.0002)
 
-    if load_weights_tf:
-        # TODO: Specify where the Model, trained on ImageNet, was saved.
-        model_path = '/path/to/pre_trained_model.checkpoint'
+        # Create training operation
+        train_op = slim.learning.create_train_op(total_loss, optimizer)
 
-        # TODO: Specify the layers of your model you want to exclude
-        from tensorflow.contrib.framework import assign_from_checkpoint_fn
-        variables_to_restore = slim.get_variables_to_restore(exclude=['fc6', 'fc7', 'fc8'])
-        init_fn = assign_from_checkpoint_fn(model_path, variables_to_restore)
+        if load_weights_tf:
+            # TODO: Specify where the Model, trained on ImageNet, was saved.
+            model_path = '/path/to/pre_trained_model.checkpoint'
 
-        # Start training.
-        slim.learning.train(train_op, log_dir, init_fn=init_fn)
+            # TODO: Specify the layers of your model you want to exclude
+            from tensorflow.contrib.framework import assign_from_checkpoint_fn
+            variables_to_restore = slim.get_variables_to_restore(exclude=['fc6', 'fc7', 'fc8'])
+            init_fn = assign_from_checkpoint_fn(model_path, variables_to_restore)
 
-    else:
-        slim.learning.train(train_op, log_dir)
+            # Start training.
+            slim.learning.train(train_op, log_dir, init_fn=init_fn)
+
+        else:
+            slim.learning.train(train_op, log_dir, init_feed_dict={K.learning_phase(): 1})

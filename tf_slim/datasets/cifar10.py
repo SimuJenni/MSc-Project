f2017eb90a490870cad8761a2cc7b055ca622e24
Simon Jenni <simujenni@students.unibe.ch>
2016-11-03 17:24:48 +0100

    CIFAR10 fine tuning

--- tf_slim/datasets/cifar10.py
+++ tf_slim/datasets/cifar10.py
@@ -84,9 +84,6 @@ def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
     labels_to_names = None
     if dataset_utils.has_labels(dataset_dir):
         labels_to_names = dataset_utils.read_label_file(dataset_dir)
-    print(labels_to_names)
-    print(dataset_dir)
-
 
     return slim.dataset.Dataset(
         data_sources=file_pattern,

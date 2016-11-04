7627fff3e552fc6a4613c72c83064f5aa20d2f00
Simon Jenni <simujenni@students.unibe.ch>
2016-11-04 09:47:39 +0100

    Disctrain

--- ToonTrainDisc.py
+++ ToonTrainDisc.py
@@ -10,13 +10,15 @@ from utils import montage
 # Training parameters
 batch_size = 200
 nb_epoch = 10
+num_layers = 3
+num_res = 4
 
 # Get the data-set object
 data = CIFAR10_Toon()
 datagen = ImageDataGenerator()
 
 # Load the net
-discriminator = Discriminator(input_shape=data.dims, num_layers=3)
+discriminator = Discriminator(input_shape=data.dims, num_layers=num_layers, num_res=num_res)
 discriminator.summary()
 
 # Name used for saving of model and outputs

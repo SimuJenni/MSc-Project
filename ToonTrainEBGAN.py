ef3dd98ff97259234e84d2d3123b6b590cd4ff86
Simon Jenni <simujenni@students.unibe.ch>
2016-11-04 11:23:18 +0100

    EBGAN+res

--- ToonTrainEBGAN.py
+++ ToonTrainEBGAN.py
@@ -22,7 +22,7 @@ batch_size = 200
 chunk_size = 10 * batch_size
 num_chunks = data.num_train // chunk_size
 nb_epoch = 20
-r_weight = 10.0
+r_weight = 2.0
 
 # Load the models
 generator = Generator(input_shape=data.dims, num_layers=num_layers, batch_size=batch_size, num_res=num_res)

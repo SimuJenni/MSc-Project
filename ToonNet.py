1e4a8782c91bf315c9bd082b9d188fee84fb62b7
Simon Jenni <simujenni@students.unibe.ch>
2016-11-05 23:10:16 +0100

    EBGAN2

--- ToonNet.py
+++ ToonNet.py
@@ -588,8 +588,8 @@ def EBGAN2(input_shape, batch_size=128, load_weights=False, num_layers_g=4, num_
     _, ge_y = generator(y_input)
     d_g_x, de_g_x = discriminator(g_x)
     d_y, de_y = discriminator(y_input)
-    # l1 = sub(de_g_x, de_y)
-    l1 = sub(d_g_x, d_y)
+    l1 = sub(de_g_x, de_y)
+    # l1 = sub(d_g_x, d_y)
     l2 = sub(d_g_x, g_x)
     if train_disc:
         l3 = sub(d_y, y_input)
@@ -598,7 +598,7 @@ def EBGAN2(input_shape, batch_size=128, load_weights=False, num_layers_g=4, num_
         gan.name = make_name('dGAN2', num_layers=[num_layers_d, num_layers_g], num_res=num_res, r_weight=r_weight,
                              d_weight=d_weight)
     else:
-        l4 = sub(ge_y, ge_x)
+        l4 = sub(g_x, y_input)
         gan = Model(input=[x_input, y_input], output=[l1, l2, l4])
         gan.compile(loss=[l2_loss, mse, l2_loss], loss_weights=[d_weight, 1.0, 1.0], optimizer=optimizer)
         gan.name = make_name('gGAN2', num_layers=[num_layers_d, num_layers_g], num_res=num_res, r_weight=r_weight,

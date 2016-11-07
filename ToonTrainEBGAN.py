fdb1676c4816cfb515d8d7491e262ba6b9613af9
Simon Jenni <simujenni@students.unibe.ch>
2016-11-07 17:15:31 +0100

    EBGAN2

--- ToonTrainEBGAN.py
+++ ToonTrainEBGAN.py
@@ -13,7 +13,7 @@ from datasets import TinyImagenetToon, CIFAR10_Toon
 from utils import montage, generator_queue
 
 # Get the data-set object
-data = TinyImagenetToon()
+data = CIFAR10_Toon()
 datagen = ImageDataGenerator(rotation_range=10,
                              width_shift_range=0.05,
                              height_shift_range=0.05,
@@ -23,7 +23,7 @@ datagen = ImageDataGenerator(rotation_range=10,
                              fill_mode='nearest')
 
 # Training parameters
-num_layers = 4
+num_layers = 3
 num_res = 0
 batch_size = 100
 chunk_size = 5 * batch_size

6d4c85f2542bb0d48b46304921aeb81fef7dcc19
Simon Jenni <simujenni@students.unibe.ch>
2016-11-07 01:34:53 +0100

    EBGAN2

--- ToonNet.py
+++ ToonNet.py
@@ -444,7 +444,7 @@ def l2_mb(y_true, y_pred):
 
 
 def l2_ms(y_true, y_pred):
-    return K.mean(K.square(y_pred) - 0.1, axis=-1)
+    return -K.mean(K.maximum(0.2 - K.square(y_pred), 0.0), axis=-1)
 
 
 def Classifier(input_shape, batch_size=128, num_layers=4, num_res=0, num_classes=1000, net_load_name=None,
@@ -603,13 +603,13 @@ def EBGAN2(input_shape, batch_size=128, load_weights=False, num_layers_g=4, num_
     if train_disc:
         l3 = sub(d_y, y_input)
         gan = Model(input=[x_input, y_input], output=[l1, l2, l3])
-        gan.compile(loss=[l2_mb, l2_ms, l2_loss], loss_weights=[-1.0, -d_weight, d_weight], optimizer=optimizer)
+        gan.compile(loss=[mae, l2_ms, l2_loss], loss_weights=[-1.0, -d_weight, d_weight], optimizer=optimizer)
         gan.name = make_name('dGAN2', num_layers=[num_layers_d, num_layers_g], num_res=num_res, r_weight=r_weight,
                              d_weight=d_weight)
     else:
         l4 = sub(g_x, y_input)
         gan = Model(input=[x_input, y_input], output=[l1, l2, l4])
-        gan.compile(loss=[l2_mb, l2_ms, l2_loss], loss_weights=[1.0, d_weight, r_weight], optimizer=optimizer)
+        gan.compile(loss=[mae, l2_ms, l2_loss], loss_weights=[1.0, d_weight, r_weight], optimizer=optimizer)
         gan.name = make_name('gGAN2', num_layers=[num_layers_d, num_layers_g], num_res=num_res, r_weight=r_weight,
                              d_weight=d_weight)
 

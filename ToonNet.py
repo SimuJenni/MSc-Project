bb4d8909ad609503e399e75d2e58980cd1ba4429
Simon Jenni <simujenni@students.unibe.ch>
2016-11-07 01:39:31 +0100

    EBGAN2

--- ToonNet.py
+++ ToonNet.py
@@ -440,11 +440,11 @@ def l2_loss(y_true, y_pred):
 
 
 def l2_mb(y_true, y_pred):
-    return K.mean(K.maximum(K.abs(y_pred) - 10.0, 0.0), axis=-1)
+    return K.mean(K.maximum(K.abs(y_pred), 10.0), axis=-1)
 
 
 def l2_ms(y_true, y_pred):
-    return -K.mean(K.maximum(0.2 - K.square(y_pred), 0.0), axis=-1)
+    return K.mean(K.maximum(K.square(y_pred), 0.2), axis=-1)
 
 
 def Classifier(input_shape, batch_size=128, num_layers=4, num_res=0, num_classes=1000, net_load_name=None,

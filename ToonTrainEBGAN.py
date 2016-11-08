db70c1e4c3347ea010758597bef7ddae49d8513d
Simon Jenni <simujenni@students.unibe.ch>
2016-11-08 09:23:28 +0100

    EBGAN2

--- ToonTrainEBGAN.py
+++ ToonTrainEBGAN.py
@@ -116,7 +116,7 @@ for epoch in range(nb_epoch):
         t_loss = h.history['loss'][0]
         l1 = h.history['{}_loss'.format(gGAN.output_names[0])][0]
         l3 = h.history['{}_loss'.format(gGAN.output_names[1])][0]
-        l4 = h.history['{}_loss'.format(gGAN.output_names[4])][0]
+        l4 = h.history['{}_loss'.format(gGAN.output_names[2])][0]
 
         # Record and print loss
         print('Loss: {} L_1: {} L_2: {} L_3: {} L_4: {}'.format(t_loss, l1, l2, l3, l4))

7cf371ced57eb42157e56a6849ecb802737216ec
Simon Jenni <simujenni@students.unibe.ch>
2016-11-08 16:33:12 +0100

    EBGAN2

--- ToonNet.py
+++ ToonNet.py
@@ -624,6 +624,7 @@ def EBGAN2(input_shape, batch_size=128, load_weights=False, num_layers_g=4, num_
         gan.name = make_name('dGAN2', num_layers=[num_layers_d, num_layers_g], num_res=num_res, r_weight=r_weight,
                              d_weight=d_weight)
     else:
+        make_trainable(class_net, False)
         gan = Model(input=[x_input, y_input], output=[l1, de_g_x])
         gan.compile(loss=[l2_loss, disc_loss_d2], loss_weights=[r_weight, d_weight], optimizer=optimizer)
         gan.name = make_name('gGAN2', num_layers=[num_layers_d, num_layers_g], num_res=num_res, r_weight=r_weight,

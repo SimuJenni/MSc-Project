cd0e0bc5d24f8089df33f04e8418d2575352c4dd
Simon Jenni <simujenni@students.unibe.ch>
2016-11-08 06:28:25 +0100

    EBGAN2

--- ToonTrainEBGAN.py
+++ ToonTrainEBGAN.py
@@ -29,8 +29,8 @@ batch_size = 100
 chunk_size = 5 * batch_size
 num_chunks = data.num_train // chunk_size
 nb_epoch = 20
-r_weight = 5.0
-d_weight = 0.5
+r_weight = 10.0
+d_weight = 2.0
 load_weights = False
 noise = K.variable(value=0.1, name='sigma')
 noise = None

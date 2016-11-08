b0be49ad317e7c958229d1974e181d3dac2da22a
Simon Jenni <simujenni@students.unibe.ch>
2016-11-08 20:15:21 +0100

    EBGAN2

--- ToonNet.py
+++ ToonNet.py
@@ -443,7 +443,7 @@ def l2_mb(y_true, y_pred):  # Idea: could pass in margin during training (simila
 
 
 def l2_ms(y_true, y_pred):
-    return -K.mean(K.maximum(0.1-K.square(y_pred), 0), axis=-1)
+    return -K.mean(K.maximum(0.5-K.square(y_pred), 0), axis=-1)
 
 
 def disc_loss_g(y_true, y_pred):
@@ -621,13 +621,13 @@ def EBGAN2(input_shape, batch_size=128, load_weights=False, num_layers_g=4, num_
     l2 = sub(d_y, y_input)
     if train_disc:
         gan = Model(input=[x_input, y_input], output=[l2, de_g_x, de_y])
-        gan.compile(loss=[l2_loss, disc_loss_d1, disc_loss_d2], loss_weights=[r_weight, d_weight, d_weight], optimizer=optimizer)
+        gan.compile(loss=[l2_loss, l2_loss, l2_loss], loss_weights=[r_weight, d_weight, -d_weight], optimizer=optimizer)
         gan.name = make_name('dGAN2', num_layers=[num_layers_d, num_layers_g], num_res=num_res, r_weight=r_weight,
                              d_weight=d_weight)
     else:
         make_trainable(class_net, False)
         gan = Model(input=[x_input, y_input], output=[l1, de_g_x])
-        gan.compile(loss=[l2_loss, disc_loss_g], loss_weights=[r_weight, d_weight], optimizer=optimizer)
+        gan.compile(loss=[l2_loss, l2_ms], loss_weights=[r_weight, -d_weight], optimizer=optimizer)
         gan.name = make_name('gGAN2', num_layers=[num_layers_d, num_layers_g], num_res=num_res, r_weight=r_weight,
                              d_weight=d_weight)
 

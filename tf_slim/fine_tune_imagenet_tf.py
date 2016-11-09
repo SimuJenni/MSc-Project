cc8de9add2818c43f125ba2593c0601aa25849af
Simon Jenni <simujenni@students.unibe.ch>
2016-11-09 14:30:30 +0100

    Fine-tuning tf_slim

--- tf_slim/fine_tune_imagenet_tf.py
+++ tf_slim/fine_tune_imagenet_tf.py
@@ -75,7 +75,7 @@ DATA_DIR = '/data/cvg/imagenet/imagenet_tfrecords/'
 BATCH_SIZE = 128
 NUM_CLASSES = 1000
 IM_SHAPE = [128, 128, 3]
-MODEL_PATH = '/data/cvg/qhu/try_GAN/checkpoint_edge_twodis_128/033/DCGAN.model-38100'
+MODEL_PATH = '/data/cvg/qhu/try_GAN/checkpoint_edge_twodis_128/050/DCGAN.model-80100'
 LOG_DIR = '/data/cvg/simon/data/logs/ft1/'
 
 # TODO: Indicate whether to use Keras or tensorflow model

6272971b47eb5d7325a43305547bfbfaeff72e43
Simon Jenni <simujenni@students.unibe.ch>
2016-11-10 22:14:08 +0100

    fixing fine-tuning

--- tf_slim/fine_tune_imagenet_tf.py
+++ tf_slim/fine_tune_imagenet_tf.py
@@ -77,16 +77,16 @@ def get_variables_to_train(trainable_scopes=None):
     return variables_to_train
 
 
-fine_tune = True
+fine_tune = False
 DATA_DIR = '/data/cvg/imagenet/imagenet_tfrecords/'
 BATCH_SIZE = 128
 NUM_CLASSES = 1000
 IM_SHAPE = [224, 224, 3]
-IM_SHAPE = [128, 128, 3]
+#IM_SHAPE = [128, 128, 3]
 
 MODEL_PATH = '/data/cvg/qhu/try_GAN/checkpoint_edge_twodis_128/050/DCGAN.model-80100'
 LOG_DIR = '/data/cvg/simon/data/logs/alex_net/'
-LOG_DIR = '/data/cvg/simon/data/logs/fine_tune/'
+#LOG_DIR = '/data/cvg/simon/data/logs/fine_tune/'
 
 # TODO: Indicate whether to use Keras or tensorflow model
 tensorflow_model = True

8d0a470faf54ef7eb47b73650518bbc3a5c557d5
Simon Jenni <simujenni@students.unibe.ch>
2016-11-12 14:44:31 +0100

    Bug fixing

--- ToonTrainGAN.py
+++ ToonTrainGAN.py
@@ -86,7 +86,7 @@ for epoch in range(nb_epoch):
 
         # Train discriminator
         # h = dGAN.fit(x=[X_train, Y_train], y=[target]*len(dGAN.output_names), nb_epoch=1, batch_size=batch_size, verbose=0)
-        h = dGAN.fit(x=[X_train, Y_train], y=[target, np.zeros((len(X_train), 1))], nb_epoch=1, batch_size=batch_size, verbose=0)
+        h = dGAN.fit(x=[X_train, Y_train], y=[target]*2 + [np.zeros((len(X_train), 1))]*2, nb_epoch=1, batch_size=batch_size, verbose=0)
         for key, value in h.history.iteritems():
             print('{}: {}'.format(key, value))
 

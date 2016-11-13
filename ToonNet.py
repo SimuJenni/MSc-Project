665427aa37f6073e7a255a94da3b8af107d59978
Simon Jenni <simujenni@students.unibe.ch>
2016-11-13 13:23:39 +0100

    New Gan objectives

--- ToonNet.py
+++ ToonNet.py
@@ -122,7 +122,7 @@ def ToonGAN(input_shape, batch_size=128, num_layers=4, train_disc=True, load_wei
 
     if train_disc:
         gan = Model(input=[x_input, y_input], output=[d_g_x, d_y, l_feat])
-        gan.compile(loss=[ld_0, ld_1, l2_loss], loss_weights=[1.0, 1.0, -0.1], optimizer=optimizer)
+        gan.compile(loss=[ld_0, ld_1, l2_margin], loss_weights=[1.0, 1.0, -0.1], optimizer=optimizer)
         gan.name = make_name('ToonGAN_d', num_layers=num_layers)
     else:
         l_rec = sub(g_x, y_input)
@@ -574,7 +574,7 @@ def l2_loss(y_true, y_pred):
 
 
 def l2_margin(y_true, y_pred):  # Idea: could pass in margin during training (similar to noise thingie)
-    return -K.maximum(K.mean(20.0-K.square(y_pred), axis=-1), 0)
+    return -K.maximum(1.0-K.mean(K.square(y_pred), axis=-1), 0)
 
 
 def l2_ms(y_true, y_pred):

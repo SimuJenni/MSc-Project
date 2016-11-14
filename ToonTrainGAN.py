ba88c5879e3932042e4e94b7bdc535739fa21bba
Simon Jenni <simujenni@students.unibe.ch>
2016-11-14 11:41:27 +0100

    Switch back to my own bn mode 3

--- ToonTrainGAN.py
+++ ToonTrainGAN.py
@@ -33,6 +33,7 @@ load_weights = False
 
 # Load the models
 generator = Gen(input_shape=data.dims, num_layers=num_layers, batch_size=batch_size)
+generator.summary()
 dGAN, d_gen, d_disc = ToonGAN(data.dims,
                               batch_size=batch_size,
                               num_layers=num_layers,
@@ -47,8 +48,8 @@ gGAN, g_gen, g_disc = ToonGAN(data.dims,
 # Paths for storing the weights
 gen_weights = os.path.join(MODEL_DIR, '{}_resize.hdf5'.format(gGAN.name))
 disc_weights = os.path.join(MODEL_DIR, '{}_resize.hdf5'.format(dGAN.name))
-# g_disc.load_weights(disc_weights)
-# g_gen.load_weights(gen_weights)
+g_disc.load_weights(disc_weights)
+g_gen.load_weights(gen_weights)
 
 # Create test data
 toon_test, edge_test, im_test = datagen.flow_from_directory(data.val_dir, batch_size=chunk_size,

0dc7f30589e6f082beabcfad8d215eb47a70fa54
Simon Jenni <simujenni@students.unibe.ch>
2016-11-16 21:48:48 +0100

    New disc in GAN

--- ToonTrainGAN_new.py
+++ ToonTrainGAN_new.py
@@ -79,7 +79,7 @@ for epoch in range(nb_epoch):
             else:
                 time.sleep(0.05)
 
-        if train_disc or l1 < 1.1 or count_skip > 10:
+        if train_disc or l1 < 1.2 or count_skip > 24:
             # Train discriminator
             print('Epoch {}/{} Chunk {}: Training Discriminator...'.format(epoch, nb_epoch, chunk))
             # Xd_train, yd_train = disc_data(toon_train, img_train, im_pred)

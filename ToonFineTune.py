d0462dac136b0db018f20ef521859aeeb6f5699a
Simon Jenni <simujenni@students.unibe.ch>
2016-11-17 13:39:00 +0100

    Init tiny-imagenet

--- ToonFineTune.py
+++ ToonFineTune.py
@@ -1,26 +1,14 @@
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 import os
+import time
+import sys
+import gc
 
-from ToonNet import Classifier, make_name
+from ToonNet import Classifier, make_name, gen_data
 from datasets import CIFAR10, TinyImagenet
+from utils import generator_queue
 
-# Training parameters
-batch_size = 100
-nb_epoch = 20
-num_layers = 3
-num_res = 0
-r_weight = 5.0
-d_weight = 1.0
-use_gan = True
-use_gen = True
-if use_gen:
-    net_load_name = make_name('ToonGen', num_layers=num_layers)
-else:
-    net_load_name = make_name('ToonDisc', num_layers=num_layers)
-
-if use_gan:
-    net_load_name = '{}_gan'.format(net_load_name)
 
 # Get the data-set object
 data = CIFAR10()
@@ -34,6 +22,24 @@ datagen = ImageDataGenerator(
     horizontal_flip=True
 )
 
+
+# Training parameters
+num_layers = 3
+batch_size = 200
+chunk_size = 4 * batch_size
+num_chunks = data.num_train // chunk_size
+nb_epoch = 30
+
+use_gan = True
+use_gen = True
+if use_gen:
+    net_load_name = make_name('ToonGen', num_layers=num_layers)
+else:
+    net_load_name = make_name('ToonDisc', num_layers=num_layers)
+
+if use_gan:
+    net_load_name = '{}_gan'.format(net_load_name)
+
 # Load the net
 classifier = Classifier(input_shape=data.dims, num_layers=num_layers, num_res=num_res, num_classes=data.num_classes,
                         net_load_name=net_load_name, use_gen=use_gen, batch_size=batch_size)
@@ -43,17 +49,43 @@ classifier.summary()
 net_name = '{}-{}'.format(classifier.name, data.name)
 print('Training network: {}'.format(net_name))
 
-# Training
-history = classifier.fit_generator(
-    datagen.flow_from_directory(data.train_dir, target_size=data.target_size, batch_size=batch_size),
-    samples_per_epoch=data.num_train,
-    nb_epoch=nb_epoch,
-    validation_data=datagen.flow_from_directory(data.val_dir, target_size=data.target_size, batch_size=batch_size),
-    nb_val_samples=data.num_val,
-    nb_worker=2,
-    pickle_safe=True,
-    max_q_size=16)
 
+
+for epoch in range(nb_epoch):
+    print('Epoch: {}/{}'.format(epoch, nb_epoch))
+    train_disc = True
+    count_skip = 0
+
+    # Create queue for training data
+    data_gen_queue, _stop, threads = generator_queue(
+        datagen.flow_from_directory(data.train_dir, batch_size=chunk_size, target_size=data.target_size),
+        max_q_size=32,
+        nb_worker=8)
+
+    for chunk in range(num_chunks):
+
+        # Get next chunk of training data from queue
+        while not _stop.is_set():
+            if not data_gen_queue.empty():
+                img_train, labels = data_gen_queue.get()
+                break
+            else:
+                time.sleep(0.05)
+
+        if use_gen:
+            X = gen_data(img_train, np.random.normal(size=(chunk_size,)+data.dims[:2]+(1,)))
+        else:
+            X = np.concatenate((img_train, img_train), axis=3)
+
+        h = classifier.fit(x=X, y=labels, nb_epoch=1, batch_size=batch_size, verbose=0)
+        t_loss = h.history['loss'][0]
+        print('Loss: {}'.format(t_loss))
+
+        sys.stdout.flush()
+
+    _stop.set()
+    del data_gen_queue, threads
+    gc.collect()
 
 class_dirs = os.listdir(data.val_dir)
 y_true = []

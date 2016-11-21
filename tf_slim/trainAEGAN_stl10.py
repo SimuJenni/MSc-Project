12b3c03ce741e9531732fa1765597751b11681a5
Simon Jenni <simujenni@students.unibe.ch>
2016-11-21 22:31:38 +0100

    Different weights

--- tf_slim/trainAEGAN_stl10.py
+++ tf_slim/trainAEGAN_stl10.py
@@ -12,7 +12,7 @@ slim = tf.contrib.slim
 
 # Setup training parameters
 NUM_LAYERS = 5
-BATCH_SIZE = 96
+BATCH_SIZE = 128
 TARGET_SHAPE = [96, 96, 3]
 NUM_EPOCHS = 30
 LOG_DIR = '/data/cvg/simon/data/logs/stl10_aegan2/'
@@ -50,9 +50,9 @@ with sess.as_default():
                                                  capacity=8 * BATCH_SIZE)
 
         # Make labels for discriminator training
-        labels = tf.Variable(tf.concat(concat_dim=0, values=[tf.zeros(shape=(BATCH_SIZE/2,), dtype=tf.int32),
-                                                             tf.ones(shape=(BATCH_SIZE/2,), dtype=tf.int32)]))
-        labels = tf.random_shuffle(labels)
+        labels = tf.Variable(tf.concat(concat_dim=0, values=[tf.zeros(shape=(BATCH_SIZE,), dtype=tf.int32),
+                                                             tf.ones(shape=(BATCH_SIZE,), dtype=tf.int32)]))
+        # labels = tf.random_shuffle(labels)
         labels_disc = slim.one_hot_encoding(labels, 2)
         labels_gen = slim.one_hot_encoding(tf.ones_like(labels) - labels, 2)
 
@@ -63,24 +63,24 @@ with sess.as_default():
 
         # Define loss for discriminator training
         disc_loss_scope = 'disc_loss'
-        dL_disc = slim.losses.sigmoid_cross_entropy(disc_out, labels_disc, scope=disc_loss_scope, weight=0.2)
+        dL_disc = slim.losses.sigmoid_cross_entropy(disc_out, labels_disc, scope=disc_loss_scope, weight=0.01)
         losses_disc = slim.losses.get_losses(disc_loss_scope)
         losses_disc += slim.losses.get_regularization_losses(disc_loss_scope)
         disc_loss = math_ops.add_n(losses_disc, name='disc_total_loss')
 
         # Define the losses for AE training
         ae_loss_scope = 'ae_loss'
-        dL_ae = slim.losses.sigmoid_cross_entropy(disc_out, labels_disc, scope=ae_loss_scope, weight=0.2)
-        l2_ae = slim.losses.sum_of_squares(img_rec, images, scope=ae_loss_scope, weight=50.0)
+        dL_ae = slim.losses.sigmoid_cross_entropy(disc_out, labels_disc, scope=ae_loss_scope, weight=0.01)
+        l2_ae = slim.losses.sum_of_squares(img_rec, images, scope=ae_loss_scope, weight=5.0)
         losses_ae = slim.losses.get_losses(ae_loss_scope)
         losses_ae += slim.losses.get_regularization_losses(ae_loss_scope)
         ae_loss = math_ops.add_n(losses_ae, name='ae_total_loss')
 
         # Define the losses for generator training
         gen_loss_scope = 'gen_loss'
-        dL_gen = slim.losses.sigmoid_cross_entropy(disc_out, labels_gen, scope=gen_loss_scope, weight=1.0)
-        l2_gen = slim.losses.sum_of_squares(gen_rec, images, scope=gen_loss_scope, weight=20.0)
-        l2feat_gen = slim.losses.sum_of_squares(gen_enc, enc_im, scope=gen_loss_scope, weight=2.0)
+        dL_gen = slim.losses.sigmoid_cross_entropy(disc_out, labels_gen, scope=gen_loss_scope, weight=0.05)
+        l2_gen = slim.losses.sum_of_squares(gen_rec, images, scope=gen_loss_scope, weight=1.0)
+        l2feat_gen = slim.losses.sum_of_squares(gen_enc, enc_im, scope=gen_loss_scope, weight=0.1)
         losses_gen = slim.losses.get_losses(gen_loss_scope)
         losses_gen += slim.losses.get_regularization_losses(gen_loss_scope)
         gen_loss = math_ops.add_n(losses_gen, name='gen_total_loss')
@@ -102,7 +102,7 @@ with sess.as_default():
 
         # Define learning rate
         decay_steps = int(data.SPLITS_TO_SIZES[SET_NAME] / BATCH_SIZE)
-        learning_rate = tf.train.exponential_decay(0.005,
+        learning_rate = tf.train.exponential_decay(0.002,
                                                    global_step,
                                                    decay_steps,
                                                    0.94,

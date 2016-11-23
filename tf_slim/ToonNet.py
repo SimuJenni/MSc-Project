fixed to softmax xentropy

tf_slim/ToonNet.py
@@ -48,12 +48,11 @@ class AEGAN4:
         return slim.one_hot_encoding(labels, 3)
 
     def classifier(self, img, edge, toon, num_classes):
-        disc_in = merge(merge(merge(img, merge(img, img)),
-                              merge(img, merge(img, img)), dim=0),
-                        merge(img, merge(img, img)), dim=0)
-        model = discriminator(disc_in, num_layers=self.num_layers, reuse=False, num_out=num_classes,
-                              batch_size=self.batch_size, training=True)
-        pass
+        disc_in = merge(img, merge(img, img))
+        _, model = discriminator(disc_in, num_layers=self.num_layers, reuse=False, num_out=num_classes,
+                                 batch_size=self.batch_size, training=True)
+        model = classifier(model, num_classes)
+        return model
 
 
 class AEGAN2:
@@ -203,7 +202,7 @@ def toon_net_argscope(activation=tf.nn.relu, kernel_size=(4, 4), padding='SAME',
     batch_norm_params = {
         'is_training': training,
         'decay': 0.999,
-        'epsilon': 0.01,
+        'epsilon': 0.001,
     }
     with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.convolution2d_transpose],
                         activation_fn=activation,
@@ -218,8 +217,7 @@ def toon_net_argscope(activation=tf.nn.relu, kernel_size=(4, 4), padding='SAME',
                     return arg_sc
 
 
-def classifier(inputs, model, num_classes):
-    net = model(inputs)
+def classifier(inputs, num_classes):
     batch_norm_params = {'decay': 0.99, 'epsilon': 0.001}
     with tf.variable_scope('fully_connected'):
         with slim.arg_scope([slim.fully_connected],
@@ -227,7 +225,7 @@ def classifier(inputs, model, num_classes):
                             weights_regularizer=slim.l2_regularizer(0.0005),
                             normalizer_fn=slim.batch_norm,
                             normalizer_params=batch_norm_params):
-            net = slim.flatten(net)
+            net = slim.flatten(inputs)
             net = slim.fully_connected(net, 4096, scope='fc1')
             net = slim.dropout(net)
             net = slim.fully_connected(net, 4096, scope='fc2')

tf_slim/trainAEGAN_cifar10.py
@@ -22,7 +22,7 @@ TARGET_SHAPE = [32, 32, 3]
 NUM_EPOCHS = 50
 SAVE_DIR = os.path.join(LOG_DIR, '{}_{}/'.format(data.NAME, model.name))
 
-tf.logging.set_verbosity(tf.logging.INFO)
+tf.logging.set_verbosity(tf.logging.DEBUG)
 
 sess = tf.Session()
 g = tf.Graph()
@@ -72,16 +72,14 @@ with sess.as_default():
 
         # Define loss for discriminator training
         disc_loss_scope = 'disc_loss'
-        dL_disc = slim.losses.sigmoid_cross_entropy(disc_out, labels_disc, scope=disc_loss_scope, weight=1.0,
-                                                    label_smoothing=0.1)
+        dL_disc = slim.losses.softmax_cross_entropy(disc_out, labels_disc, scope=disc_loss_scope, weight=1.0)
         losses_disc = slim.losses.get_losses(disc_loss_scope)
         losses_disc += slim.losses.get_regularization_losses(disc_loss_scope)
         disc_loss = math_ops.add_n(losses_disc, name='disc_total_loss')
 
         # Define the losses for AE training
         ae_loss_scope = 'ae_loss'
-        dL_ae = slim.losses.sigmoid_cross_entropy(disc_out, labels_ae, scope=ae_loss_scope, weight=1.0,
-                                                  label_smoothing=0.1)
+        dL_ae = slim.losses.softmax_cross_entropy(disc_out, labels_ae, scope=ae_loss_scope, weight=1.0)
         l2_ae = slim.losses.absolute_difference(img_rec, imgs_train, scope=ae_loss_scope, weight=100.0)
         losses_ae = slim.losses.get_losses(ae_loss_scope)
         losses_ae += slim.losses.get_regularization_losses(ae_loss_scope)
@@ -89,8 +87,7 @@ with sess.as_default():
 
         # Define the losses for generator training
         gen_loss_scope = 'gen_loss'
-        dL_gen = slim.losses.sigmoid_cross_entropy(disc_out, labels_gen, scope=gen_loss_scope, weight=2,
-                                                   label_smoothing=0.1)
+        dL_gen = slim.losses.softmax_cross_entropy(disc_out, labels_gen, scope=gen_loss_scope, weight=2)
         l2_gen = slim.losses.absolute_difference(gen_rec, imgs_train, scope=gen_loss_scope, weight=50)
         for lg, le in zip(gen_enc, enc_im):
             slim.losses.sum_of_squares(lg, le, scope=gen_loss_scope, weight=5.0)
@@ -136,7 +133,7 @@ with sess.as_default():
         tf.image_summary('images/edges', montage(edges_test, 8, 8), max_images=1)
 
         # Define optimizer
-        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.5, epsilon=1e-6)
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.5, epsilon=1e-5)
 
         # Generator training operation
         scopes_gen = 'generator'
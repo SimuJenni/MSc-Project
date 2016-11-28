4b86eda7d20d7bce4382ce87f90ff3958dd1289f
Simon Jenni <simujenni@students.unibe.ch>
2016-11-28 12:16:07 +0100

    New setup

--- tf_slim/trainAEGAN_cifar10.py
+++ tf_slim/trainAEGAN_cifar10.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 
-from ToonNet import AEGAN2
+from ToonNet import AEGAN4
 from constants import LOG_DIR
 from datasets import cifar10
 from preprocess import preprocess_images_toon, preprocess_images_toon_test
@@ -16,7 +16,7 @@ slim = tf.contrib.slim
 # Setup training parameters
 data = cifar10
 TRAIN_SET_NAME = 'train'
-model = AEGAN2(num_layers=5, batch_size=128, data_size=data.SPLITS_TO_SIZES[TRAIN_SET_NAME], num_epochs=75)
+model = AEGAN4(num_layers=5, batch_size=128, data_size=data.SPLITS_TO_SIZES[TRAIN_SET_NAME], num_epochs=75)
 train_ae = False
 TEST_SET_NAME = 'test'
 TARGET_SHAPE = [64, 64, 3]

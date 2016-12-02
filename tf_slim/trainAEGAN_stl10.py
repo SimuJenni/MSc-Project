291ea1407cf67a4873f75425b6939af49f657fd9
Simon Jenni <simujenni@students.unibe.ch>
2016-12-03 00:58:10 +0100

    Final setup cifar10

--- tf_slim/trainAEGAN_stl10.py
+++ tf_slim/trainAEGAN_stl10.py
@@ -17,7 +17,7 @@ slim = tf.contrib.slim
 data = stl10
 TRAIN_SET_NAME = 'train_unlabeled'
 TEST_SET_NAME = 'test'
-model = AEGAN(num_layers=5, batch_size=72, data_size=data.SPLITS_TO_SIZES[TRAIN_SET_NAME], num_epochs=100)
+model = AEGAN(num_layers=5, batch_size=80, data_size=data.SPLITS_TO_SIZES[TRAIN_SET_NAME], num_epochs=100)
 TARGET_SHAPE = [96, 96, 3]
 RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)
 SAVE_DIR = os.path.join(LOG_DIR, '{}_{}/'.format(data.NAME, model.name))

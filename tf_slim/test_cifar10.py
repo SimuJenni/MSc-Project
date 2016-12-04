1fbe655360db4a8f1b22908875bec3bff4096ad1
Simon Jenni <simujenni@students.unibe.ch>
2016-12-04 14:56:42 +0100

    Finetune cifar10

--- tf_slim/test_cifar10.py
+++ tf_slim/test_cifar10.py
@@ -16,7 +16,7 @@ slim = tf.contrib.slim
 
 # Setup
 finetuned = True
-net_type = 'generator'
+net_type = 'discriminator'
 data = cifar10
 model = AEGAN(num_layers=4, batch_size=500, data_size=data.SPLITS_TO_SIZES['train'])
 TARGET_SHAPE = [32, 32, 3]

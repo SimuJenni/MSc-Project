f09730df7ba70e9ab1c6975a549f6d7c8ccad164
Simon Jenni <simujenni@students.unibe.ch>
2016-12-05 17:10:52 +0100

    Train maxpoolnet

--- tf_slim/ToonNetMaxPool.py
+++ tf_slim/ToonNetMaxPool.py
@@ -210,7 +210,8 @@ def discriminator(net, num_layers=5, reuse=None, num_out=2, training=True):
 
             encoded = net
             # Fully connected layers
-            net = slim.conv2d(net, num_outputs=1024, scope='conv_'.format(num_layers+1), stride=2, padding='VALID')
+            net = slim.conv2d(net, num_outputs=f_dims[num_layers], scope='conv_'.format(num_layers+1), stride=2,
+                              padding='VALID')
             net = spatial_dropout(net, 0.9)
             net = slim.flatten(net)
             net = slim.fully_connected(net, 2048)

b4834ee3ebe9a7e71a3f0288ba68e9be8d885d14
Simon Jenni <simujenni@students.unibe.ch>
2016-12-08 00:27:04 +0100

    New ae code

--- tf_slim/fine_tune_stl10.py
+++ tf_slim/fine_tune_stl10.py
@@ -133,8 +133,9 @@ with sess.as_default():
         if fine_tune:
             # Specify the layers of your model you want to exclude
             variables_to_restore = slim.get_variables_to_restore(
-                exclude=['fully_connected', ops.GraphKeys.GLOBAL_STEP])
-            slim.get_or_create_global_step()
+                include=[net_type], exclude=['fully_connected', 'discriminator/fully_connected',
+                                             ops.GraphKeys.GLOBAL_STEP])
+            print('Variables to restore: {}'.format([v.op.name for v in variables_to_restore]))
             init_fn = assign_from_checkpoint_fn(MODEL_PATH, variables_to_restore, ignore_missing_vars=True)
 
         # Start training

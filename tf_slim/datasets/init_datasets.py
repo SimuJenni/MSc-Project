36eb8605eb33b435e7e760540509f46227582307
Simon Jenni <simujenni@students.unibe.ch>
2016-12-19 21:52:09 +0100

    New settings

--- tf_slim/datasets/init_datasets.py
+++ tf_slim/datasets/init_datasets.py
@@ -1,6 +1,7 @@
-from tf_slim.datasets import download_and_convert_cifar10, convert_imagenet, download_and_convert_stl10
-from constants import IMAGENET_TRAIN_DIR, IMAGENET_VAL_DIR, IMAGENET_TF_DATADIR, CIFAR10_TF_DATADIR
+from tf_slim.datasets import download_and_convert_cifar10, convert_imagenet, download_and_convert_stl10, convert_voc2007
+from constants import *
 
-download_and_convert_cifar10.run(CIFAR10_TF_DATADIR)
-convert_imagenet.run(IMAGENET_TRAIN_DIR, IMAGENET_VAL_DIR, IMAGENET_TF_DATADIR)
-download_and_convert_stl10.run()
+# download_and_convert_cifar10.run(CIFAR10_TF_DATADIR)
+# convert_imagenet.run(IMAGENET_TRAIN_DIR, IMAGENET_VAL_DIR, IMAGENET_TF_DATADIR)
+# download_and_convert_stl10.run()
+convert_voc2007.run(VOC2007_TF_DATADIR, VOC2007_SRC_DIR)

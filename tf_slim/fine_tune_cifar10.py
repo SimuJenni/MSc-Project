fc78fc3c0ab8acf485795fffa8c4342acd79cbc0
Simon Jenni <simujenni@students.unibe.ch>
2016-12-19 12:22:00 +0100

    New settings

--- tf_slim/fine_tune_cifar10.py
+++ tf_slim/fine_tune_cifar10.py
@@ -15,7 +15,7 @@ slim = tf.contrib.slim
 
 # Setup
 fine_tune = True
-net_type = 'encoder'
+net_type = 'discriminator'
 data = cifar10
 model = AEGAN(num_layers=4, batch_size=512, data_size=data.SPLITS_TO_SIZES['train'], num_epochs=300)
 TARGET_SHAPE = [32, 32, 3]
@@ -23,9 +23,9 @@ RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)
 TEST_WHILE_TRAIN = False
 
 CHECKPOINT = 'model.ckpt-58502'
-MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_new_settings2/{}'.format(data.NAME, model.name, CHECKPOINT))
+MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_cond/{}'.format(data.NAME, model.name, CHECKPOINT))
 if fine_tune:
-    SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_new_settings2/'.format(data.NAME, model.name, net_type))
+    SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_cond/'.format(data.NAME, model.name, net_type))
 else:
     SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_classifier/'.format(data.NAME, model.name))
 

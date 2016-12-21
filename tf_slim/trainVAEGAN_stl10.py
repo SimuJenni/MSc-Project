94c4a67de0d7c0a900760c260c1c039807b84725
Simon Jenni <simujenni@students.unibe.ch>
2016-12-21 12:36:42 +0100

    New settings

--- tf_slim/trainVAEGAN_stl10.py
+++ tf_slim/trainVAEGAN_stl10.py
@@ -18,7 +18,7 @@ slim = tf.contrib.slim
 data = stl10
 TRAIN_SET_NAME = 'train_unlabeled'
 TEST_SET_NAME = 'test'
-model = VAEGAN(num_layers=5, batch_size=192, data_size=data.SPLITS_TO_SIZES[TRAIN_SET_NAME], num_epochs=150)
+model = VAEGAN(num_layers=5, batch_size=160, data_size=data.SPLITS_TO_SIZES[TRAIN_SET_NAME], num_epochs=150)
 TARGET_SHAPE = [96, 96, 3]
 RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)
 SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_new/'.format(data.NAME, model.name))

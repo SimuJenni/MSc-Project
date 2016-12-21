39d2471fadd216ad6e28b1c7d25c31c1e25f4ad9
Simon Jenni <simujenni@students.unibe.ch>
2016-12-21 08:44:42 +0100

    New settings

--- tf_slim/fine_tune_cifar10.py
+++ tf_slim/fine_tune_cifar10.py
@@ -22,7 +22,7 @@ TARGET_SHAPE = [32, 32, 3]
 RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)
 TEST_WHILE_TRAIN = False
 
-CHECKPOINT = 'model.ckpt-29102'
+CHECKPOINT = 'model.ckpt-29100'
 MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_new/{}'.format(data.NAME, model.name, CHECKPOINT))
 if fine_tune:
     SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_new/'.format(data.NAME, model.name, net_type))

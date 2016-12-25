e12a9260cf33b9f72cd5bf6b51f462ed484c9b3d
SimuJenni <simujenni@gmail.com>
2016-12-25 16:22:45 +0100

    Fixed trainable scopes

--- tf_slim/test_classifier_cifar10.py
+++ tf_slim/test_classifier_cifar10.py
@@ -13,17 +13,17 @@ slim = tf.contrib.slim
 
 # Setup
 finetuned = True
-net_type = 'discriminator'
+net_type = 'generator'
 data = cifar10
 model = VAEGAN(num_layers=3, batch_size=500, data_size=data.SPLITS_TO_SIZES['train'])
 TARGET_SHAPE = [32, 32, 3]
 RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)
-NUM_CONV_TRAIN = 2
+NUM_CONV_TRAIN = 0
 
 if finetuned:
-    MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_andanothersetting_0.01weight/'.format(
+    MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_andanothersetting/'.format(
         data.NAME, model.name, net_type, NUM_CONV_TRAIN))
-    LOG_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_eval_andanothersetting_0.01weight/'.format(
+    LOG_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_eval_andanothersetting/'.format(
         data.NAME, model.name, net_type, NUM_CONV_TRAIN))
 else:
     MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_classifier/'.format(data.NAME, model.name))

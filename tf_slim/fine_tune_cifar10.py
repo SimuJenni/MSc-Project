b56c890a78ea3032cd2ad5300f412e6425be057d
SimuJenni <simujenni@gmail.com>
2016-12-25 14:12:11 +0100

    Fixed trainable scopes

--- tf_slim/fine_tune_cifar10.py
+++ tf_slim/fine_tune_cifar10.py
@@ -27,12 +27,12 @@ TARGET_SHAPE = [32, 32, 3]
 RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)
 TEST_WHILE_TRAIN = False
 NUM_CONV_TRAIN = 2
-pre_trained_grad_weight = 0.5
+pre_trained_grad_weight = 0.01
 
 CHECKPOINT = 'model.ckpt-29102'
 MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_andanothersetting/{}'.format(data.NAME, model.name, CHECKPOINT))
 if fine_tune:
-    SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_andanothersetting_0.5weight/'.format(data.NAME, model.name,
+    SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_andanothersetting_0.01weight/'.format(data.NAME, model.name,
                                                                                              net_type, NUM_CONV_TRAIN))
 else:
     SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_classifier/'.format(data.NAME, model.name))

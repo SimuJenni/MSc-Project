26410a8ab5731e93b7398c91ad4b83d375975955
SimuJenni <simujenni@gmail.com>
2016-12-26 16:49:06 +0100

    Fixed trainable scopes

--- tf_slim/ToonNetAEGAN.py
+++ tf_slim/ToonNetAEGAN.py
@@ -19,7 +19,7 @@ class VAEGAN:
             data_size: Number of training images in the dataset
             num_epochs: Number of epochs used for training
         """
-        self.name = 'AEGAN_vgga_bn_1.5'
+        self.name = 'AEGAN_vgga_final'
         self.num_layers = num_layers
         self.batch_size = batch_size
         self.data_size = data_size
@@ -248,9 +248,9 @@ def classifier(inputs, num_classes, reuse=None, training=True, activation=tf.nn.
         with slim.arg_scope(toon_net_argscope(activation=activation, training=training)):
             net = slim.flatten(inputs)
             net = slim.fully_connected(net, 4096, scope='fc1')
-            net = slim.dropout(net, 0.9, is_training=training)
+            # net = slim.dropout(net, 0.9, is_training=training)
             net = slim.fully_connected(net, 4096, scope='fc2')
-            net = slim.dropout(net, 0.9, is_training=training)
+            # net = slim.dropout(net, 0.9, is_training=training)
             net = slim.fully_connected(net, num_classes, scope='fc3',
                                        activation_fn=None,
                                        normalizer_fn=None,

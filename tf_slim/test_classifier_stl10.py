c7337f13bec84858761fd5e1f48adafe936701ea
SimuJenni <simujenni@gmail.com>
2016-12-29 00:20:04 +0100

    try new weight

--- tf_slim/test_classifier_stl10.py
+++ tf_slim/test_classifier_stl10.py
@@ -18,7 +18,7 @@ data = stl10
 model = VAEGAN(num_layers=4, batch_size=100, data_size=data.SPLITS_TO_SIZES['train'])
 TARGET_SHAPE = [96, 96, 3]
 RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)
-NUM_CONV_TRAIN = 1
+NUM_CONV_TRAIN = 2
 
 if finetuned:
     MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final/'.format(

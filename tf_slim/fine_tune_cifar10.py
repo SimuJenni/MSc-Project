87d381ab464d7f9ce0f179d30cf3010fb6a2fdcf
SimuJenni <simujenni@gmail.com>
2016-12-28 14:56:00 +0100

    Testing stl10

--- tf_slim/fine_tune_cifar10.py
+++ tf_slim/fine_tune_cifar10.py
@@ -19,14 +19,14 @@ slim = tf.contrib.slim
 
 # Setup
 fine_tune = True
-net_type = 'discriminator'
+net_type = 'generator'
 data = cifar10
 num_layers = 3
 model = VAEGAN(num_layers=num_layers, batch_size=512, data_size=data.SPLITS_TO_SIZES['train'], num_epochs=300)
 TARGET_SHAPE = [32, 32, 3]
 RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)
 TEST_WHILE_TRAIN = True
-NUM_CONV_TRAIN = 3
+NUM_CONV_TRAIN = 1
 pre_trained_grad_weight = 0.1
 
 CHECKPOINT = 'model.ckpt-29102'

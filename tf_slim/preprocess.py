480edb1d93931235fc8817ce72e7ab20183726ea
SimuJenni <simujenni@gmail.com>
2016-12-29 17:36:59 +0100

    try new weight

--- tf_slim/preprocess.py
+++ tf_slim/preprocess.py
@@ -389,11 +389,11 @@ def preprocess_toon_test(image, edge, cartoon, output_height, output_width, resi
 
 def preprocess_finetune_train(image, edge, output_height, output_width, resize_side_min=_RESIZE_SIDE_MIN,
                               resize_side_max=_RESIZE_SIDE_MAX):
-    image = tf.to_float(image) * (1. / 255.)
-    image = tf.image.random_brightness(image, 0.1, seed=None)
-    image = tf.image.random_contrast(image, 0.7, 1.4, seed=None)
-    image = tf.image.random_hue(image, 0.2, seed=None)
-    image = tf.image.random_saturation(image, 0.7, 1.4, seed=None)
+    # image = tf.to_float(image) * (1. / 255.)
+    # image = tf.image.random_brightness(image, 0.1, seed=None)
+    # image = tf.image.random_contrast(image, 0.7, 1.4, seed=None)
+    # image = tf.image.random_hue(image, 0.2, seed=None)
+    # image = tf.image.random_saturation(image, 0.7, 1.4, seed=None)
 
     # Compute zoom side-size
     resize_side = tf.random_uniform([], minval=resize_side_min, maxval=resize_side_max + 1, dtype=tf.int32)

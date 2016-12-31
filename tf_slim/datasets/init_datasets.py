8d4cbca1dc56502fd3ac584b6d6ebc4427527da3
SimuJenni <simujenni@gmail.com>
2016-12-31 11:25:03 +0100

    preprocess small imagenet

--- tf_slim/datasets/init_datasets.py
+++ tf_slim/datasets/init_datasets.py
@@ -5,4 +5,4 @@ from constants import *
 # convert_imagenet.run(IMAGENET_TRAIN_DIR, IMAGENET_VAL_DIR, IMAGENET_TF_DATADIR)
 # download_and_convert_stl10.run()
 # convert_voc2007.run(VOC2007_TF_DATADIR, VOC2007_SRC_DIR)
-convert_imagenet_small.run(IMAGENET_TRAIN_DIR, IMAGENET_VAL_DIR, IMAGENET_TF_DATADIR)
+convert_imagenet_small.run(IMAGENET_TRAIN_DIR, IMAGENET_VAL_DIR, IMAGENET_SMALL_TF_DATADIR)

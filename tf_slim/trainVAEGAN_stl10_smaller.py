0179ceeed93d6b074fa7282078c059c2dc980fbc
SimuJenni <simujenni@gmail.com>
2016-12-31 14:43:24 +0100

    preprocess small imagenet

--- tf_slim/trainVAEGAN_stl10_smaller.py
+++ tf_slim/trainVAEGAN_stl10_smaller.py
@@ -17,7 +17,7 @@ slim = tf.contrib.slim
 data = stl10
 TRAIN_SET_NAME = 'train_unlabeled'
 TEST_SET_NAME = 'test'
-model = VAEGAN(num_layers=4, batch_size=128, data_size=data.SPLITS_TO_SIZES[TRAIN_SET_NAME], num_epochs=300)
+model = VAEGAN(num_layers=4, batch_size=128, data_size=data.SPLITS_TO_SIZES[TRAIN_SET_NAME], num_epochs=240)
 TARGET_SHAPE = [64, 64, 3]
 LR = 0.0001
 RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)

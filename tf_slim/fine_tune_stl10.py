a90fbd133672a8b9d40a66578a89a140ddaaf24e
SimuJenni <simujenni@gmail.com>
2017-01-02 15:43:45 +0100

    preprocess small imagenet

--- tf_slim/fine_tune_stl10.py
+++ tf_slim/fine_tune_stl10.py
@@ -25,13 +25,13 @@ num_layers = 4
 model = VAEGAN(num_layers=num_layers, batch_size=256, data_size=data.SPLITS_TO_SIZES['train'], num_epochs=500)
 TARGET_SHAPE = [96, 96, 3]
 TEST_WHILE_TRAIN = True
-NUM_CONV_TRAIN = 2
+NUM_CONV_TRAIN = 0
 pre_trained_grad_weight = 0.1
 
 CHECKPOINT = 'model.ckpt-104001'
 MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_final_small/{}'.format(data.NAME, model.name, CHECKPOINT))
 if fine_tune:
-    SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_small/'.format(data.NAME, model.name,
+    SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_small_maug/'.format(data.NAME, model.name,
                                                                                         net_type, NUM_CONV_TRAIN))
 else:
     SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_classifier/'.format(data.NAME, model.name))

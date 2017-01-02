378f4eed956bfdc73ad13bed5584ccb6af91d245
SimuJenni <simujenni@gmail.com>
2017-01-02 12:21:05 +0100

    preprocess small imagenet

--- tf_slim/fine_tune_stl10.py
+++ tf_slim/fine_tune_stl10.py
@@ -25,7 +25,7 @@ num_layers = 4
 model = VAEGAN(num_layers=num_layers, batch_size=256, data_size=data.SPLITS_TO_SIZES['train'], num_epochs=500)
 TARGET_SHAPE = [96, 96, 3]
 TEST_WHILE_TRAIN = False
-NUM_CONV_TRAIN = 0
+NUM_CONV_TRAIN = 1
 pre_trained_grad_weight = 0.2
 
 CHECKPOINT = 'model.ckpt-104001'

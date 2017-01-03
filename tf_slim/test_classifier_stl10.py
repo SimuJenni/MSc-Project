61e16679a09d46b5c338663d189ee6581735e076
SimuJenni <simujenni@gmail.com>
2017-01-03 12:03:11 +0100

    preprocess small imagenet

--- tf_slim/test_classifier_stl10.py
+++ tf_slim/test_classifier_stl10.py
@@ -16,14 +16,14 @@ finetuned = True
 net_type = 'discriminator'
 data = stl10
 model = VAEGAN(num_layers=4, batch_size=500, data_size=data.SPLITS_TO_SIZES['train'])
-TARGET_SHAPE = [96, 96, 3]
+TARGET_SHAPE = [64, 64, 3]
 RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)
-NUM_CONV_TRAIN = 1
+NUM_CONV_TRAIN = 0
 
 if finetuned:
-    MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_small/'.format(
+    MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final/'.format(
         data.NAME, model.name, net_type, NUM_CONV_TRAIN))
-    LOG_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_small/'.format(
+    LOG_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final/'.format(
         data.NAME, model.name, net_type, NUM_CONV_TRAIN))
 else:
     MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_classifier/'.format(data.NAME, model.name))

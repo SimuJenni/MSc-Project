46fe532a0901857cc85c7491a403b26f31a82ec3
SimuJenni <simujenni@gmail.com>
2017-01-06 18:51:53 +0100

    aegan3

--- tf_slim/test_classifier_stl10.py
+++ tf_slim/test_classifier_stl10.py
@@ -4,7 +4,7 @@ import os
 
 import tensorflow as tf
 
-from ToonNetAEGAN2 import VAEGAN
+from ToonNetAEGAN3 import VAEGAN
 from constants import LOG_DIR
 from datasets import stl10
 from preprocess import preprocess_finetune_test
@@ -18,7 +18,7 @@ data = stl10
 model = VAEGAN(num_layers=4, batch_size=500, data_size=data.SPLITS_TO_SIZES['train'])
 TARGET_SHAPE = [96, 96, 3]
 RESIZE_SIZE = max(TARGET_SHAPE[0], data.MIN_SIZE)
-NUM_CONV_TRAIN = 2
+NUM_CONV_TRAIN = 0
 
 if finetuned:
     MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final/'.format(

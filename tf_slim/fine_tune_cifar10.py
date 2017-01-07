26eb1af0fc8b282cc8ca411bf63ec9e03f431efa
SimuJenni <simujenni@gmail.com>
2017-01-07 16:19:03 +0100

    aegan3

--- tf_slim/fine_tune_cifar10.py
+++ tf_slim/fine_tune_cifar10.py
@@ -29,7 +29,7 @@ TEST_WHILE_TRAIN = True
 NUM_CONV_TRAIN = 1
 pre_trained_grad_weight = 0.1
 
-CHECKPOINT = 'model.ckpt-29102'
+CHECKPOINT = 'model.ckpt-14550'
 MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_final/{}'.format(data.NAME, model.name, CHECKPOINT))
 if fine_tune:
     SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final/'.format(data.NAME, model.name,

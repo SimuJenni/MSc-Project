39f14de5cabd2ae0c2157345b1e617b52320736b
SimuJenni <simujenni@gmail.com>
2017-01-07 21:26:02 +0100

    aegan3

--- tf_slim/trainVAEGAN_cifar10.py
+++ tf_slim/trainVAEGAN_cifar10.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 
-from ToonNetAEGAN3_cifar import VAEGAN
+from ToonNetAEGAN_test import VAEGAN
 from constants import LOG_DIR
 from datasets import cifar10
 from preprocess import preprocess_toon_train, preprocess_toon_test
@@ -78,19 +78,19 @@ with sess.as_default():
         losses_disc += slim.losses.get_regularization_losses(disc_loss_scope)
         disc_loss = math_ops.add_n(losses_disc, name='disc_total_loss')
 
-        # Define the losses for AE training
-        ae_loss_scope = 'ae_loss'
-        l2_ae = slim.losses.sum_of_squares(img_rec, imgs_train, scope=ae_loss_scope, weight=100)
-        losses_ae = slim.losses.get_losses(ae_loss_scope)
-        losses_ae += slim.losses.get_regularization_losses(ae_loss_scope)
-        ae_loss = math_ops.add_n(losses_ae, name='ae_total_loss')
+        # # Define the losses for AE training
+        # ae_loss_scope = 'ae_loss'
+        # l2_ae = slim.losses.sum_of_squares(img_rec, imgs_train, scope=ae_loss_scope, weight=100)
+        # losses_ae = slim.losses.get_losses(ae_loss_scope)
+        # losses_ae += slim.losses.get_regularization_losses(ae_loss_scope)
+        # ae_loss = math_ops.add_n(losses_ae, name='ae_total_loss')
 
         # Define the losses for generator training
         gen_loss_scope = 'gen_loss'
         dL_gen = slim.losses.softmax_cross_entropy(disc_out, labels_gen, scope=gen_loss_scope, weight=1.0)
         l2_gen = slim.losses.sum_of_squares(gen_rec, imgs_train, scope=gen_loss_scope, weight=30.0)
-        l2_mu = slim.losses.sum_of_squares(gen_mu, enc_mu, scope=gen_loss_scope, weight=3.0)
-        l2_sigma = slim.losses.sum_of_squares(gen_logvar, enc_logvar, scope=gen_loss_scope, weight=3.0)
+        # l2_mu = slim.losses.sum_of_squares(gen_mu, enc_mu, scope=gen_loss_scope, weight=3.0)
+        # l2_sigma = slim.losses.sum_of_squares(gen_logvar, enc_logvar, scope=gen_loss_scope, weight=3.0)
         losses_gen = slim.losses.get_losses(gen_loss_scope)
         losses_gen += slim.losses.get_regularization_losses(gen_loss_scope)
         gen_loss = math_ops.add_n(losses_gen, name='gen_total_loss')
@@ -107,7 +107,7 @@ with sess.as_default():
         if update_ops:
             updates = tf.group(*update_ops)
             gen_loss = control_flow_ops.with_dependencies([updates], gen_loss)
-            ae_loss = control_flow_ops.with_dependencies([updates], ae_loss)
+            # ae_loss = control_flow_ops.with_dependencies([updates], ae_loss)
             disc_loss = control_flow_ops.with_dependencies([updates], disc_loss)
 
         # Define learning parameters
@@ -123,9 +123,9 @@ with sess.as_default():
         tf.scalar_summary('losses/discriminator loss', disc_loss)
         tf.scalar_summary('losses/disc-loss generator', dL_gen)
         tf.scalar_summary('losses/l2 generator', l2_gen)
-        tf.scalar_summary('losses/L2 mu', l2_mu)
-        tf.scalar_summary('losses/L2 sigma', l2_sigma)
-        tf.scalar_summary('losses/l2 auto-encoder', l2_ae)
+        # tf.scalar_summary('losses/L2 mu', l2_mu)
+        # tf.scalar_summary('losses/L2 sigma', l2_sigma)
+        # tf.scalar_summary('losses/l2 auto-encoder', l2_ae)
 
         if TEST:
             img_rec_test, gen_rec_test, _, _, _ = model.net(imgs_test, toons_test, edges_test, reuse=True,

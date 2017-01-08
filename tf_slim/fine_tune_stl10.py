686bdef88b9748bd4c38810be263175d0f4aa934
SimuJenni <simujenni@gmail.com>
2017-01-08 17:09:18 +0100

    aegan3

--- tf_slim/fine_tune_stl10.py
+++ tf_slim/fine_tune_stl10.py
@@ -28,7 +28,7 @@ NUM_CONV_TRAIN = 0
 pre_trained_grad_weight = [0.5 * 0.5 ** i for i in range(num_layers)]
 
 CHECKPOINT = 'model.ckpt-75000'
-MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_final_small/{}'.format(data.NAME, model.name, CHECKPOINT))
+MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_final/{}'.format(data.NAME, model.name, CHECKPOINT))
 if fine_tune:
     SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final/'.format(data.NAME, model.name,
                                                                                  net_type, NUM_CONV_TRAIN))

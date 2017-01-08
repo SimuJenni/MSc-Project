2f62a93b0d2676defc5e20938dc1b0a8cb0b6287
SimuJenni <simujenni@gmail.com>
2017-01-08 13:23:27 +0100

    aegan3

--- tf_slim/test_recon_cifar10.py
+++ tf_slim/test_recon_cifar10.py
@@ -4,7 +4,7 @@ import os
 
 import tensorflow as tf
 
-from ToonNetAEGAN3_cifar import VAEGAN
+from ToonNetAEGAN_test import VAEGAN
 from constants import LOG_DIR
 from datasets import cifar10
 from preprocess import preprocess_toon_test

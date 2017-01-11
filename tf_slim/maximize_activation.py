05ec69d6bd325912f421da832091c54e9a525112
SimuJenni <simujenni@gmail.com>
2017-01-11 18:22:54 +0100

    Clean up

--- tf_slim/maximize_activation.py
+++ tf_slim/maximize_activation.py
@@ -31,8 +31,7 @@ def max_activity_img(layer_id, num_filters, lr, ckpt, reuse=None):
         saver.restore(sess, ckpt.model_checkpoint_path)
 
         for f in range(num_filters):
-            assign_op = x.assign(tf.random_normal([1, 128, 128, 3], stddev=10))
-            sess.run(assign_op)
+            x = tf.Variable(tf.random_normal([1, 128, 128, 3], stddev=10), name='x')
             loss = tf.reduce_sum(layers[layer_id - 1][:, :, :, f])
             opt = tf.train.GradientDescentOptimizer(learning_rate=lr)
             grads_and_vars = opt.compute_gradients(loss, var_list=[x])

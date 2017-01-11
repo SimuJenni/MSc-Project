a8b237ab5737492409b36c61e7eee2b7508fb0d0
SimuJenni <simujenni@gmail.com>
2017-01-11 21:04:16 +0100

    Clean up

--- tf_slim/maximize_activation.py
+++ tf_slim/maximize_activation.py
@@ -69,7 +69,7 @@ MODLE_DIR = os.path.join(LOG_DIR, '{}_{}_final/'.format(data.NAME, model.name))
 ckpt = tf.train.get_checkpoint_state(MODLE_DIR)
 LAYER = 1
 LR = 1
-FILTERS = [i for i in range(4)]
+FILTERS = [i for i in range(9)]
 imgs = [None for i in FILTERS]
 for i, f in enumerate(FILTERS):
     if i == 0:

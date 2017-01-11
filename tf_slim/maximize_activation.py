28d372c7319e523fd5cda56d4917e8c077498d40
SimuJenni <simujenni@gmail.com>
2017-01-11 16:03:43 +0100

    Clean up

--- tf_slim/maximize_activation.py
+++ tf_slim/maximize_activation.py
@@ -13,23 +13,23 @@ slim = tf.contrib.slim
 
 # util function to convert a tensor into a valid image
 def deprocess_image(x):
-    x -= x.mean()
-    x /= (x.std() + 1e-5)
-    x *= 0.1
-
-    # clip to [0, 1]
-    x += 0.5
-    x = np.clip(x, 0, 1)
-
-    # convert to RGB array
-    x *= 255
-    x = np.clip(x, 0, 255).astype('uint8')
-    return x
-
-    # x += 1.0
-    # x *= 255/2.
+    # x -= x.mean()
+    # x /= (x.std() + 1e-5)
+    # x *= 0.1
+    #
+    # # clip to [0, 1]
+    # x += 0.5
+    # x = np.clip(x, 0, 1)
+    #
+    # # convert to RGB array
+    # x *= 255
     # x = np.clip(x, 0, 255).astype('uint8')
     # return x
+
+    x += 1.0
+    x *= 255/2.
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
 
 
 data = imagenet
@@ -37,11 +37,12 @@ model = VAEGAN(num_layers=5, batch_size=1, data_size=1, num_epochs=1)
 MODLE_DIR = os.path.join(LOG_DIR, '{}_{}_final/'.format(data.NAME, model.name))
 LAYER_IDX = 2
 FILTER_IDX = 0
-LR = 10
-NUM_STEPS = 1000
+LR = 1
+NUM_STEPS = 200
 l2decay = 0.0001
 
-x = tf.Variable(tf.random_normal([1, 128, 128, 3]), name='x')
+x = tf.Variable(tf.random_normal([1, 128, 128, 3], stddev=10), name='x')
+x = clip_by_value(x, clip_value_min=-1., clip_value_max=1.)
 
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
@@ -52,10 +53,10 @@ with tf.Session() as sess:
     saver = tf.train.Saver(var_list=vars)
     saver.restore(sess, ckpt.model_checkpoint_path)
 
-    loss = tf.reduce_mean(layers[LAYER_IDX][:, :, :, FILTER_IDX])
+    loss = tf.reduce_sum(layers[LAYER_IDX][:, :, :, FILTER_IDX])
     opt = tf.train.GradientDescentOptimizer(learning_rate=LR)
     grads_and_vars = opt.compute_gradients(loss, var_list=[x])
-    modded_grads_and_vars = [(-gv[0]/(tf.sqrt(tf.reduce_mean(tf.square(gv[0]))) + 1e-5), gv[1]) for gv in grads_and_vars]
+    modded_grads_and_vars = [(-gv[0], gv[1]) for gv in grads_and_vars]
     train_op = opt.apply_gradients(modded_grads_and_vars)
 
 

01fec59b7421fcdcf28cd5f0e5835a7005cabc8e
SimuJenni <simujenni@gmail.com>
2017-01-11 23:13:24 +0100

    Clean up

--- tf_slim/maximize_activation.py
+++ tf_slim/maximize_activation.py
@@ -32,86 +32,31 @@ def deprocess_image(x):
     # return x
 
 
-# def max_activity_img(layer_id, filter_id, lr, ckpt, reuse=None):
-#     x = tf.Variable(tf.random_normal([1, 128, 128, 3], stddev=10), name='x')
-#     with tf.Session() as sess:
-#
-#         sess.run(tf.initialize_all_variables())
-#
-#         _, _, layers = discriminator(x, training=False, train_fc=False, reuse=reuse)
-#         vars = slim.get_variables_to_restore(include=['discriminator'])
-#         saver = tf.train.Saver(var_list=vars)
-#         saver.restore(sess, ckpt.model_checkpoint_path)
-#
-#         loss = -tf.reduce_sum(layers[layer_id - 1][:, :, :, filter_id])
-#         loss += 0.001*tf.reduce_sum(tf.square(x))
-#         opt = tf.train.GradientDescentOptimizer(learning_rate=lr)
-#         # grads_and_vars = opt.compute_gradients(loss, var_list=[x])
-#         # modded_grads_and_vars = [(-gv[0], gv[1]) for gv in grads_and_vars]
-#         # train_op = opt.apply_gradients(modded_grads_and_vars)
-#         train_op = opt.minimize(loss, var_list=[x])
-#         print('Layer: {} Filter: {} Learning-Rate: {}'.format(layer_id, filter_id, lr))
-#
-#         for j in range(200):
-#             sess.run([train_op])
-#             with tf.control_dependencies([train_op]):
-#                 # x *= (1. - 0.0001)
-#                 x = clip_by_value(x, clip_value_min=-1., clip_value_max=1.)
-#                 # print(sess.run(loss))
-#
-#         img = x.eval()
-#         img = deprocess_image(np.squeeze(img))
-#         return img
-#
-#
-# data = imagenet
-# model = VAEGAN(num_layers=5, batch_size=1, data_size=1, num_epochs=1)
-# MODLE_DIR = os.path.join(LOG_DIR, '{}_{}_final/'.format(data.NAME, model.name))
-# ckpt = tf.train.get_checkpoint_state(MODLE_DIR)
-# LAYER = 2
-# LR = 2
-# FILTERS = [i for i in range(64)]
-# imgs = [None for i in FILTERS]
-# for i, f in enumerate(FILTERS):
-#     if i == 0:
-#         reuse = None
-#     else:
-#         reuse = True
-#     imgs[i] = max_activity_img(LAYER, f, LR, ckpt, reuse=reuse)
-#
-# montage_img = montage(imgs)
-# scipy.misc.toimage(montage_img, cmin=0, cmax=255).save('max_act_%d_more.png' % (LAYER))
-#
-
-
-def max_activity_img(layer_id, num_filters, lr, ckpt, reuse=None):
-    imgs = [None for i in range(num_filters)]
-    x = tf.Variable(tf.random_normal([1, 128, 128, 3], stddev=1), name='x')
+def max_activity_img(layer_id, filter_id, lr, ckpt, reuse=None):
+    x = tf.Variable(tf.random_normal([1, 128, 128, 3], stddev=10), name='x')
     with tf.Session() as sess:
+
         sess.run(tf.initialize_all_variables())
 
         _, _, layers = discriminator(x, training=False, train_fc=False, reuse=reuse)
-        vars = slim.get_variables_to_restore(include=['discriminator'], exclude=['discriminator/fully_connected'])
+        vars = slim.get_variables_to_restore(include=['discriminator'])
         saver = tf.train.Saver(var_list=vars)
         saver.restore(sess, ckpt.model_checkpoint_path)
 
-        for f in range(num_filters):
-            loss = -tf.reduce_sum(layers[layer_id - 1][:, :, :, f])
-            loss += 0.001*tf.reduce_sum(tf.square(x))
-            opt = tf.train.GradientDescentOptimizer(learning_rate=lr)
-            train_op = opt.minimize(loss, var_list=[x])
+        loss = -tf.reduce_sum(layers[layer_id - 1][:, :, :, filter_id])
+        loss += 0.001*tf.reduce_sum(tf.square(x))
+        opt = tf.train.GradientDescentOptimizer(learning_rate=lr)
+        train_op = opt.minimize(loss, var_list=[x])
+        print('Layer: {} Filter: {} Learning-Rate: {}'.format(layer_id, filter_id, lr))
 
-            print('Layer: {} Filter: {} Learning-Rate: {}'.format(layer_id, f, lr))
-            for i in range(200):
-                sess.run([train_op])
-                with tf.control_dependencies([train_op]):
-                    # x *= (1. - 0.0001)
-                    x = clip_by_value(x, clip_value_min=-1., clip_value_max=1.)
+        for j in range(100):
+            sess.run([train_op])
+            with tf.control_dependencies([train_op]):
+                x = clip_by_value(x, clip_value_min=-1., clip_value_max=1.)
 
-            img = x.eval()
-            imgs[f] = deprocess_image(np.squeeze(img))
-
-        return imgs
+        img = x.eval()
+        img = deprocess_image(np.squeeze(img))
+        return img, sess.run(loss)
 
 data = imagenet
 model = VAEGAN(num_layers=5, batch_size=1, data_size=1, num_epochs=1)
@@ -119,7 +64,17 @@ MODLE_DIR = os.path.join(LOG_DIR, '{}_{}_final/'.format(data.NAME, model.name))
 ckpt = tf.train.get_checkpoint_state(MODLE_DIR)
 LAYER = 1
 LR = 1
-NUM_FILTERS = 64
-imgs = max_activity_img(LAYER, NUM_FILTERS, LR, ckpt)
-montage_img = montage(imgs)
+FILTERS = [i for i in range(16)]
+imgs = [None for i in FILTERS]
+losses = [0. for i in FILTERS]
+for i, f in enumerate(FILTERS):
+    if i == 0:
+        reuse = None
+    else:
+        reuse = True
+    imgs[i], losses[i] = max_activity_img(LAYER, f, LR, ckpt, reuse=reuse)
+
+print(losses)
+print(np.argsort(losses)[::-1])
+montage_img = montage(imgs[np.argsort(losses)[::-1]])
 scipy.misc.toimage(montage_img, cmin=0, cmax=255).save('max_act_%d_test.png' % (LAYER))

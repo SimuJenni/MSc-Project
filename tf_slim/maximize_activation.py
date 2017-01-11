98b5186691bdcc673e2bfbb92f9a66f48bd6b801
SimuJenni <simujenni@gmail.com>
2017-01-11 16:37:28 +0100

    Clean up

--- tf_slim/maximize_activation.py
+++ tf_slim/maximize_activation.py
@@ -7,13 +7,12 @@ from datasets import imagenet
 from tensorflow.python.ops.clip_ops import clip_by_value
 
 from scipy.misc import imsave
-from scipy.ndimage.filters import median_filter
 import numpy as np
 slim = tf.contrib.slim
 
 
 # util function to convert a tensor into a valid image
-def deprocess_image(im):
+def deprocess_image(x):
     # x -= x.mean()
     # x /= (x.std() + 1e-5)
     # x *= 0.1
@@ -27,20 +26,20 @@ def deprocess_image(im):
     # x = np.clip(x, 0, 255).astype('uint8')
     # return x
 
-    im += 1.0
-    im *= 255/2.
-    im = np.clip(im, 0, 255).astype('uint8')
-    return im
+    x += 1.0
+    x *= 255/2.
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
 
 
 data = imagenet
 model = VAEGAN(num_layers=5, batch_size=1, data_size=1, num_epochs=1)
 MODLE_DIR = os.path.join(LOG_DIR, '{}_{}_final/'.format(data.NAME, model.name))
 LAYER_IDX = 3
-FILTER_IDX = 11
-LR = 10
-NUM_STEPS = 200
-medfilt_steps = 6
+FILTER_IDX = 18
+LR = 6
+NUM_STEPS = 300
+l2decay = 0.001
 
 x = tf.Variable(tf.random_normal([1, 128, 128, 3], stddev=10), name='x')
 
@@ -62,15 +61,10 @@ with tf.Session() as sess:
     for i in range(NUM_STEPS):
         sess.run([train_op])
         with tf.control_dependencies([train_op]):
-            tmp = x.eval()
-            # tmp = clip_by_value(tmp, clip_value_min=-1., clip_value_max=1.)
-            tmp = np.clip(tmp, -1, 1)
-
-            if not i % medfilt_steps:
-                tmp = median_filter(tmp, size=[1, 3, 3, 1])
+            x *= (1. - l2decay)
+            x = clip_by_value(x, clip_value_min=-1., clip_value_max=1.)
+            if not i % 100:
                 print(sess.run(loss))
-            assign_op = x.assign(tmp)
-            sess.run(assign_op)
 
     img = x.eval()
     img = deprocess_image(np.squeeze(img))

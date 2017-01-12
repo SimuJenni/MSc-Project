877762fcdf13a65fc55785aaa7fb49e811c98826
SimuJenni <simujenni@gmail.com>
2017-01-12 18:00:06 +0100

    Clean up

--- tf_slim/test_recon_cifar10.py
+++ tf_slim/test_recon_cifar10.py
@@ -4,7 +4,7 @@ import os
 
 import tensorflow as tf
 
-from ToonNet_dual import VAEGAN
+from ToonNet_cifar import VAEGAN
 from constants import LOG_DIR
 from datasets import cifar10
 from preprocess import preprocess_toon_test

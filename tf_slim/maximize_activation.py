8b00dafd44de89477a9adfbc67c065f957f21fb2
SimuJenni <simujenni@gmail.com>
2017-01-13 20:52:21 +0100

    finetune stl

--- tf_slim/maximize_activation.py
+++ tf_slim/maximize_activation.py
@@ -21,7 +21,9 @@ def deprocess_image(x):
     return x
 
 
-def max_activity_img(layer_id, filter_id, lr, ckpt, reuse=None):
+def max_activity_img(layer_id, num_filters, lr, ckpt, reuse=None):
+    imgs = [None for i in range(num_filters)]
+    losses = [0. for i in range(num_filters)]
     x = tf.Variable(tf.random_normal([1, 128, 128, 3], stddev=2), name='x')
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.1)
 
@@ -35,37 +37,42 @@ def max_activity_img(layer_id, filter_id, lr, ckpt, reuse=None):
         saver = tf.train.Saver(var_list=vars)
         saver.restore(sess, ckpt.model_checkpoint_path)
 
-        loss = -tf.reduce_sum(layers[layer_id - 1][:, :, :, filter_id])
-        loss += 0.0001*tf.reduce_sum(tf.square(x))
-        opt = tf.train.GradientDescentOptimizer(learning_rate=lr)
-        train_op = opt.minimize(loss, var_list=[x])
-        print('Layer: {} Filter: {} Learning-Rate: {}'.format(layer_id, filter_id, lr))
+        for f in range(num_filters):
+            sess.run(x.assign(tf.random_normal([1, 128, 128, 3], stddev=2)))
+            loss = -tf.reduce_sum(layers[layer_id - 1][:, :, :, f])
+            loss += 0.0001*tf.reduce_sum(tf.square(x))
+            opt = tf.train.GradientDescentOptimizer(learning_rate=lr)
+            train_op = opt.minimize(loss, var_list=[x])
+            print('Layer: {} Filter: {} Learning-Rate: {}'.format(layer_id, f, lr))
 
-        for j in range(200):
-            tmp = x.eval()
-            tmp = clip_by_value(tmp, clip_value_min=-1., clip_value_max=1.)
-            sess.run(x.assign(tmp))
-            sess.run([train_op])
+            for j in range(200):
+                tmp = x.eval()
+                tmp = clip_by_value(tmp, clip_value_min=-1., clip_value_max=1.)
+                sess.run(x.assign(tmp))
+                sess.run([train_op])
 
-        img = x.eval()
-        img = deprocess_image(np.squeeze(img))
-        return img, sess.run(loss)
+            img = x.eval()
+            imgs[f] = deprocess_image(np.squeeze(img))
+            losses[f] = sess.run(loss)
+        return imgs, losses
 
 data = imagenet
 model = VAEGAN(num_layers=5, batch_size=1, data_size=1, num_epochs=1)
 MODLE_DIR = os.path.join(LOG_DIR, '{}_{}_final/'.format(data.NAME, model.name))
 ckpt = tf.train.get_checkpoint_state(MODLE_DIR)
-LAYER = 2
+LAYER = 1
 LR = 1
-FILTERS = [i for i in range(36)]
-imgs = [None for i in FILTERS]
-losses = [0. for i in FILTERS]
-for i, f in enumerate(FILTERS):
-    if i == 0:
-        reuse = None
-    else:
-        reuse = True
-    imgs[i], losses[i] = max_activity_img(LAYER, f, LR, ckpt, reuse=reuse)
+# FILTERS = [i for i in range(36)]
+# imgs = [None for i in FILTERS]
+# losses = [0. for i in FILTERS]
+# for i, f in enumerate(FILTERS):
+#     if i == 0:
+#         reuse = None
+#     else:
+#         reuse = True
+#     imgs[i], losses[i] = max_activity_img(LAYER, f, LR, ckpt, reuse=reuse)
+
+imgs, losses = max_activity_img(LAYER, 36, LR, ckpt)
 
 print(losses)
 imgs = [x for (y,x) in sorted(zip(losses, imgs))]

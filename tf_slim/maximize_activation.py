538d4d093335afa56e169fbeeb02cd931694e298
SimuJenni <simujenni@gmail.com>
2017-01-13 01:43:59 +0100

    Clean up

--- tf_slim/maximize_activation.py
+++ tf_slim/maximize_activation.py
@@ -55,9 +55,9 @@ data = imagenet
 model = VAEGAN(num_layers=5, batch_size=1, data_size=1, num_epochs=1)
 MODLE_DIR = os.path.join(LOG_DIR, '{}_{}_final/'.format(data.NAME, model.name))
 ckpt = tf.train.get_checkpoint_state(MODLE_DIR)
-LAYER = 5
+LAYER = 1
 LR = 1
-FILTERS = [i for i in range(4)]
+FILTERS = [i for i in range(16)]
 imgs = [None for i in FILTERS]
 losses = [0. for i in FILTERS]
 for i, f in enumerate(FILTERS):

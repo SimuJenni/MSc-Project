849b90a3a34523b7072c6c44f5d14c0b9f448bbc
SimuJenni <simujenni@gmail.com>
2017-01-15 11:20:58 +0100

    finetune stl

--- tf_slim/fine_tune_stl10.py
+++ tf_slim/fine_tune_stl10.py
@@ -25,7 +25,7 @@ num_layers = 4
 model = VAEGAN(num_layers=num_layers, batch_size=256, data_size=data.SPLITS_TO_SIZES['train'], num_epochs=400)
 TARGET_SHAPE = [96, 96, 3]
 TEST_WHILE_TRAIN = True
-NUM_CONV_TRAIN = 3
+NUM_CONV_TRAIN = 2
 TRAIN_SET = 'train_fold_0'
 pre_trained_grad_weight = [0.5 * 0.5 ** i for i in range(NUM_CONV_TRAIN)]
 

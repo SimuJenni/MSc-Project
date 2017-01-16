dee8a7f16118a988e2cce2cd235b268e3fcfb705
SimuJenni <simujenni@gmail.com>
2017-01-16 09:20:00 +0100

    finetune stl

--- tf_slim/test_classifier_stl10.py
+++ tf_slim/test_classifier_stl10.py
@@ -17,67 +17,68 @@ data = stl10
 model = VAEGAN(num_layers=4, batch_size=1000)
 TARGET_SHAPE = [96, 96, 3]
 RESIZE_SIZE = 96
-NUM_CONV_TRAIN = 3
+NUM_CONV_TRAIN = 0
 
-if finetuned:
-    MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_train_fold_0/'.format(
-        data.NAME, model.name, net_type, NUM_CONV_TRAIN))
-    LOG_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_train_fold_0/'.format(
-        data.NAME, model.name, net_type, NUM_CONV_TRAIN))
-else:
-    MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_classifier/'.format(data.NAME, model.name))
-    LOG_PATH = os.path.join(LOG_DIR, '{}_{}_classifier_eval/'.format(data.NAME, model.name))
+for fold in range(10):
+    if finetuned:
+        MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_train_fold_{}/'.format(
+            data.NAME, model.name, net_type, NUM_CONV_TRAIN, fold))
+        LOG_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_train_fold_{}/'.format(
+            data.NAME, model.name, net_type, NUM_CONV_TRAIN, fold))
+    else:
+        MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_classifier/'.format(data.NAME, model.name))
+        LOG_PATH = os.path.join(LOG_DIR, '{}_{}_classifier_eval/'.format(data.NAME, model.name))
 
-print('Evaluating model: {}'.format(MODEL_PATH))
+    print('Evaluating model: {}'.format(MODEL_PATH))
 
-sess = tf.Session()
-tf.logging.set_verbosity(tf.logging.DEBUG)
+    sess = tf.Session()
+    tf.logging.set_verbosity(tf.logging.DEBUG)
 
-g = tf.Graph()
-with sess.as_default():
-    with g.as_default():
-        global_step = slim.create_global_step()
+    g = tf.Graph()
+    with sess.as_default():
+        with g.as_default():
+            global_step = slim.create_global_step()
 
-        with tf.device('/cpu:0'):
-            # Get test-data
-            test_set = data.get_split('test')
-            provider = slim.dataset_data_provider.DatasetDataProvider(test_set, num_readers=1, shuffle=False)
-            [img_test, label_test] = provider.get(['image', 'label'])
+            with tf.device('/cpu:0'):
+                # Get test-data
+                test_set = data.get_split('test_fold_{}'.format(fold))
+                provider = slim.dataset_data_provider.DatasetDataProvider(test_set, num_readers=1, shuffle=False)
+                [img_test, label_test] = provider.get(['image', 'label'])
 
-            # Pre-process data
-            img_test = preprocess_finetune_test(img_test,
-                                                output_height=TARGET_SHAPE[0],
-                                                output_width=TARGET_SHAPE[1],
-                                                resize_side=RESIZE_SIZE)
-            # Make batches
-            imgs_test, labels_test = tf.train.batch(
-                [img_test, label_test],
-                batch_size=model.batch_size, num_threads=1)
+                # Pre-process data
+                img_test = preprocess_finetune_test(img_test,
+                                                    output_height=TARGET_SHAPE[0],
+                                                    output_width=TARGET_SHAPE[1],
+                                                    resize_side=RESIZE_SIZE)
+                # Make batches
+                imgs_test, labels_test = tf.train.batch(
+                    [img_test, label_test],
+                    batch_size=model.batch_size, num_threads=1)
 
-        # Get predictions
-        preds_test = model.classifier(imgs_test, None, data.NUM_CLASSES, training=False,
-                                      fine_tune=finetuned, type=net_type)
+            # Get predictions
+            preds_test = model.classifier(imgs_test, None, data.NUM_CLASSES, training=False,
+                                          fine_tune=finetuned, type=net_type)
 
-        # Compute predicted label for accuracy
-        preds_test = tf.argmax(preds_test, 1)
+            # Compute predicted label for accuracy
+            preds_test = tf.argmax(preds_test, 1)
 
-        # Choose the metrics to compute:
-        names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
-            'accuracy': slim.metrics.streaming_accuracy(preds_test, labels_test),
-            'precision': slim.metrics.streaming_precision(preds_test, labels_test),
-            'recall': slim.metrics.streaming_recall(preds_test, labels_test),
-        })
+            # Choose the metrics to compute:
+            names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
+                'accuracy': slim.metrics.streaming_accuracy(preds_test, labels_test),
+                'precision': slim.metrics.streaming_precision(preds_test, labels_test),
+                'recall': slim.metrics.streaming_recall(preds_test, labels_test),
+            })
 
-        # Create the summary ops such that they also print out to std output:
-        summary_ops = []
-        for metric_name, metric_value in names_to_values.iteritems():
-            op = tf.scalar_summary(metric_name, metric_value)
-            op = tf.Print(op, [metric_value], metric_name)
-            summary_ops.append(op)
+            # Create the summary ops such that they also print out to std output:
+            summary_ops = []
+            for metric_name, metric_value in names_to_values.iteritems():
+                op = tf.scalar_summary(metric_name, metric_value)
+                op = tf.Print(op, [metric_value], metric_name)
+                summary_ops.append(op)
 
-        num_eval_steps = int(data.SPLITS_TO_SIZES['test'] / model.batch_size)
-        slim.evaluation.evaluation_loop('', MODEL_PATH, LOG_PATH,
-                                        num_evals=num_eval_steps,
-                                        max_number_of_evaluations=1,
-                                        eval_op=names_to_updates.values(),
-                                        summary_op=tf.merge_summary(summary_ops))
+            num_eval_steps = int(data.SPLITS_TO_SIZES['test'] / model.batch_size)
+            slim.evaluation.evaluation_loop('', MODEL_PATH, LOG_PATH,
+                                            num_evals=num_eval_steps,
+                                            max_number_of_evaluations=1,
+                                            eval_op=names_to_updates.values(),
+                                            summary_op=tf.merge_summary(summary_ops))

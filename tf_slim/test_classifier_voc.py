8c42df11e78c508e3475230a7f46bf3c996f25e6
SimuJenni <simujenni@gmail.com>
2017-01-17 23:09:07 +0100

    finetune stl

--- tf_slim/test_classifier_voc.py
+++ tf_slim/test_classifier_voc.py
@@ -15,14 +15,14 @@ finetuned = True
 net_type = 'discriminator'
 data = voc
 model = VAEGAN(num_layers=5, batch_size=500)
-TARGET_SHAPE = [128, 128, 3]
-RESIZE_SIZE = 128
-NUM_CONV_TRAIN = 0
+TARGET_SHAPE = [160, 160, 3]
+RESIZE_SIZE = 160
+NUM_CONV_TRAIN = 3
 
 if finetuned:
-    MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_trainval_stl/'.format(
+    MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_trainval_imnet/'.format(
         data.NAME, model.name, net_type, NUM_CONV_TRAIN))
-    LOG_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_trainval_stl/'.format(
+    LOG_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_trainval_imnet/'.format(
         data.NAME, model.name, net_type, NUM_CONV_TRAIN))
 else:
     MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_classifier/'.format(data.NAME, model.name))

ca7450def3e6c74fb0ff93fbc075009942cfac12
SimuJenni <simujenni@gmail.com>
2017-01-18 13:44:32 +0100

    finetune stl

--- tf_slim/fine_tune_voc2007.py
+++ tf_slim/fine_tune_voc2007.py
@@ -24,7 +24,7 @@ data = voc
 num_layers = 5
 model = VAEGAN(num_layers=num_layers, batch_size=192)
 TARGET_SHAPE = [128, 128, 3]
-num_ep = 40
+num_ep = 80
 TEST_WHILE_TRAIN = False
 NUM_CONV_TRAIN = 3
 TRAIN_SET = 'train'

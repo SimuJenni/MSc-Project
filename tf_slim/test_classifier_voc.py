f69167e45d98bef4942afc1d6a7f35ca2de4994b
SimuJenni <simujenni@gmail.com>
2017-01-19 23:05:57 +0100

    finetune stl

--- tf_slim/test_classifier_voc.py
+++ tf_slim/test_classifier_voc.py
@@ -17,7 +17,7 @@ net_type = 'discriminator'
 data = voc
 model = VAEGAN(num_layers=5, batch_size=200)
 TARGET_SHAPE = [128, 128, 3]
-NUM_CONV_TRAIN = 4
+NUM_CONV_TRAIN = 3
 TRAIN_SET = 'train'
 TEST_SET = 'val'
 

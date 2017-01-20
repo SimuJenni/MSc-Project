7c6f579b90541c2470afd57c0d5735f989d64542
SimuJenni <simujenni@gmail.com>
2017-01-21 00:54:34 +0100

    finetune stl

--- tf_slim/train_imagenet.py
+++ tf_slim/train_imagenet.py
@@ -19,7 +19,7 @@ data = imagenet
 TRAIN_SET_NAME = 'train'
 TEST_SET_NAME = 'validation'
 num_epochs = 30
-model = VAEGAN(num_layers=5, batch_size=128)
+model = VAEGAN(num_layers=5, batch_size=96)
 TARGET_SHAPE = [128, 128, 3]
 LR = 0.0002
 SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_final/'.format(data.NAME, model.name))

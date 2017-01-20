acf6ad157fe91375b06c9e6facaa86002b844681
SimuJenni <simujenni@gmail.com>
2017-01-20 16:23:09 +0100

    finetune stl

--- tf_slim/fine_tune_voc2007.py
+++ tf_slim/fine_tune_voc2007.py
@@ -18,7 +18,7 @@ from utils import assign_from_checkpoint_fn, montage_tf
 slim = tf.contrib.slim
 
 # Setup
-fine_tune = True
+fine_tune = False
 net_type = 'discriminator'
 data = voc
 num_layers = 5
@@ -58,7 +58,7 @@ with sess.as_default():
 
             # Pre-process data
             img_train = preprocess_voc(img_train, output_height=TARGET_SHAPE[0], output_width=TARGET_SHAPE[1],
-                                       augment_color=True, aspect_ratio_range=[0.5, 1.5], area_range=[0.1, 1.0])
+                                       augment_color=True, aspect_ratio_range=[0.5, 1.5], area_range=[0.05, 1.0])
 
             # Make batches
             imgs_train, labels_train = tf.train.batch([img_train, label_train],

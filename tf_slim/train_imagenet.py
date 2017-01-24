f2fb2e64798476b9197c844aedd06a9e31dd1df5
SimuJenni <simujenni@gmail.com>
2017-01-24 08:38:13 +0100

    test cassifiers

--- tf_slim/train_imagenet.py
+++ tf_slim/train_imagenet.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 
-from ToonNet_Alex import VAEGAN
+from ToonNet_Alex2 import VAEGAN
 from constants import LOG_DIR
 from datasets import imagenet
 from preprocess import preprocess_toon_train, preprocess_toon_test

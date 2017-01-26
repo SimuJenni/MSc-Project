ec6f7e91259418fc7431305a559eeaec2b6dcfb4
SimuJenni <simujenni@gmail.com>
2017-01-26 22:26:22 +0100

    test cassifiers

--- tf_slim/fine_tune_imagenet.py
+++ tf_slim/fine_tune_imagenet.py
@@ -48,7 +48,7 @@ with sess.as_default():
 
             # Get the training dataset
             train_set = data.get_split('train', dataset_dir=IMAGENET_TF_DATADIR)
-            provider = slim.dataset_data_provider.DatasetDataProvider(train_set, num_readers=8,
+            provider = slim.dataset_data_provider.DatasetDataProvider(train_set, num_readers=16,
                                                                       common_queue_capacity=16 * model.batch_size,
                                                                       common_queue_min=8 * model.batch_size)
             [img_train, label_train] = provider.get(['image', 'label'])
@@ -59,7 +59,7 @@ with sess.as_default():
 
             # Make batches
             imgs_train, labels_train = tf.train.batch([img_train, label_train], batch_size=model.batch_size,
-                                                      num_threads=8, capacity=4 * model.batch_size)
+                                                      num_threads=16, capacity=4 * model.batch_size)
 
             if TEST_WHILE_TRAIN:
                 # Get test-data

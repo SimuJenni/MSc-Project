dcdb4ae8886ff501e85fd1f8f801236719394f1b
SimuJenni <simujenni@gmail.com>
2017-01-31 19:49:06 +0100

    test cassifiers

--- tf_slim/fine_tune_imagenet.py
+++ tf_slim/fine_tune_imagenet.py
@@ -25,7 +25,7 @@ num_layers = 5
 model = VAEGAN(num_layers=num_layers, batch_size=256)
 TARGET_SHAPE = [224, 224, 3]
 TEST_WHILE_TRAIN = False
-NUM_CONV_TRAIN = 1
+NUM_CONV_TRAIN = 0
 num_epochs = 60
 
 CHECKPOINT = 'model.ckpt-600542'

959076ad1e2e892bf7828456b19c5f24f9e1f60c
SimuJenni <simujenni@gmail.com>
2017-02-01 12:02:03 +0100

    test cassifiers

--- tf_slim/test_classifier_voc.py
+++ tf_slim/test_classifier_voc.py
@@ -16,8 +16,8 @@ finetuned = True
 net_type = 'discriminator'
 data = voc
 model = VAEGAN(num_layers=5, batch_size=1)
-# TARGET_SHAPE = [224, 224, 3]
-TARGET_SHAPE = [128, 128, 3]
+TARGET_SHAPE = [224, 224, 3]
+# TARGET_SHAPE = [128, 128, 3]
 NUM_CONV_TRAIN = 4
 TRAIN_SET = 'trainval'
 TEST_SET = 'test'

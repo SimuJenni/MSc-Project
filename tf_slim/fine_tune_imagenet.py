920950b7b7ea8e577f173601a3ce0073a5801aec
SimuJenni <simujenni@gmail.com>
2017-02-03 18:58:46 +0100

    SGD instead of ADAM

--- tf_slim/fine_tune_imagenet.py
+++ tf_slim/fine_tune_imagenet.py
@@ -25,9 +25,9 @@ num_layers = 5
 model = VAEGAN(num_layers=num_layers, batch_size=256)
 TARGET_SHAPE = [224, 224, 3]
 TEST_WHILE_TRAIN = False
-NUM_CONV_TRAIN = 4
+NUM_CONV_TRAIN = 3
 num_epochs = 90
-num_preprocess_threads = 32
+num_preprocess_threads = 16
 
 CHECKPOINT = 'model.ckpt-600542'
 MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_final/{}'.format(data.NAME, model.name, CHECKPOINT))

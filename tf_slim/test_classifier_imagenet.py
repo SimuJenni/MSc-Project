7f3797949b6b659fbda849f9ce861c61f4c88ec3
SimuJenni <simujenni@gmail.com>
2017-02-07 17:08:09 +0100

    SGD instead of ADAM

--- tf_slim/test_classifier_imagenet.py
+++ tf_slim/test_classifier_imagenet.py
@@ -12,13 +12,13 @@ from preprocess import preprocess_finetune_test
 slim = tf.contrib.slim
 
 # Setup
-finetuned = True
+finetuned = False
 net_type = 'discriminator'
 data = imagenet
 model = VAEGAN(num_layers=5, batch_size=500)
 TARGET_SHAPE = [224, 224, 3]
 RESIZE_SIZE = 224
-NUM_CONV_TRAIN = 2
+NUM_CONV_TRAIN = 4
 
 if finetuned:
     MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_sgd/'.format(data.NAME, model.name,

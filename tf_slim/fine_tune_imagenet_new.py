afb419c1aff94de9dd189f1bb01606392c77425b
SimuJenni <simujenni@gmail.com>
2017-02-26 11:27:14 +0100

    SGD instead of ADAM

--- tf_slim/fine_tune_imagenet_new.py
+++ tf_slim/fine_tune_imagenet_new.py
@@ -17,7 +17,7 @@ from constants import IMAGENET_TF_256_DATADIR
 slim = tf.contrib.slim
 
 # Setup
-fine_tune = False
+fine_tune = True
 data = imagenet
 num_layers = 5
 model = VAEGAN(num_layers=num_layers, batch_size=256)
@@ -29,8 +29,8 @@ num_preprocess_threads = 16
 CHECKPOINT = 'model.ckpt-800721'
 MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_final/{}'.format(data.NAME, model.name, CHECKPOINT))
 if fine_tune:
-    SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain_final_sgd256/'.format(data.NAME, model.name,
-                                                                                      NUM_CONV_TRAIN))
+    SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain_final_sgd256_nomusub/'.format(data.NAME, model.name,
+                                                                                              NUM_CONV_TRAIN))
 else:
     SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_classifier_sgd256_nomusub/'.format(data.NAME, model.name))
 

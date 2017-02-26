656ed579da92f17eb7c56ace93050df98517e9a0
SimuJenni <simujenni@gmail.com>
2017-02-26 13:42:14 +0100

    SGD instead of ADAM

--- tf_slim/test_recon_imagenet.py
+++ tf_slim/test_recon_imagenet.py
@@ -4,7 +4,7 @@ import os
 
 import tensorflow as tf
 
-from ToonNet_Alex_comp import VAEGAN
+from ToonNet_Alex_comp_centerBN_relu import VAEGAN
 from constants import LOG_DIR
 from datasets import imagenet
 from preprocess import preprocess_toon_test

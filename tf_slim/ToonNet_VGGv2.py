8976c8e341cc01335a26873e842971ecef7d87af
SimuJenni <simujenni@gmail.com>
2017-03-01 21:38:09 +0100

    SGD instead of ADAM

--- tf_slim/ToonNet_VGGv2.py
+++ tf_slim/ToonNet_VGGv2.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from tensorflow.python.ops import math_ops
+from preprocess import dist_color
 
 from layers import lrelu, up_conv2d, sample, merge, add_noise_plane
 
@@ -55,10 +56,11 @@ class VAEGAN:
         # noise_img = img + tf.random_normal(shape=noise_shape, stddev=0.2*noise_amount(num_train_steps))
         # noise_img = tf.clip_by_value(noise_img, -1.0, 1.0)
 
-        scale = (0.5-0.49*noise_amount(num_train_steps)) * 255.
+        scale = (0.75-0.7*noise_amount(num_train_steps)) * 255.
         noise_img = 0.5*img + 0.5
         noise_img = tf.to_float(tf.floor(noise_img * scale))
         noise_img = 2.*noise_img/scale-1.0
+        noise_img = dist_color(noise_img)
         noise_img = tf.clip_by_value(noise_img, -1.0, 1.0)
 
         disc_in = merge(img, noise_img, dim=0)

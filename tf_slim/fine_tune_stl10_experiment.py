65a9a17dcfd2cbbb450eaabbd69bfa152363cd9f
SimuJenni <simujenni@gmail.com>
2017-03-02 13:52:17 +0100

    SGD instead of ADAM

--- tf_slim/fine_tune_stl10_experiment.py
+++ tf_slim/fine_tune_stl10_experiment.py
@@ -9,7 +9,7 @@ from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.framework import ops
 
-from ToonNet_VGG import VAEGAN
+from ToonNet_VGGv2 import VAEGAN
 from constants import LOG_DIR
 from datasets import stl10
 from preprocess import preprocess_finetune_train, preprocess_finetune_test

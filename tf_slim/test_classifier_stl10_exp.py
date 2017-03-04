4548e1c51476e4d4d0b07f58e4f2120899f6bc88
SimuJenni <simujenni@gmail.com>
2017-03-04 12:07:50 +0100

    train disc on stl10 experiments

--- tf_slim/test_classifier_stl10_exp.py
+++ tf_slim/test_classifier_stl10_exp.py
@@ -11,7 +11,7 @@ from preprocess import preprocess_finetune_test
 slim = tf.contrib.slim
 
 # Setup
-finetuned = True
+finetuned = False
 net_type = 'discriminator'
 data = stl10
 model = VAEGAN(num_layers=4, batch_size=500)

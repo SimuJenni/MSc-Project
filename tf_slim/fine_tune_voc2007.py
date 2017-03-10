73328ccf5e797582933d03ecdaaafe7bcf443bbe
SimuJenni <simujenni@gmail.com>
2017-03-10 01:58:18 +0100

    testing rcnn

--- tf_slim/fine_tune_voc2007.py
+++ tf_slim/fine_tune_voc2007.py
@@ -8,7 +8,7 @@ from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 from tensorflow.python.framework import ops
 
-from ToonNet_AlexV2_voc import VAEGAN
+from ToonNet_AlexV2 import VAEGAN
 from constants import LOG_DIR
 from datasets import voc, imagenet
 from preprocess import preprocess_voc
@@ -22,12 +22,12 @@ fine_tune = True
 net_type = 'discriminator'
 data = voc
 num_layers = 5
-model = VAEGAN(num_layers=num_layers, batch_size=64)
+model = VAEGAN(num_layers=num_layers, batch_size=128)
 TARGET_SHAPE = [224, 224, 3]
 num_ep = 500
 NUM_CONV_TRAIN = 5
 TRAIN_SET = 'trainval'
-num_preprocess_threads = 4
+num_preprocess_threads = 8
 
 CHECKPOINT = 'model.ckpt-800722'
 MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_80ep/{}'.format(imagenet.NAME, model.name, CHECKPOINT))
@@ -60,7 +60,7 @@ with sess.as_default():
 
                 # Pre-process data
                 img_train = preprocess_voc(img_train, output_height=TARGET_SHAPE[0], output_width=TARGET_SHAPE[1],
-                                           augment_color=False)
+                                           augment_color=True)
                 images_and_labels.append([img_train, label_train])
 
             # Make batches
@@ -94,13 +94,10 @@ with sess.as_default():
 
         # Define learning parameters
         num_train_steps = 80000
-        boundaries = [np.int64(10000), np.int64(20000), np.int64(30000), np.int64(40000), np.int64(50000),
-                      np.int64(60000), np.int64(70000)]
-        values = [0.001, 0.0005, 0.00025, 0.000125, 0.0000625, 0.00003125, 0.000015625, 0.0000078125]
-        learning_rate = tf.train.piecewise_constant(global_step, boundaries=boundaries, values=values)
+        learning_rate = tf.train.polynomial_decay(0.001, global_step, num_train_steps, end_learning_rate=0.0)
 
         # Define optimizer
-        optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate, momentum=0.9, use_nesterov=True)
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=0.9)
 
         # Create training operation
         # if fine_tune:

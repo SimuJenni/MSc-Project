9a5799efba9c9b4e0f52304da5ac864f91c90122
SimuJenni <simujenni@gmail.com>
2017-03-10 19:33:05 +0100

    test rcnn

--- tf_slim/train_stl10.py
+++ tf_slim/train_stl10.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from tensorflow.python.ops import control_flow_ops
 from tensorflow.python.ops import math_ops
 
-from ToonNet_VGG import VAEGAN
+from ToonNet_Alex_stl import VAEGAN
 from constants import LOG_DIR
 from datasets import stl10
 from preprocess import preprocess_toon_train

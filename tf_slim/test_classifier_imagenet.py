21da494be930050c02c9de40bf76a732dbfac272
SimuJenni <simujenni@gmail.com>
2017-03-12 20:22:50 +0100

    new stl exp

--- tf_slim/test_classifier_imagenet.py
+++ tf_slim/test_classifier_imagenet.py
@@ -18,7 +18,7 @@ data = imagenet
 model = VAEGAN(num_layers=5, batch_size=500)
 TARGET_SHAPE = [224, 224, 3]
 RESIZE_SIZE = 224
-NUM_CONV_TRAIN = 3
+NUM_CONV_TRAIN = 4
 
 if finetuned:
     MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain_final/'.format(data.NAME, model.name,

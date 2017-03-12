6cb48f716769684bc7c67a649d3a38126ca1b5da
SimuJenni <simujenni@gmail.com>
2017-03-12 09:29:25 +0100

    test supervised voc

--- tf_slim/fine_tune_stl10_experiment.py
+++ tf_slim/fine_tune_stl10_experiment.py
@@ -135,4 +135,4 @@ def fine_tune_model(data, num_layers, num_conv_train, target_shape, checkpoint, 
                                 log_every_n_steps=100)
 
 
-fine_tune_model(stl10, 4, 0, [96, 96, 3], 'model.ckpt-150002', 'train', 64, 400, fine_tune=True)
+fine_tune_model(stl10, 4, 0, [96, 96, 3], 'model.ckpt-150002', 'train', 64, 300, fine_tune=True)

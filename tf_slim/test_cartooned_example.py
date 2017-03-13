3a09318947807db8b5da1b3e8233317eb42e8ec0
SimuJenni <simujenni@gmail.com>
2017-03-13 16:10:48 +0100

    test face edge

--- tf_slim/test_cartooned_example.py
+++ tf_slim/test_cartooned_example.py
@@ -44,9 +44,9 @@ with sess.as_default():
         global_step = slim.create_global_step()
 
         # toon_in = tf.placeholder(tf.float32, shape=np.shape(toon_im))
-        toon_in = tf.Variable(toon_im, name="toon", trainable=False, expected_shape=np.shape(toon_im))
+        toon_in = tf.constant(toon_im, dtype=tf.float32, shape=np.shape(toon_im), name='toon')
         # edge_in = tf.placeholder(tf.float32, shape=np.shape(edge_im))
-        edge_in = tf.Variable(edge_im, name="edge", trainable=False, expected_shape=np.shape(edge_im))
+        edge_in = tf.constant(edge_im, dtype=tf.float32, shape=np.shape(edge_im), name='edge')
 
 
         with tf.device('/cpu:0'):

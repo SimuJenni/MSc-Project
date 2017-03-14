d90c31d5e1452f0cce089d993bd716577ed416ae
SimuJenni <simujenni@gmail.com>
2017-03-14 15:52:38 +0100

    test face edge

--- tf_slim/train_stl10_exp5.py
+++ tf_slim/train_stl10_exp5.py
@@ -9,7 +9,7 @@ from constants import LOG_DIR
 from datasets import stl10
 from preprocess import preprocess_toon_train
 from tf_slim.utils import get_variables_to_train
-from utils import assign_from_checkpoint_fn, montage_tf
+from utils import montage_tf
 
 slim = tf.contrib.slim
 
@@ -17,8 +17,8 @@ slim = tf.contrib.slim
 data = stl10
 TRAIN_SET_NAME = 'train_unlabeled'
 TEST_SET_NAME = 'test'
-model = VAEGAN(num_layers=4, batch_size=200)
-num_epochs = 300
+model = VAEGAN(num_layers=4, batch_size=256)
+num_epochs = 200
 TARGET_SHAPE = [64, 64, 3]
 LR = 0.0002
 SAVE_DIR = os.path.join(LOG_DIR, '{}_{}_exp5/'.format(data.NAME, model.name))
@@ -67,6 +67,13 @@ with sess.as_default():
         losses_disc += slim.losses.get_regularization_losses(disc_loss_scope)
         disc_loss = math_ops.add_n(losses_disc, name='disc_total_loss')
 
+        # Define the losses for AE training
+        ae_loss_scope = 'ae_loss'
+        l2_ae = slim.losses.sum_of_squares(img_rec, imgs_train, scope=ae_loss_scope, weight=30)
+        losses_ae = slim.losses.get_losses(ae_loss_scope)
+        losses_ae += slim.losses.get_regularization_losses(ae_loss_scope)
+        ae_loss = math_ops.add_n(losses_ae, name='ae_total_loss')
+
         # Define the losses for generator training
         gen_loss_scope = 'gen_loss'
         dL_gen = slim.losses.softmax_cross_entropy(disc_out, labels_gen, scope=gen_loss_scope, weight=1.0)
@@ -88,6 +95,7 @@ with sess.as_default():
         if update_ops:
             updates = tf.group(*update_ops)
             gen_loss = control_flow_ops.with_dependencies([updates], gen_loss)
+            ae_loss = control_flow_ops.with_dependencies([updates], ae_loss)
             disc_loss = control_flow_ops.with_dependencies([updates], disc_loss)
 
         # Define learning parameters
@@ -102,6 +110,7 @@ with sess.as_default():
         tf.scalar_summary('losses/disc-loss generator', dL_gen)
         tf.scalar_summary('losses/l2 generator', l2_gen)
         tf.scalar_summary('losses/L2 mu', l2_mu)
+        tf.scalar_summary('losses/l2 auto-encoder', l2_ae)
         tf.image_summary('images/generator', montage_tf(gen_rec, NUM_IMG_SUMMARY, NUM_IMG_SUMMARY), max_images=1)
         tf.image_summary('images/ae', montage_tf(img_rec, NUM_IMG_SUMMARY, NUM_IMG_SUMMARY), max_images=1)
         tf.image_summary('images/ground-truth', montage_tf(imgs_train, NUM_IMG_SUMMARY, NUM_IMG_SUMMARY),
@@ -115,17 +124,23 @@ with sess.as_default():
         train_op_gen = slim.learning.create_train_op(gen_loss, optimizer, variables_to_train=vars2train_gen,
                                                      global_step=global_step, summarize_gradients=False)
 
+        # Auto-encoder training operation
+        scopes_ae = 'encoder, decoder'
+        vars2train_ae = get_variables_to_train(trainable_scopes=scopes_ae)
+        train_op_ae = slim.learning.create_train_op(ae_loss, optimizer, variables_to_train=vars2train_ae,
+                                                    global_step=global_step, summarize_gradients=False)
+
         # Discriminator training operation
         scopes_disc = 'discriminator'
         vars2train_disc = get_variables_to_train(trainable_scopes=scopes_disc)
         train_op_disc = slim.learning.create_train_op(disc_loss, optimizer, variables_to_train=vars2train_disc,
                                                       global_step=global_step, summarize_gradients=False)
 
-
         # Start training
-        slim.learning.train(train_op_gen + train_op_disc,
+        slim.learning.train(train_op_ae + train_op_gen + train_op_disc,
                             SAVE_DIR,
                             save_summaries_secs=300,
                             save_interval_secs=3000,
                             log_every_n_steps=100,
                             number_of_steps=num_train_steps)
+

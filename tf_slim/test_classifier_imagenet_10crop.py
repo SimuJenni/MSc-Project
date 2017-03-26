428ea095c92a00cf901a7f142ea4bbde6376495b
SimuJenni <simujenni@gmail.com>
2017-03-26 13:18:14 +0200

    Test transfer without batchnorm training

--- tf_slim/test_classifier_imagenet_10crop.py
+++ tf_slim/test_classifier_imagenet_10crop.py
@@ -44,7 +44,6 @@ with sess.as_default():
             [img_test, label_test] = provider.get(['image', 'label'])
             label_test -= data.LABEL_OFFSET
 
-            labels_test = tf.tile(label_test, [10, 1])
             imgs_test_t = tf.tile(tf.expand_dims(img_test, dim=0), [10, 1, 1, 1])
             imgs_test_p = tf.unpack(imgs_test_t, axis=0, num=10)
             imgs_test_p = [preprocess_imagenet_256_test(im, TARGET_SHAPE[0], TARGET_SHAPE[1]) for im in imgs_test_p]
@@ -60,7 +59,7 @@ with sess.as_default():
 
         # Choose the metrics to compute:
         names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
-            'accuracy': slim.metrics.streaming_accuracy(preds_test, labels_test),
+            'accuracy': slim.metrics.streaming_accuracy(preds_test, label_test),
         })
 
         # Create the summary ops such that they also print out to std output:

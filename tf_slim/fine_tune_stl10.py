1a82566e8b14e9ec70609da742c0a9d103dc348a
SimuJenni <simujenni@gmail.com>
2017-04-07 23:13:45 +0200

    TEsting fine-tune

--- tf_slim/fine_tune_stl10.py
+++ tf_slim/fine_tune_stl10.py
@@ -8,7 +8,7 @@ from utils import get_checkpoint_path
 model = ToonNet(num_layers=4, batch_size=200, vgg_discriminator=True)
 data = STL10()
 preprocessor = Preprocessor(target_shape=[64, 64, 3])
-trainer = ToonNet_Trainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=200, tag='refactored',
+trainer = ToonNet_Trainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=400, tag='refactored',
                           lr_policy='linear', optimizer='adam')
 chpt_path = get_checkpoint_path(trainer.get_save_dir())
 trainer.finetune_cv(chpt_path, num_conv2train=5, num_conv2init=5)

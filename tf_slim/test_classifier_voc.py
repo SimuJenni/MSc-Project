400cbe3de88fb01a7a9a87d180eeb8c9d201f194
SimuJenni <simujenni@gmail.com>
2017-04-10 17:43:58 +0200

    Testing finetune on Imagenet

--- tf_slim/test_classifier_voc.py
+++ tf_slim/test_classifier_voc.py
@@ -6,5 +6,5 @@ from Preprocessor import VOCPreprocessor
 model = ToonNet(num_layers=5, batch_size=1)
 data = VOC2007()
 preprocessor = VOCPreprocessor(target_shape=[224, 224, 3])
-tester = ToonNet_Tester(model, data, preprocessor)
+tester = ToonNet_Tester(model, data, preprocessor, tag='refactored')
 tester.test_classifier_voc(num_conv_trained=5)

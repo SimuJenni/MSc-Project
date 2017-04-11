27c1733f8a6f1fa141ff777fc86cf7f116c88059
SimuJenni <simujenni@gmail.com>
2017-04-11 09:59:26 +0200

    Testing STL-10 classifier

--- tf_slim/test_classifier_stl10.py
+++ tf_slim/test_classifier_stl10.py
@@ -4,7 +4,7 @@ from ToonNet_Tester import ToonNet_Tester
 from Preprocessor import Preprocessor
 
 for fold in range(10):
-    model = ToonNet(num_layers=4, batch_size=200)
+    model = ToonNet(num_layers=4, batch_size=200, vgg_discriminator=True)
     data = STL10()
     preprocessor = Preprocessor(target_shape=[96, 96, 3])
     tester = ToonNet_Tester(model, data, preprocessor, tag='refactored')

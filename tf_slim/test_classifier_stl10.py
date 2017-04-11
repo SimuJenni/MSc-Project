f44be9f862758a8be5715abb4cc1cb3c9c4af230
SimuJenni <simujenni@gmail.com>
2017-04-11 09:44:18 +0200

    Testing STL-10 classifier

--- tf_slim/test_classifier_stl10.py
+++ tf_slim/test_classifier_stl10.py
@@ -1,86 +1,12 @@
-from __future__ import print_function
-import os
-
-import tensorflow as tf
-
-from ToonNet_VGG import VAEGAN
-from constants import LOG_DIR
-from datasets import stl10
-from preprocess import preprocess_finetune_test
-
-slim = tf.contrib.slim
-
-# Setup
-finetuned = True
-net_type = 'discriminator'
-data = stl10
-model = VAEGAN(num_layers=4, batch_size=200)
-TARGET_SHAPE = [96, 96, 3]
-RESIZE_SIZE = 96
-NUM_CONV_TRAIN = 5
-use_test_set = True
+from ToonNet import ToonNet
+from datasets.STL10 import STL10
+from ToonNet_Tester import ToonNet_Tester
+from Preprocessor import Preprocessor
 
 for fold in range(10):
-    if finetuned:
-        MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_train_fold_{}/'.format(
-            data.NAME, model.name, net_type, NUM_CONV_TRAIN, fold))
-        LOG_PATH = os.path.join(LOG_DIR, '{}_{}_finetune_{}_Retrain{}_final_train_fold_{}/'.format(
-            data.NAME, model.name, net_type, NUM_CONV_TRAIN, fold))
-    else:
-        MODEL_PATH = os.path.join(LOG_DIR, '{}_{}_classifier/'.format(data.NAME, model.name))
-        LOG_PATH = os.path.join(LOG_DIR, '{}_{}_classifier_eval/'.format(data.NAME, model.name))
-
-    print('Evaluating model: {}'.format(MODEL_PATH))
-
-    sess = tf.Session()
-    tf.logging.set_verbosity(tf.logging.DEBUG)
-
-    g = tf.Graph()
-    with sess.as_default():
-        with g.as_default():
-            global_step = slim.create_global_step()
-
-            with tf.device('/cpu:0'):
-                # Get test-data
-                if use_test_set:
-                    test_set = data.get_split('test')
-                    num_eval_steps = int(data.SPLITS_TO_SIZES['test'] / model.batch_size)
-                else:
-                    test_set = data.get_split('test_fold_{}'.format(fold))
-                    num_eval_steps = int(data.SPLITS_TO_SIZES['test_fold_{}'.format(fold)] / model.batch_size)
-                provider = slim.dataset_data_provider.DatasetDataProvider(test_set, num_readers=1, shuffle=False)
-                [img_test, label_test] = provider.get(['image', 'label'])
-
-                # Pre-process data
-                img_test = preprocess_finetune_test(img_test,
-                                                    output_height=TARGET_SHAPE[0],
-                                                    output_width=TARGET_SHAPE[1])
-                # Make batches
-                imgs_test, labels_test = tf.train.batch(
-                    [img_test, label_test],
-                    batch_size=model.batch_size, num_threads=1)
-
-            # Get predictions
-            preds_test = model.classifier(imgs_test, None, data.NUM_CLASSES, training=False,
-                                          fine_tune=finetuned, type=net_type)
-
-            # Compute predicted label for accuracy
-            preds_test = tf.argmax(preds_test, 1)
-
-            # Choose the metrics to compute:
-            names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
-                'accuracy': slim.metrics.streaming_accuracy(preds_test, labels_test),
-            })
-
-            # Create the summary ops such that they also print out to std output:
-            summary_ops = []
-            for metric_name, metric_value in names_to_values.iteritems():
-                op = tf.scalar_summary(metric_name, metric_value)
-                op = tf.Print(op, [metric_value], metric_name)
-                summary_ops.append(op)
-
-            slim.evaluation.evaluation_loop('', MODEL_PATH, LOG_PATH,
-                                            num_evals=num_eval_steps,
-                                            max_number_of_evaluations=1,
-                                            eval_op=names_to_updates.values(),
-                                            summary_op=tf.merge_summary(summary_ops))
+    model = ToonNet(num_layers=4, batch_size=200)
+    data = STL10()
+    preprocessor = Preprocessor(target_shape=[96, 96, 3])
+    tester = ToonNet_Tester(model, data, preprocessor, tag='refactored')
+    tester.test_classifier_voc(num_conv_trained=5)
+    tester.test_classifier_cv(num_conv_trained=5, fold=fold)

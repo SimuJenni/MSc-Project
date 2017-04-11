1461e00c87caf0b712324513b10cec7cf617a920
SimuJenni <simujenni@gmail.com>
2017-04-11 10:39:34 +0200

    Testing STL-10 reconstruction

--- tf_slim/test_reconstruction_stl10.py
+++ tf_slim/test_reconstruction_stl10.py
@@ -5,6 +5,6 @@ from Preprocessor import Preprocessor
 
 model = ToonNet(num_layers=4, batch_size=200, vgg_discriminator=True)
 data = STL10()
-preprocessor = Preprocessor(target_shape=[96, 96, 3])
+preprocessor = Preprocessor(target_shape=[64, 64, 3])
 tester = ToonNet_Tester(model, data, preprocessor, tag='refactored')
 tester.test_reconstruction()

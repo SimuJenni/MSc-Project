ec2a03c42d46dbaf89c484bc7785d2446f385707
SimuJenni <simujenni@gmail.com>
2017-04-18 09:06:41 +0200

    Test and fix

--- tf_slim/fine_tune_imagenet.py
+++ tf_slim/fine_tune_imagenet.py
@@ -1,13 +1,13 @@
 from ToonNet import ToonNet
 from datasets.ImageNet import ImageNet
 from ToonNetTrainer import ToonNetTrainer
-from Preprocessor import VOCPreprocessor
+from Preprocessor import ImageNetPreprocessor
 from utils import get_checkpoint_path
 
 model = ToonNet(num_layers=5, batch_size=256)
 data = ImageNet()
-preprocessor = VOCPreprocessor(target_shape=[224, 224, 3])
+preprocessor = ImageNetPreprocessor(target_shape=[224, 224, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=90, tag='refactored',
                          lr_policy='step', optimizer='sgd+momentum')
 chpt_path = get_checkpoint_path(trainer.get_save_dir())
-trainer.transfer_finetune(chpt_path, num_conv2train=5, num_conv2init=0)
+trainer.transfer_finetune(chpt_path, num_conv2train=0, num_conv2init=5)

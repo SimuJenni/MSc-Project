1cec2fbb47391c05fc769a09ca4781a81de68592
SimuJenni <simujenni@gmail.com>
2017-04-23 22:14:30 +0200

    test conv5

--- tf_slim/test_classifier_imagenet.py
+++ tf_slim/test_classifier_imagenet.py
@@ -3,7 +3,7 @@ from datasets.ImageNet import ImageNet
 from ToonNetTester import ToonNetTester
 from Preprocessor import ImageNetPreprocessor
 
-model = ToonNet(num_layers=5, batch_size=512)
+model = ToonNet(num_layers=5, batch_size=128)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[224, 224, 3])
 tester = ToonNetTester(model, data, preprocessor, tag='refactored')

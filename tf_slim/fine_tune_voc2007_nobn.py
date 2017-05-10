7169594b013aca40579a5bd0e20b1aed586ac34d
SimuJenni <simujenni@gmail.com>
2017-05-10 19:38:33 +0200

    Finetune tf no bn

---
+++ tf_slim/fine_tune_voc2007_nobn.py
@@ -0,0 +1,48 @@
+from ToonNet_noBN import ToonNet_noBN
+from datasets.ImageNet import ImageNet
+from ToonNetTrainer import ToonNetTrainer
+from Preprocessor import Preprocessor, distort_image
+from utils import get_checkpoint_path
+from datasets.VOC2007 import VOC2007
+import tensorflow as tf
+
+
+class VOCPreprocessor_nobn(Preprocessor):
+    def __init__(self, target_shape, augment_color=True, aspect_ratio_range=(0.9, 1.1), area_range=(0.1, 1.0)):
+        Preprocessor.__init__(self, target_shape, augment_color, aspect_ratio_range, area_range)
+
+    def process_transfer_test(self, image):
+        # Select random crops
+        image = distort_image(image, self.target_shape[0], self.target_shape[1],
+                              self.aspect_ratio_range, self.area_range)
+
+        # Scale to [-1, 1]
+        image = tf.to_float(image) - 127.5
+
+        # Flip left-right
+        image = tf.image.random_flip_left_right(image)
+
+        return image
+
+    def process_transfer_test(self, image):
+        # Select random crops
+        image = distort_image(image, self.target_shape[0], self.target_shape[1],
+                              self.aspect_ratio_range, self.area_range)
+
+        # Scale to [-1, 1]
+        image = tf.to_float(image) - 127.5
+
+        # Flip left-right
+        image = tf.image.random_flip_left_right(image)
+
+        return image
+
+
+model = ToonNet_noBN(num_layers=5, batch_size=32)
+data = ImageNet()
+preprocessor = VOCPreprocessor_nobn(target_shape=[224, 224, 3])
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=500, tag='continuation',
+                         lr_policy='linear', optimizer='adam')
+chpt_path = get_checkpoint_path(trainer.get_save_dir())
+trainer.dataset = VOC2007()
+trainer.transfer_finetune(chpt_path, num_conv2train=5, num_conv2init=5)

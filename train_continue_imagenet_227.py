c5c8a741682f1817a465ccfa9d25142e3bc9102c
SimuJenni <simujenni@gmail.com>
2017-05-12 10:13:41 +0200

    Trying to retrain on 227 images

--- train_continue_imagenet_227.py
+++ train_continue_imagenet_227.py
@@ -2,15 +2,15 @@ import os
 
 from Preprocessor import ImageNetPreprocessor
 from train.ToonNetTrainer_cont_nobn import ToonNetTrainer
-import constants
+from constants import LOG_DIR
 from datasets.ImageNet import ImageNet
-from models.ToonNet_noBN import ToonNet_noBN
+from models.ToonNet import ToonNet
 
-model = ToonNet_noBN(num_layers=5, batch_size=128)
+model = ToonNet(num_layers=5, batch_size=128)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[227, 227, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=30, tag='227',
-                         lr_policy='linear', optimizer='adam')
-print(trainer.get_save_dir())
-all_ckpt ='/Data/Logs/ToonNet/imagenet_ToonNet_default_continuation/model.ckpt-900811'
+                         lr_policy='const', optimizer='adam')
+
+all_ckpt = os.path.join(LOG_DIR, 'imagenet_ToonNet_default_refactored/model.ckpt-800722')
 trainer.train(all_ckpt, all_ckpt)

6f9cb4373903c031af92746a8d26b5434a500155
SimuJenni <simujenni@gmail.com>
2017-05-15 23:03:43 +0200

    Trying another ToonNet setting

--- models/ToonNet_4x4.py
+++ models/ToonNet_4x4.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from layers import lrelu, up_conv2d, sample, merge, add_noise_plane
 
-DEFAULT_FILTER_DIMS = [32, 64, 128, 256, 512]
+DEFAULT_FILTER_DIMS = [64, 128, 256, 512, 512]
 REPEATS = [1, 2, 2, 2, 2]
 NOISE_CHANNELS = [1, 4, 8, 16, 32, 64, 128]
 
@@ -142,16 +142,14 @@ class ToonNet:
             Encoding of the input.
         """
         f_dims = DEFAULT_FILTER_DIMS
-        num_layers = min(self.num_layers, 4)
         with tf.variable_scope('generator', reuse=reuse):
             with slim.arg_scope(toon_net_argscope(padding='SAME', training=training, center=False)):
-                net = slim.conv2d(net, num_outputs=32, stride=1, scope='conv_0')
-                for l in range(0, num_layers):
+                for l in range(0, self.num_layers):
                     net = add_noise_plane(net, NOISE_CHANNELS[l], training=training)
                     net = slim.conv2d(net, num_outputs=f_dims[l], stride=2, scope='conv_{}'.format(l + 1))
 
                 encoded = net
-                mu = slim.conv2d(net, num_outputs=f_dims[num_layers - 1], scope='conv_mu', activation_fn=None,
+                mu = slim.conv2d(net, num_outputs=f_dims[self.num_layers - 1], scope='conv_mu', activation_fn=None,
                                  normalizer_fn=None)
                 if training:
                     net = sample(mu, 0.1*tf.ones_like(mu))
@@ -172,15 +170,13 @@ class ToonNet:
             Encoding of the input image.
         """
         f_dims = DEFAULT_FILTER_DIMS
-        num_layers = min(self.num_layers, 4)
         with tf.variable_scope('encoder', reuse=reuse):
             with slim.arg_scope(toon_net_argscope(padding='SAME', training=training, center=False)):
-                net = slim.conv2d(net, num_outputs=32, stride=1, scope='conv_0')
-                for l in range(0, num_layers):
+                for l in range(0, self.num_layers):
                     net = slim.conv2d(net, num_outputs=f_dims[l], stride=2, scope='conv_{}'.format(l + 1))
 
                 encoded = net
-                mu = slim.conv2d(net, num_outputs=f_dims[num_layers - 1], scope='conv_mu', activation_fn=None,
+                mu = slim.conv2d(net, num_outputs=f_dims[self.num_layers - 1], scope='conv_mu', activation_fn=None,
                                  normalizer_fn=None)
                 if training:
                     net = sample(mu, 0.1*tf.ones_like(mu))
@@ -203,9 +199,13 @@ class ToonNet:
         num_layers = min(self.num_layers, 4)
         with tf.variable_scope('decoder', reuse=reuse):
             with slim.arg_scope(toon_net_argscope(padding='SAME', training=training, center=False)):
-                for l in range(0, num_layers):
+                for l in range(0, num_layers-1):
                     net = up_conv2d(net, num_outputs=f_dims[num_layers - l - 1], scope='deconv_{}'.format(l))
-                net = slim.conv2d(net, num_outputs=3, scope='deconv_{}'.format(num_layers),
+
+                in_shape = net.get_shape().as_list()
+                net = tf.image.resize_images(net, (2 * in_shape[1], 2 * in_shape[2]),
+                                             tf.image.ResizeMethod.BILINEAR)
+                net = slim.conv2d(net, num_outputs=3, scope='deconv_{}'.format(num_layers), stride=1,
                                   activation_fn=tf.nn.tanh, normalizer_fn=None)
                 return net
 

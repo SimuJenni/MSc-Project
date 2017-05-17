b4b9487aca6d64a57fad26241e3275f050b2a6ed
SimuJenni <simujenni@gmail.com>
2017-05-17 22:13:01 +0200

    Some slight adjustments

--- train_imagenet_64.py
+++ train_imagenet_64.py
@@ -6,7 +6,7 @@ from models.ToonNet_64 import ToonNet
 model = ToonNet(num_layers=5, batch_size=256)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[64, 64, 3])
-trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=90, tag='second',
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=90, tag='3rd',
                          lr_policy='linear', optimizer='adam', init_lr=0.0003, end_lr=0.0001)
 trainer.train()
 

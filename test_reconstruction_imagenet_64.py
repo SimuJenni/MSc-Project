b455f2f7536c4accccefa16581cef0ebb0b5f3c4
SimuJenni <simujenni@gmail.com>
2017-05-18 13:18:56 +0200

    Testing 64

--- test_reconstruction_imagenet_64.py
+++ test_reconstruction_imagenet_64.py
@@ -3,7 +3,7 @@ from eval.ToonNetTester_64 import ToonNetTester
 from datasets.ImageNet import ImageNet
 from models.ToonNet_64 import ToonNet
 
-model = ToonNet(num_layers=5, batch_size=128)
+model = ToonNet(num_layers=5, batch_size=256)
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=[96, 96, 3])
 tester = ToonNetTester(model, data, preprocessor, tag='3rd')

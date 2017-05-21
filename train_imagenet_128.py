041c64b712ba84329794e3461d169cb6b46f30d3
SimuJenni <simujenni@gmail.com>
2017-05-21 15:34:15 +0200

    Removing BN in TF

--- train_imagenet_128.py
+++ train_imagenet_128.py
@@ -6,7 +6,7 @@ from models.ToonNet import ToonNet
 model = ToonNet(num_layers=5, batch_size=96)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[128, 128, 3])
-trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=80, tag='refactored',
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=80, tag='128',
                          lr_policy='const', optimizer='adam')
 trainer.train()
 

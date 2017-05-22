b73c481d8407af2a1faf0ee12fc2f5e9ea0b2ec7
Simon Jenni <simujenni@gmail.com>
2017-05-22 16:12:02 +0200

    Test with LRN

--- test_classifier_voc.py
+++ test_classifier_voc.py
@@ -6,5 +6,5 @@ from models.ToonNet_64 import ToonNet
 model = ToonNet(num_layers=5, batch_size=1)
 data = VOC2007()
 preprocessor = VOCPreprocessor(target_shape=[224, 224, 3], augment_color=False, area_range=(0.1, 1.0))
-tester = ToonNetTester(model, data, preprocessor, tag='unsupervised_wbn')
+tester = ToonNetTester(model, data, preprocessor, tag='64_unsupervised_wbn')
 tester.test_classifier_voc(num_conv_trained=5)

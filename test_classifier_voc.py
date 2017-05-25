329b1a48407aa43ccc24cfed29cecdccf06a64c4
SimuJenni <simujenni@gmail.com>
2017-05-25 13:43:52 +0200

    Retrain with relu

--- test_classifier_voc.py
+++ test_classifier_voc.py
@@ -1,10 +1,10 @@
 from Preprocessor import VOCPreprocessor
 from eval.ToonNetTester import ToonNetTester
 from datasets.VOC2007 import VOC2007
-from models.ToonNet_64 import ToonNet
+from models.ToonNet_fcnobn import ToonNet
 
 model = ToonNet(num_layers=5, batch_size=1)
 data = VOC2007()
-preprocessor = VOCPreprocessor(target_shape=[224, 224, 3], augment_color=False, area_range=(0.1, 1.0))
-tester = ToonNetTester(model, data, preprocessor, tag='64_unsupervised_wbn')
+preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0))
+tester = ToonNetTester(model, data, preprocessor, tag='supervised_bn')
 tester.test_classifier_voc(num_conv_trained=5)

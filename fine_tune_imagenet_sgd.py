fcd9b88f5a0a1cf5eeda70470b64b9b9113a578a
SimuJenni <simujenni@gmail.com>
2017-05-25 16:28:04 +0200

    Retrain with relu

--- fine_tune_imagenet_sgd.py
+++ fine_tune_imagenet_sgd.py
@@ -11,4 +11,4 @@ preprocessor = ImageNetPreprocessor(target_shape=[224, 224, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=5, tag='retrain_sgd',
                          lr_policy='voc', optimizer='sgd+momentum', init_lr=0.001)
 chpt_path = os.path.join(LOG_DIR, 'imagenet_ToonNet_default_refactored_finetune_conv_5/model.ckpt-450360')
-trainer.transfer_finetune(chpt_path, num_conv2train=5, num_conv2init=0)
+trainer.transfer_finetune(chpt_path, num_conv2train=5, num_conv2init=5)

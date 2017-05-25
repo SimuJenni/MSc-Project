0084edefd9fa0959689231585af00affd828cad7
SimuJenni <simujenni@gmail.com>
2017-05-26 01:37:24 +0200

    Retrain with relu

--- fine_tune_imagenet_sgd.py
+++ fine_tune_imagenet_sgd.py
@@ -9,6 +9,6 @@ model = ToonNet(num_layers=5, batch_size=256)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[224, 224, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=5, tag='retrain_nobn_sgd',
-                         lr_policy='voc', optimizer='sgd+momentum', init_lr=0.001)
+                         lr_policy='voc', optimizer='sgd+momentum', init_lr=0.0002)
 chpt_path = os.path.join(LOG_DIR, 'batchnorm_removed/alexnet_nobn_sup.ckpt')
 trainer.transfer_finetune(chpt_path, num_conv2train=5, num_conv2init=5)

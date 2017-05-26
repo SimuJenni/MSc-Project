136e8af03ca1f06b5a92f41db52b1097149ea74c
SimuJenni <simujenni@gmail.com>
2017-05-26 11:36:19 +0200

    Testing with reinit

--- train_continue_imagenet.py
+++ train_continue_imagenet.py
@@ -10,7 +10,7 @@ model = ToonNet(num_layers=5, batch_size=128)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[96, 96, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=10, tag='continue_nobn_sgd_reinit',
-                         lr_policy='step', optimizer='sgd+momentum', init_lr=0.0002, reinit_fc=True)
+                         lr_policy='const', optimizer='adam', init_lr=0.0002, reinit_fc=True)
 
 all_ckpt = os.path.join(LOG_DIR, 'imagenet_ToonNet_default_refactored/model.ckpt-800722')
 disc_ckpt = os.path.join(LOG_DIR, 'batchnorm_removed/alexnet_nobn.ckpt')

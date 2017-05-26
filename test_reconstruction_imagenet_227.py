f725a9ba3a44bf02a0e851f6040a8fce861a7b91
SimuJenni <simujenni@gmail.com>
2017-05-26 15:48:00 +0200

    Testing with reinit

--- test_reconstruction_imagenet_227.py
+++ test_reconstruction_imagenet_227.py
@@ -1,7 +1,7 @@
 from Preprocessor import ImageNetPreprocessor
 from eval.ToonNetTester import ToonNetTester
 from datasets.ImageNet_227 import ImageNet
-from models.ToonNet_nobn import ToonNet
+from models.ToonNet_bn224 import ToonNet
 
 model = ToonNet(num_layers=5, batch_size=24)
 data = ImageNet()

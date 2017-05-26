a063a6c47d68c13546facb59d8ee13b09b49b164
SimuJenni <simujenni@gmail.com>
2017-05-26 11:28:54 +0200

    Testing with reinit

--- fine_tune_imagenet_sgd.py
+++ fine_tune_imagenet_sgd.py
@@ -9,6 +9,6 @@ model = ToonNet(num_layers=5, batch_size=256)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[224, 224, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=5, tag='retrain_nobn_sgd_reinitfc',
-                         lr_policy='voc', optimizer='sgd+momentum', init_lr=0.0002, reinit_fc=True)
+                         lr_policy='step', optimizer='sgd+momentum', init_lr=0.0002, reinit_fc=True)
 chpt_path = os.path.join(LOG_DIR, 'batchnorm_removed/alexnet_nobn_sup.ckpt')
 trainer.transfer_finetune(chpt_path, num_conv2train=5, num_conv2init=5)

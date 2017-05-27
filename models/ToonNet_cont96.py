333962f9471f4de8bb8b6deb52ccb89d6156a285
SimuJenni <simujenni@gmail.com>
2017-05-27 15:38:36 +0200

    contiunue training 96

--- models/ToonNet_cont96.py
+++ models/ToonNet_cont96.py
@@ -273,9 +273,9 @@ class AlexNet:
                     # Fully connected layers
                     net = slim.flatten(net)
                     net = slim.fully_connected(net, 4096, scope='fc1', trainable=with_fc)
-                    net = slim.dropout(net, 0.5, is_training=training)
+                    net = slim.dropout(net, 0.8, is_training=training)
                     net = slim.fully_connected(net, 4096, scope='fc2', trainable=with_fc)
-                    net = slim.dropout(net, 0.5, is_training=training)
+                    net = slim.dropout(net, 0.8, is_training=training)
                     net = slim.fully_connected(net, 2,
                                                activation_fn=None,
                                                normalizer_fn=None,

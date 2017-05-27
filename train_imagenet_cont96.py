62acfae4d2278b1c80977224f154f5e495890e7f
SimuJenni <simujenni@gmail.com>
2017-05-27 16:08:44 +0200

    contiunue training 96

--- train_imagenet_cont96.py
+++ train_imagenet_cont96.py
@@ -7,6 +7,6 @@ model = ToonNet(num_layers=5, batch_size=128)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[96, 96, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=120, tag='cont96',
-                         lr_policy='const', optimizer='adam')
+                         lr_policy='cont', optimizer='adam')
 trainer.train()
 

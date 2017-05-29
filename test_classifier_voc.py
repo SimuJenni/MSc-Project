60968fae658756d277c5b8e9dfd4139ad5e4b343
Simon Jenni <simujenni@gmail.com>
2017-05-29 17:34:19 +0200

    Testing

--- test_classifier_voc.py
+++ test_classifier_voc.py
@@ -1,10 +1,10 @@
 from Preprocessor import VOCPreprocessor
 from eval.ToonNetTester import ToonNetTester
 from datasets.VOC2007 import VOC2007
-from models.ToonNet_fcnobn import ToonNet
+from models.ToonNet import ToonNet
 
 model = ToonNet(num_layers=5, batch_size=1)
 data = VOC2007()
 preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0))
-tester = ToonNetTester(model, data, preprocessor, tag='supervised_bn_adam_restrart')
+tester = ToonNetTester(model, data, preprocessor, tag='old')
 tester.test_classifier_voc(num_conv_trained=5)

c1fad4d8fa3a8a04c4f19713590ba413753e8444
SimuJenni <simujenni@gmail.com>
2017-05-31 22:14:44 +0200

    Testing

--- test_converter_rmbn.py
+++ test_converter_rmbn.py
@@ -44,7 +44,7 @@ trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, 
 
 model_dir = '../test_converter'
 proto_path = 'deploy 3.prototxt'
-ckpt = '../test_converter/model.ckpt-1201082'
+ckpt = '../test_converter/model.ckpt-1401260'
 save_path = os.path.join(model_dir, 'alexnet_v2.caffemodel')
 
 np.random.seed(42)

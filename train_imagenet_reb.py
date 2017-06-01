2fc29ce58332d97e4c5fef8cabfa377f4ad9fc4b
SimuJenni <simujenni@gmail.com>
2017-06-01 10:31:08 +0200

    Testing

--- train_imagenet_reb.py
+++ train_imagenet_reb.py
@@ -1,11 +1,11 @@
 from Preprocessor import ImageNetPreprocessor
-from train.ToonNetTrainer_reb import ToonNetTrainer
+from train.ToonNetTrainer import ToonNetTrainer
 from datasets.ImageNet import ImageNet
 from models.ToonNet_reb import ToonNet
 
 model = ToonNet(num_layers=5, batch_size=128)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[96, 96, 3])
-trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=90, tag='rebuttal_3',
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=90, tag='rebuttal',
                          lr_policy='const', optimizer='adam')
 trainer.train()

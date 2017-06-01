5462dd2433f1814a469c0e33742b8be767ff9787
SimuJenni <simujenni@gmail.com>
2017-06-01 09:32:17 +0200

    Testing

--- train_continue_imagenet.py
+++ train_continue_imagenet.py
@@ -4,14 +4,14 @@ from Preprocessor import ImageNetPreprocessor
 from train.ToonNetTrainer_cont import ToonNetTrainer
 from constants import LOG_DIR
 from datasets.ImageNet import ImageNet
-from models.ToonNet_wobn import ToonNet
+from models.ToonNet import ToonNet
 
 model = ToonNet(num_layers=5, batch_size=128)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[96, 96, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=40,
-                         tag='cont_wobn_96_2', lr_policy='const', optimizer='adam', init_lr=0.0002,
-                         reinit_fc=True)
+                         tag='cont_essy', lr_policy='const', optimizer='adam', init_lr=0.0002,
+                         reinit_fc=False)
 
 all_ckpt = os.path.join(LOG_DIR, 'imagenet_ToonNet_default_refactored/model.ckpt-800722')
 disc_ckpt = os.path.join(LOG_DIR, 'batchnorm_removed/alexnet_wobn.ckpt')
--- train_imagenet_reb.py
+++ train_imagenet_reb.py
@@ -5,7 +5,7 @@ from models.ToonNet_reb import ToonNet
 
 model = ToonNet(num_layers=5, batch_size=196)
 data = ImageNet()
-preprocessor = ImageNetPreprocessor(target_shape=[64, 64, 3])
+preprocessor = ImageNetPreprocessor(target_shape=[96, 96, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=120, tag='rebuttal_3',
                          lr_policy='const', optimizer='adam')
 trainer.train()

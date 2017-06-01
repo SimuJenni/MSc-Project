5b693fefe9f9d524d33bb01c430842d964fbab99
SimuJenni <simujenni@gmail.com>
2017-06-01 22:00:23 +0200

    test

--- test_reconstruction_imagenet.py
+++ test_reconstruction_imagenet.py
@@ -7,5 +7,5 @@ from constants import IMAGENET_EASY_TF_DATADIR
 model = ToonNet(num_layers=5, batch_size=128)
 data = ImageNet(cartoon_data_dir=IMAGENET_EASY_TF_DATADIR)
 preprocessor = Preprocessor(target_shape=[96, 96, 3])
-tester = ToonNetTester(model, data, preprocessor, tag='rebuttal')
+tester = ToonNetTester(model, data, preprocessor, tag='rebuttal_new')
 tester.test_reconstruction()

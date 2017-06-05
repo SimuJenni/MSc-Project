9349fd32453f816a4d4a39c28745c5a5c177c290
SimuJenni <simujenni@gmail.com>
2017-06-05 12:51:29 +0200

    test

--- train_imagenet_vanilla.py
+++ train_imagenet_vanilla.py
@@ -1,12 +1,12 @@
 from Preprocessor import ImageNetPreprocessor
 from train.ToonNetTrainer import ToonNetTrainer
 from datasets.ImageNet import ImageNet
-from models.ToonNet_vanilla import ToonNet
+from models.ToonNet import ToonNet
 
 model = ToonNet(num_layers=5, batch_size=128, vanilla_alex=True)
 data = ImageNet()
 preprocessor = ImageNetPreprocessor(target_shape=[96, 96, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=90, tag='vanilla_correct',
-                         lr_policy='linear', optimizer='adam', init_lr=0.0003)
+                         lr_policy='linear', optimizer='adam', init_lr=0.0004)
 trainer.train()
 

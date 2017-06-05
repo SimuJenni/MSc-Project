08d2e12ae3074cf7ac701b5303627eba71bda09a
SimuJenni <simujenni@gmail.com>
2017-06-05 12:43:03 +0200

    test

--- test_reconstruction_imagenet.py
+++ test_reconstruction_imagenet.py
@@ -6,5 +6,5 @@ from models.ToonNet import ToonNet
 model = ToonNet(num_layers=5, batch_size=128, vanilla_alex=True)
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=[96, 96, 3])
-tester = ToonNetTester(model, data, preprocessor, tag='vanilla')
+tester = ToonNetTester(model, data, preprocessor, tag='vanilla_correct')
 tester.test_reconstruction()

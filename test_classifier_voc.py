c7198e72d39db23ae40dd2067e8c603d30c7237a
Simon Jenni <simujenni@gmail.com>
2017-06-08 11:53:39 +0200

    Making ready for cleanup

--- test_classifier_voc.py
+++ test_classifier_voc.py
@@ -1,10 +1,10 @@
 from Preprocessor import VOCPreprocessor
 from eval.ToonNetTester import ToonNetTester
 from datasets.VOC2007 import VOC2007
-from models.ToonNet import ToonNet
+from models.ToonNet_new import ToonNet
 
-model = ToonNet(num_layers=5, batch_size=1)
+model = ToonNet(num_layers=5, batch_size=1, fix_bn=True, vanilla_alex=True)
 data = VOC2007()
-preprocessor = VOCPreprocessor(target_shape=[224, 224, 3], augment_color=False, area_range=(0.1, 1.0))
-tester = ToonNetTester(model, data, preprocessor, tag='orig')
+preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0))
+tester = ToonNetTester(model, data, preprocessor, tag='NEW_correct_4')
 tester.test_classifier_voc(num_conv_trained=5)

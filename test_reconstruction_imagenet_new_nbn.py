1baf9336767eb9e750346adfccc8546d887ed1f3
SimuJenni <simujenni@gmail.com>
2017-06-12 10:50:40 +0200

    Making ready for merging

---
+++ test_reconstruction_imagenet_new_nbn.py
@@ -0,0 +1,10 @@
+from Preprocessor import Preprocessor
+from eval.ToonNetTester import ToonNetTester
+from datasets.ImageNet import ImageNet
+from models.ToonNet_new_nbn import ToonNet
+
+model = ToonNet(num_layers=5, batch_size=128, vanilla_alex=True)
+data = ImageNet()
+preprocessor = Preprocessor(target_shape=[96, 96, 3])
+tester = ToonNetTester(model, data, preprocessor, tag='NEW_nbn')
+tester.test_reconstruction()

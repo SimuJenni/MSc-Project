34cb654f4d7b044d9ee5221c83e3e8e15852834d
SimuJenni <simujenni@gmail.com>
2017-06-14 23:54:17 +0200

    Making ready for merging

--- models/ToonNet_nfcbn.py
+++ models/ToonNet_nfcbn.py
@@ -354,11 +354,11 @@ class AlexNet:
                 encoded = net
 
                 if with_fc:
-                    net = slim.conv2d(net, 1, kernel_size=[3, 3], stride=1, activation_fn=tf.nn.tanh, scope='conv_6',
-                                      padding='VALID', normalizer_fn=None, biases_initializer=tf.zeros_initializer)
-                    net = slim.avg_pool2d(net, kernel_size=[2, 2], stride=1)
                     net = slim.flatten(net)
-
+                    net = slim.fully_connected(net, 1, scope='fc',
+                                               activation_fn=None,
+                                               normalizer_fn=None,
+                                               biases_initializer=tf.zeros_initializer)
                 return net, encoded
 
 

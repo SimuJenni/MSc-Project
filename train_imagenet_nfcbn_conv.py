d743e1d5a9f0df5d3b4cb37d19f7b486d5544ce7
SimuJenni <simujenni@gmail.com>
2017-06-15 21:37:09 +0200

    Making ready for merging

--- train_imagenet_nfcbn_conv.py
+++ train_imagenet_nfcbn_conv.py
@@ -3,7 +3,7 @@ from train.ToonNetTrainer_nfcbn import ToonNetTrainer
 from datasets.ImageNet import ImageNet
 from models.ToonNet_nfcbn import ToonNet
 
-model = ToonNet(num_layers=5, batch_size=128, vanilla_alex=True)
+model = ToonNet(num_layers=5, batch_size=96, vanilla_alex=True)
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=[112, 112, 3])
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=100, tag='nfcbn_conv',

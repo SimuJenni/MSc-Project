3e0ab2343919ed3010b39b5844fd732829f5a44f
SimuJenni <simujenni@gmail.com>
2017-06-22 18:34:30 +0200

    testing nofc2

--- train_imagenet_nofc2.py
+++ train_imagenet_nofc2.py
@@ -5,8 +5,8 @@ from models.ToonNet_nofc2 import ToonNet
 
 model = ToonNet(num_layers=5, batch_size=96, vanilla_alex=True)
 data = ImageNet()
-preprocessor = Preprocessor(target_shape=[112, 112, 3], augment_color=True, hsv_color=False)
-trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=135, tag='nofc2',
+preprocessor = Preprocessor(target_shape=[112, 112, 3], augment_color=True, hsv_color=True)
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=135, tag='nofc2_hsv',
                          lr_policy='linear', optimizer='adam', init_lr=0.0003, end_lr=0.0)
 trainer.train()
 

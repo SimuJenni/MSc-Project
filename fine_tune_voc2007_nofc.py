80c020db0717b604e324257a7bd955b3365fe06c
Simon Jenni <simujenni@gmail.com>
2017-06-23 10:21:27 +0200

    Testing with higher BN decay

--- fine_tune_voc2007_nofc.py
+++ fine_tune_voc2007_nofc.py
@@ -1,4 +1,4 @@
-from Preprocessor import VOCPreprocessor
+from Preprocessor_new import VOCPreprocessor
 from train.ToonNetTrainer import ToonNetTrainer
 from datasets.ImageNet import ImageNet
 from datasets.VOC2007 import VOC2007
@@ -8,8 +8,8 @@ from utils import get_checkpoint_path
 
 model = ToonNet(num_layers=5, batch_size=16, fix_bn=False, vanilla_alex=True)
 data = ImageNet()
-preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0))
-trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=250, tag='nofc2',
+preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0), hsv_color=True)
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=250, tag='nofc2_hsv',
                          lr_policy='linear', optimizer='adam', init_lr=0.0002)
 chpt_path = get_checkpoint_path(trainer.get_save_dir())
 
--- test_classifier_voc_nofc.py
+++ test_classifier_voc_nofc.py
@@ -1,10 +1,10 @@
-from Preprocessor import VOCPreprocessor
+from Preprocessor_new import VOCPreprocessor
 from eval.ToonNetTester import ToonNetTester
 from datasets.VOC2007 import VOC2007
 from models.ToonNet_nofc2 import ToonNet
 
 model = ToonNet(num_layers=5, batch_size=1, fix_bn=True, vanilla_alex=True)
 data = VOC2007()
-preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0))
-tester = ToonNetTester(model, data, preprocessor, tag='nofc2')
+preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0), hsv_color=True)
+tester = ToonNetTester(model, data, preprocessor, tag='nofc2_hsv')
 tester.test_classifier_voc(num_conv_trained=5)

9e0d0a7b6925e5d7d81fbd07a0ffcbf30ad2d43a
SimuJenni <simujenni@gmail.com>
2017-06-25 23:27:01 +0200

    testing nofc2

---
+++ test_reconstruction_imagenet_nofc3.py
@@ -0,0 +1,10 @@
+from Preprocessor_new import Preprocessor
+from eval.ToonNetTester_nofc import ToonNetTester
+from datasets.ImageNet import ImageNet
+from models.ToonNet_nofc3 import ToonNet
+
+model = ToonNet(num_layers=5, batch_size=96, vanilla_alex=True)
+data = ImageNet()
+preprocessor = Preprocessor(target_shape=[112, 122, 3])
+tester = ToonNetTester(model, data, preprocessor, tag='nofc3')
+tester.test_reconstruction()

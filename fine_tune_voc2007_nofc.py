ee27e2c51140acdcc9ca7cb4886af5eafdd3478b
Simon Jenni <simujenni@gmail.com>
2017-06-27 13:00:12 +0200

    s

--- fine_tune_voc2007_nofc.py
+++ fine_tune_voc2007_nofc.py
@@ -6,7 +6,7 @@ from models.ToonNet_nofc2 import ToonNet
 from utils import get_checkpoint_path
 
 
-model = ToonNet(num_layers=5, batch_size=16, fix_bn=False, vanilla_alex=True)
+model = ToonNet(num_layers=5, batch_size=16, fix_bn=True, vanilla_alex=True)
 data = ImageNet()
 preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0), hsv_color=True)
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=250, tag='nofc2_hsv',

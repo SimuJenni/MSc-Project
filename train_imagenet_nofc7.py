812dd24aec99dfd88514c670c3eee71ef8726129
Simon Jenni <simujenni@gmail.com>
2017-06-27 15:59:24 +0200

    testing nofc5

--- train_imagenet_nofc7.py
+++ train_imagenet_nofc7.py
@@ -3,7 +3,7 @@ from train.ToonNetTrainer_nofc7 import ToonNetTrainer
 from datasets.ImageNet import ImageNet
 from models.ToonNet_nofc7 import ToonNet
 
-model = ToonNet(num_layers=5, batch_size=128, vanilla_alex=True)
+model = ToonNet(num_layers=5, batch_size=96, vanilla_alex=True)
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=[112, 112, 3], augment_color=True, hsv_color=False)
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=180, tag='nofc7',

ce9279c5569d1a3de50fe2e3229ed393509207d5
Simon Jenni <simujenni@gmail.com>
2017-07-03 11:09:33 +0200

    testing preprocessing

--- test_reconstruction_imagenet_nofc7.py
+++ test_reconstruction_imagenet_nofc7.py
@@ -6,5 +6,5 @@ from models.ToonNet_nofc7 import ToonNet
 model = ToonNet(num_layers=5, batch_size=64, vanilla_alex=True)
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=[112, 112, 3], augment_color=True, hsv_color=False)
-tester = ToonNetTester(model, data, preprocessor, tag='nofc7')
+tester = ToonNetTester(model=model, dataset=data, pre_processor=preprocessor, tag='nofc7')
 tester.test_reconstruction()

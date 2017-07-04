dcd57e26cf962575c9bcf8864a09437a0558516b
Simon Jenni <simujenni@gmail.com>
2017-07-04 09:55:09 +0200

    testing

--- fine_tune_voc2007_nofc.py
+++ fine_tune_voc2007_nofc.py
@@ -1,15 +1,15 @@
 from Preprocessor_new import VOCPreprocessor
-from train.ToonNetTrainer_nofc7 import ToonNetTrainer
+from train.ToonNetTrainer_nofc6 import ToonNetTrainer
 from datasets.ImageNet import ImageNet
 from datasets.VOC2007 import VOC2007
-from models.ToonNet_nofc7 import ToonNet
+from models.ToonNet_nofc6 import ToonNet
 from utils import get_checkpoint_path
 
 
 model = ToonNet(num_layers=5, batch_size=16, fix_bn=False, vanilla_alex=True)
 data = ImageNet()
 preprocessor = VOCPreprocessor(target_shape=[224, 224, 3], augment_color=False, area_range=(0.1, 1.0), hsv_color=False)
-trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=250, tag='nofc7',
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=250, tag='nofc6',
                          lr_policy='linear', optimizer='adam', init_lr=0.0002)
 chpt_path = get_checkpoint_path(trainer.get_save_dir())
 
--- test_classifier_voc_nofc.py
+++ test_classifier_voc_nofc.py
@@ -1,10 +1,10 @@
 from Preprocessor_new import VOCPreprocessor
 from eval.ToonNetTester import ToonNetTester
 from datasets.VOC2007 import VOC2007
-from models.ToonNet_nofc2 import ToonNet
+from models.ToonNet_nofc6 import ToonNet
 
-model = ToonNet(num_layers=5, batch_size=1, fix_bn=True, vanilla_alex=True)
+model = ToonNet(num_layers=5, batch_size=1, fix_bn=False, vanilla_alex=True)
 data = VOC2007()
-preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0), hsv_color=True)
-tester = ToonNetTester(model, data, preprocessor, tag='nofc2_hsv')
+preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0), hsv_color=False)
+tester = ToonNetTester(model, data, preprocessor, tag='nofc6')
 tester.test_classifier_voc(num_conv_trained=5)

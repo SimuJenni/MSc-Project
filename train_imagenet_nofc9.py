a114c38fd5229612e58518a6271b6d26672e6173
Simon Jenni <simujenni@gmail.com>
2017-07-04 10:49:09 +0200

    testing

---
+++ train_imagenet_nofc9.py
@@ -0,0 +1,13 @@
+from Preprocessor_new import Preprocessor
+from train.ToonNetTrainer_nofc9 import ToonNetTrainer
+from datasets.ImageNet import ImageNet
+from models.ToonNet_nofc9 import ToonNet
+
+im_shape = [96, 96, 3]
+model = ToonNet(num_layers=4, batch_size=128, im_shape=im_shape, vanilla_alex=True)
+data = ImageNet()
+preprocessor = Preprocessor(target_shape=im_shape, augment_color=True, hsv_color=False)
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=180, tag='nofc9',
+                         lr_policy='linear', optimizer='adam', init_lr=0.0003, end_lr=1e-6)
+trainer.train()
+

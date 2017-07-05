f77787231f26fae2bc415139e0d69712f794657a
SimuJenni <simujenni@gmail.com>
2017-07-05 12:58:37 +0200

    nofc4

---
+++ test_reconstruction_imagenet_nofc10.py
@@ -0,0 +1,11 @@
+from Preprocessor_new import Preprocessor
+from eval.ToonNetTester_nofc10 import ToonNetTester
+from datasets.ImageNet import ImageNet
+from models.ToonNet_nofc10 import ToonNet
+
+im_shape = [112, 112, 3]
+model = ToonNet(num_layers=4, batch_size=128, im_shape=im_shape, vanilla_alex=True)
+data = ImageNet()
+preprocessor = Preprocessor(target_shape=im_shape, augment_color=True, hsv_color=False)
+tester = ToonNetTester(model=model, dataset=data, pre_processor=preprocessor, tag='nofc10')
+tester.test_reconstruction()

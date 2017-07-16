601d89b160c6b6b233283594053dde5fb6b9a488
SimuJenni <simujenni@gmail.com>
2017-07-16 20:54:42 +0200

    Testing nofc11

--- train_imagenet_nofc12.py
+++ train_imagenet_nofc12.py
@@ -4,7 +4,7 @@ from datasets.ImageNet import ImageNet
 from models.ToonNet_nofc12 import ToonNet
 
 im_shape = [112, 112, 3]
-model = ToonNet(num_layers=3, batch_size=128, im_shape=im_shape, vanilla_alex=True)
+model = ToonNet(num_layers=4, batch_size=128, im_shape=im_shape, vanilla_alex=True)
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=im_shape, augment_color=True, hsv_color=False, area_range=(0.25, 1.0))
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=160, tag='nofc12',

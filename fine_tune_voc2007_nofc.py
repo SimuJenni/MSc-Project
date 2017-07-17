8d0b4981b48aacfd8bcf505680fbae27acf1a844
Simon Jenni <simujenni@gmail.com>
2017-07-17 12:43:35 +0200

    just testing

--- fine_tune_voc2007_nofc.py
+++ fine_tune_voc2007_nofc.py
@@ -1,15 +1,15 @@
 from Preprocessor_new import VOCPreprocessor
-from train.ToonNetTrainer_nofc6 import ToonNetTrainer
+from train.ToonNetTrainer_nofc10 import ToonNetTrainer
 from datasets.ImageNet import ImageNet
 from datasets.VOC2007 import VOC2007
-from models.ToonNet_nofc6 import ToonNet
+from models.ToonNet_nofc10 import ToonNet
 from utils import get_checkpoint_path
 
-
-model = ToonNet(num_layers=5, batch_size=16, fix_bn=False, vanilla_alex=True)
+im_shape = [224, 224, 3]
+model = ToonNet(num_layers=5, batch_size=16, fix_bn=False, vanilla_alex=True, im_shape=im_shape)
 data = ImageNet()
-preprocessor = VOCPreprocessor(target_shape=[224, 224, 3], augment_color=False, area_range=(0.1, 1.0), hsv_color=False)
-trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=250, tag='nofc6',
+preprocessor = VOCPreprocessor(target_shape=im_shape, augment_color=False, area_range=(0.1, 1.0), hsv_color=False)
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=250, tag='nofc10',
                          lr_policy='linear', optimizer='adam', init_lr=0.0002)
 chpt_path = get_checkpoint_path(trainer.get_save_dir())
 
--- test_classifier_voc_nofc.py
+++ test_classifier_voc_nofc.py
@@ -1,10 +1,11 @@
 from Preprocessor_new import VOCPreprocessor
 from eval.ToonNetTester import ToonNetTester
 from datasets.VOC2007 import VOC2007
-from models.ToonNet_nofc6 import ToonNet
+from models.ToonNet_nofc10 import ToonNet
 
-model = ToonNet(num_layers=5, batch_size=1, fix_bn=False, vanilla_alex=True)
+im_shape = [224, 224, 3]
+model = ToonNet(num_layers=5, batch_size=1, fix_bn=False, vanilla_alex=True, im_shape=im_shape)
 data = VOC2007()
-preprocessor = VOCPreprocessor(target_shape=[227, 227, 3], augment_color=False, area_range=(0.1, 1.0), hsv_color=False)
-tester = ToonNetTester(model, data, preprocessor, tag='nofc6')
+preprocessor = VOCPreprocessor(target_shape=im_shape, augment_color=False, area_range=(0.1, 1.0), hsv_color=False)
+tester = ToonNetTester(model, data, preprocessor, tag='nofc10')
 tester.test_classifier_voc(num_conv_trained=5)

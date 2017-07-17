2d43df3c83a3f6a2b6bbe6d635341e3b89d7e52e
SimuJenni <simujenni@gmail.com>
2017-07-17 22:34:55 +0200

    Testing nofc11

--- test_reconstruction_imagenet_nofc12.py
+++ test_reconstruction_imagenet_nofc12.py
@@ -6,5 +6,5 @@ from models.ToonNet_nofc12 import ToonNet
 model = ToonNet(num_layers=4, batch_size=128, im_shape=[96, 96])
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=[227, 227, 3], augment_color=True, hsv_color=False)
-tester = ToonNetTester(model=model, dataset=data, pre_processor=preprocessor, tag='nofc11')
+tester = ToonNetTester(model=model, dataset=data, pre_processor=preprocessor, tag='nofc12')
 tester.test_reconstruction()

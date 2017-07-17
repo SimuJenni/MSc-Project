b337f1c21f6dec744302556e4fc22537d1d7d731
Simon Jenni <simujenni@gmail.com>
2017-07-17 15:25:07 +0200

    Testing bigger pdrop

--- train_imagenet_nofc12.py
+++ train_imagenet_nofc12.py
@@ -3,7 +3,7 @@ from train.ToonNetTrainer_nofc12 import ToonNetTrainer
 from datasets.ImageNet import ImageNet
 from models.ToonNet_nofc12 import ToonNet
 
-model = ToonNet(num_layers=4, batch_size=128, im_shape=[112, 112])
+model = ToonNet(num_layers=4, batch_size=128, im_shape=[96, 96])
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=[227, 227, 3], augment_color=True, hsv_color=False, area_range=(0.1, 1.0))
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=200, tag='nofc12',

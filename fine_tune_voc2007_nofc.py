4fb6112e932f61d6840923527d3d8440aaf25d26
Simon Jenni <simujenni@gmail.com>
2017-07-18 23:32:47 +0200

    testing

--- fine_tune_voc2007_nofc.py
+++ fine_tune_voc2007_nofc.py
@@ -2,14 +2,14 @@ from Preprocessor_new import VOCPreprocessor
 from train.ToonNetTrainer_nofc10 import ToonNetTrainer
 from datasets.ImageNet import ImageNet
 from datasets.VOC2007 import VOC2007
-from models.ToonNet_nofc11 import ToonNet
+from models.ToonNet_nofc12 import ToonNet
 from utils import get_checkpoint_path
 
-im_shape = [224, 224, 3]
-model = ToonNet(num_layers=5, batch_size=16, fix_bn=False, vanilla_alex=True, im_shape=im_shape)
+im_shape = [227, 227, 3]
+model = ToonNet(num_layers=5, batch_size=16, fix_bn=False, im_shape=im_shape)
 data = ImageNet()
 preprocessor = VOCPreprocessor(target_shape=im_shape, augment_color=False, area_range=(0.1, 1.0), hsv_color=False)
-trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=250, tag='nofc11',
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=250, tag='nofc12',
                          lr_policy='linear', optimizer='adam', init_lr=0.0002)
 chpt_path = get_checkpoint_path(trainer.get_save_dir())
 
--- test_classifier_voc_nofc.py
+++ test_classifier_voc_nofc.py
@@ -1,11 +1,11 @@
 from Preprocessor_new import VOCPreprocessor
 from eval.ToonNetTester import ToonNetTester
 from datasets.VOC2007 import VOC2007
-from models.ToonNet_nofc10 import ToonNet
+from models.ToonNet_nofc12 import ToonNet
 
-im_shape = [224, 224, 3]
-model = ToonNet(num_layers=5, batch_size=1, fix_bn=False, vanilla_alex=True, im_shape=im_shape)
+im_shape = [227, 227, 3]
+model = ToonNet(num_layers=5, batch_size=1, fix_bn=False, im_shape=im_shape)
 data = VOC2007()
 preprocessor = VOCPreprocessor(target_shape=im_shape, augment_color=False, area_range=(0.1, 1.0), hsv_color=False)
-tester = ToonNetTester(model, data, preprocessor, tag='nofc11')
+tester = ToonNetTester(model, data, preprocessor, tag='nofc12')
 tester.test_classifier_voc(num_conv_trained=5)

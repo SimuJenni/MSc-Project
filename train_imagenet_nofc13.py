0b0c68906d4acf111cd30e6d0cb928c2ef810a37
SimuJenni <simujenni@gmail.com>
2017-07-18 12:06:10 +0200

    Testing nofc11

--- train_imagenet_nofc13.py
+++ train_imagenet_nofc13.py
@@ -6,7 +6,7 @@ from models.ToonNet_nofc13 import ToonNet
 model = ToonNet(num_layers=4, batch_size=128, im_shape=[96, 96])
 data = ImageNet()
 preprocessor = Preprocessor(target_shape=[227, 227, 3], augment_color=True, hsv_color=False, area_range=(0.2, 1.0))
-trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=200, tag='nofc13',
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=60, tag='nofc13',
                          lr_policy='linear', optimizer='adam', init_lr=0.0004, end_lr=1e-6)
 trainer.train_autoencoder()
 

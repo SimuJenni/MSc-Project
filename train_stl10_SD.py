e15ef80e46fb7dc20ff59b95ae5ec53b62b5c165
SimuJenni <simujenni@gmail.com>
2017-07-20 14:21:01 +0200

    Testing nofc11

--- train_stl10_SD.py
+++ train_stl10_SD.py
@@ -3,7 +3,7 @@ from train.ToonNetTrainer_SD import ToonNetTrainer
 from datasets.STL10 import STL10
 from models.ToonNet_SD import ToonNet
 
-model = ToonNet(num_layers=3, batch_size=200, im_shape=[64, 64, 3], pool5=False)
+model = ToonNet(num_layers=4, batch_size=200, im_shape=[64, 64, 3], pool5=False)
 data = STL10()
 preprocessor = Preprocessor(target_shape=[64, 64, 3], im_shape=[96, 96, 3], augment_color=True)
 trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=200, tag='SD',

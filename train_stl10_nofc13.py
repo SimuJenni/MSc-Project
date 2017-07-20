c3113eaca3072fef6b69211984ddc111c9b5b60a
SimuJenni <simujenni@gmail.com>
2017-07-20 13:39:53 +0200

    Testing nofc11

--- train_stl10_nofc13.py
+++ train_stl10_nofc13.py
@@ -6,7 +6,7 @@ from models.ToonNet_nofc13 import ToonNet
 model = ToonNet(num_layers=3, batch_size=200, im_shape=[64, 64, 3], pool5=False)
 data = STL10()
 preprocessor = Preprocessor(target_shape=[64, 64, 3], im_shape=[96, 96, 3], augment_color=True)
-trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=300, tag='nofc13',
+trainer = ToonNetTrainer(model=model, dataset=data, pre_processor=preprocessor, num_epochs=200, tag='nofc13',
                          lr_policy='const', optimizer='adam', init_lr=0.0002)
 # trainer.train_autoencoder()
 trainer.train()
